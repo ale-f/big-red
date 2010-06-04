a42a4d338121fd3e00e63c3803289e22ac61ce95
Alec Faithfull <alef@itu.dk>
2010-06-04 14:23:59 +0200

    [Signature] Added the Signature class
    The plan is that Signatures will take over all of the responsibilities
    currently discharged by the Port- and ControlAuthorities; this is to bring
    the internal representation of bigraphs more in line with the formal model.

---
+++ src/dk/itu/big_red/model/Signature.java
@@ -0,0 +1,12 @@
+package dk.itu.big_red.model;
+
+/**
+ * The Signature is a central, per-{@link Bigraph} registry of {@link Control}s
+ * - definitions of the kinds of {@link Node} that can exist on a bigraph,
+ * along with their {@link Port}s.
+ * @author alec
+ *
+ */
+public class Signature {
+
+}
