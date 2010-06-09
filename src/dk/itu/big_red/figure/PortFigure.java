9a2c08f84b3ca06530d42a8da929a9f87bd49d78
Alec Faithfull <alef@itu.dk>
2010-06-09 11:57:00 +0200

    [PortFigure] Added a default appearance
    Since the code was taken in its entirety from Name, Ports will now appear
    on the bigraph as a slightly transparent red rectangle. And that's it -
    they have a tooltip, but nothing else (and they can't be moved or
    manipulated). Still, it's an important first step

--- src/dk/itu/big_red/figure/PortFigure.java
+++ src/dk/itu/big_red/figure/PortFigure.java
@@ -1,13 +1,29 @@
 package dk.itu.big_red.figure;
 
+import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Graphics;
+import org.eclipse.draw2d.XYLayout;
 
 public class PortFigure extends AbstractFigure {
 
+	public PortFigure() {
+		super();
+		
+		XYLayout layout = new XYLayout();
+		setLayoutManager(layout);
+		
+	}
+	
 	@Override
 	protected void fillShape(Graphics graphics) {
-		// TODO Auto-generated method stub
-
+		graphics.pushState();
+		try {
+			graphics.setBackgroundColor(ColorConstants.red);
+			graphics.setAlpha(128);
+			graphics.fillRectangle(getConstraintCopy(getTotalOffset()));
+		} finally {
+			graphics.popState();
+		}
 	}
 
 	@Override
@@ -15,5 +31,4 @@ public class PortFigure extends AbstractFigure {
 		// TODO Auto-generated method stub
 
 	}
-
 }
