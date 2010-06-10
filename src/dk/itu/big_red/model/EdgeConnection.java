0b6110247359e4ba31ed480f4c5c9f3ca3ad489d
Alec Faithfull <alef@itu.dk>
2010-06-10 11:14:57 +0200

    [EdgeConnection] Added a new model object to represent actual connections
    Since the Edge has become a strange, vague container, this new class will
    handle the representation of an individual connection on the bigraph, and
    so it has to worry about sources and targets and things

---
+++ src/dk/itu/big_red/model/EdgeConnection.java
@@ -0,0 +1,61 @@
+package dk.itu.big_red.model;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+
+import org.eclipse.draw2d.Connection;
+
+import dk.itu.big_red.model.interfaces.IConnectable;
+import dk.itu.big_red.model.interfaces.IPropertyChangeNotifier;
+
+/**
+ * An EdgeConnection is the model object behind an actual {@link Connection}
+ * on the bigraph. {@link Edge}s create and manage them.
+ * @author alec
+ *
+ */
+public class EdgeConnection implements IPropertyChangeNotifier {
+	private PropertyChangeSupport listeners = new PropertyChangeSupport(this);
+
+	/**
+	 * The property name fired when the connection source changes.
+	 */
+	public static final String PROPERTY_SOURCE = "EdgeConnectionSource";
+	/**
+	 * The property name fired when the connection target changes.
+	 */
+	public static final String PROPERTY_TARGET = "EdgeConnectionTarget";
+	
+	private IConnectable source, target;
+	private Edge parent;
+	
+	public EdgeConnection(Edge parent) {
+		this.parent = parent;
+	}
+	
+	public void setSource(IConnectable source, String key) {
+		if (source != null) {
+			IConnectable oldSource = this.source;
+			this.source = source;
+			listeners.firePropertyChange(PROPERTY_SOURCE, oldSource, source);
+		}
+	}
+
+	public void setTarget(IConnectable target, String key) {
+		if (target != null) {
+			IConnectable oldTarget = this.target;
+			this.target = target;
+			listeners.firePropertyChange(PROPERTY_TARGET, oldTarget, target);
+		}
+	}
+	
+	@Override
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		listeners.addPropertyChangeListener(listener);
+	}
+
+	@Override
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		listeners.removePropertyChangeListener(listener);
+	}
+}
