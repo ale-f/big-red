6f95378049f8a3ff534e65ae96d26c79b8bfabb5
Alec Faithfull <alef@itu.dk>
2010-06-10 11:06:24 +0200

    [EdgeReconnectSourceCommand, EdgeReconnectTargetCommand] Deleted both
    Neither class did anything, and they both made use of methods that are
    about to go away - deletion is thus wholly justifiable

--- src/dk/itu/big_red/editpolicies/ThingEdgePolicy.java
+++ src/dk/itu/big_red/editpolicies/ThingEdgePolicy.java
@@ -7,8 +7,6 @@ import org.eclipse.gef.requests.CreateConnectionRequest;
 import org.eclipse.gef.requests.ReconnectRequest;
 
 import dk.itu.big_red.commands.EdgeCreateCommand;
-import dk.itu.big_red.commands.EdgeReconnectSourceCommand;
-import dk.itu.big_red.commands.EdgeReconnectTargetCommand;
 import dk.itu.big_red.model.Thing;
 import dk.itu.big_red.model.Edge;
 
@@ -34,18 +32,12 @@ public class ThingEdgePolicy extends GraphicalNodeEditPolicy {
 	
 	@Override
 	protected Command getReconnectTargetCommand(ReconnectRequest request) {
-		EdgeReconnectTargetCommand cmd = new EdgeReconnectTargetCommand();
-		cmd.setModel((Edge)request.getConnectionEditPart().getModel());
-		cmd.setTarget((Thing)getHost().getModel());
-		return cmd;
+		return null;
 	}
 	
 	@Override
 	protected Command getReconnectSourceCommand(ReconnectRequest request) {
-		EdgeReconnectSourceCommand cmd = new EdgeReconnectSourceCommand();
-		cmd.setModel((Edge)request.getConnectionEditPart().getModel());
-		cmd.setSource((Thing)getHost().getModel());
-		return cmd;
+		return null;
 	}
 
 }
