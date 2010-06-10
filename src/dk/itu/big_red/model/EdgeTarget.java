fa8868d30fd5f2f51987209cd815c5e64aa1b0f3
Alec Faithfull <alef@itu.dk>
2010-06-10 10:59:24 +0200

    [EdgeTarget] Added some interfaces to EdgeTarget
    They're implemented with useless stubs at the moment, but that's fine, as
    nothing actually tries to create an EdgeTarget yet

--- src/dk/itu/big_red/model/EdgeTarget.java
+++ src/dk/itu/big_red/model/EdgeTarget.java
@@ -1,5 +1,9 @@
 package dk.itu.big_red.model;
 
+import org.eclipse.draw2d.geometry.Rectangle;
+
+import dk.itu.big_red.model.interfaces.IConnectable;
+
 /**
  * An EdgeTarget is a small object used to keep {@link Edge}s as close to the
  * formal bigraphical model as possible. GEF/GMF requires that every connection
@@ -9,6 +13,36 @@ package dk.itu.big_red.model;
  * @author alec
  *
  */
-public class EdgeTarget {
+public class EdgeTarget implements IConnectable {
+
+	@Override
+	public void connect(Edge e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void disconnect(Edge e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isConnected(Edge e) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Rectangle getLayout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLayout(Rectangle layout) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
