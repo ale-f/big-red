6385a6c6e672139541c60dc9a79e3d1ad8a44237
Alec Faithfull <alef@itu.dk>
2010-06-10 11:11:19 +0200

    [Edge] Removed all mention of sources, targets, and keys
    As you can see from the Javadoc, Edges aren't supposed to manage these
    things any more - they're closer to the formal bigraph model, and so much
    more abstract than they were. A new class will be introduced in the next
    commit to pick up the slack

--- src/dk/itu/big_red/model/Edge.java
+++ src/dk/itu/big_red/model/Edge.java
@@ -2,6 +2,7 @@ package dk.itu.big_red.model;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
 
 
 import org.eclipse.core.runtime.IAdaptable;
@@ -9,6 +10,7 @@ import org.eclipse.draw2d.Connection;
 import org.eclipse.ui.views.properties.IPropertySource;
 import org.w3c.dom.Node;
 
+import dk.itu.big_red.model.interfaces.IConnectable;
 import dk.itu.big_red.model.interfaces.IPropertyChangeNotifier;
 import dk.itu.big_red.model.interfaces.IXMLisable;
 import dk.itu.big_red.propertysources.EdgePropertySource;
@@ -29,52 +31,13 @@ import dk.itu.big_red.propertysources.EdgePropertySource;
 public class Edge implements IAdaptable, IPropertyChangeNotifier, IXMLisable {
 	private PropertyChangeSupport listeners = new PropertyChangeSupport(this);
 	
-	public static final String PROPERTY_SOURCE = "EdgeSource";
-	public static final String PROPERTY_TARGET = "EdgeTarget";
 	public static final String PROPERTY_COMMENT = "EdgeComment";
-	public static final String PROPERTY_SOURCE_KEY = "EdgeSourceKey";
-	public static final String PROPERTY_TARGET_KEY = "EdgeTargetKey";
 	
-	private String sourceKey, targetKey;
-	private Thing source, target;
+	private ArrayList<IConnectable> points = new ArrayList<IConnectable>();
 	
 	private IPropertySource propertySource = null;
 
 	private String comment = null;
-	
-	public void setSource(Thing source, String key) {
-		if (source != null) {
-			Thing oldSource = this.source;
-			String oldSourceKey = this.sourceKey;
-			this.source = source;
-			this.sourceKey = key;
-			listeners.firePropertyChange(PROPERTY_SOURCE, oldSource, source);
-			listeners.firePropertyChange(PROPERTY_SOURCE_KEY, oldSourceKey, key);
-		}
-	}
-	public Thing getSource() {
-		return source;
-	}
-	public String getSourceKey() {
-		return sourceKey;
-	}
-	
-	public void setTarget(Thing target, String key) {
-		if (target != null) {
-			Thing oldTarget = this.target;
-			String oldTargetKey = this.targetKey;
-			this.target = target;
-			this.targetKey = key;
-			listeners.firePropertyChange(PROPERTY_TARGET, oldTarget, target);
-			listeners.firePropertyChange(PROPERTY_SOURCE_KEY, oldTargetKey, key);
-		}
-	}
-	public Thing getTarget() {
-		return target;
-	}
-	public String getTargetKey() {
-		return targetKey;
-	}
 	
 	@SuppressWarnings("unchecked")
 	@Override
@@ -106,13 +69,6 @@ public class Edge implements IAdaptable, IPropertyChangeNotifier, IXMLisable {
 		listeners.firePropertyChange(PROPERTY_COMMENT, oldComment, comment);
 	}
 	
-	public boolean sourceOK(Thing source) {
-		return (source != null && source != this.source && source != this.target);
-	}
-	
-	public boolean targetOK(Thing target) {
-		return (target != null && target != this.source && target != this.target);
-	}
 	@Override
 	public void fromXML(Node d) {
 		// TODO Auto-generated method stub
