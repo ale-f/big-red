748c91e6d4140b847d3b04cd896686f326047b24
Alec Faithfull <alef@itu.dk>
2010-06-11 11:01:16 +0200

    [Edge] Added the Edge.addPoint and Edge.removePoint methods
    This moves Edge substantially closer to completion - it creates and manages
    EdgeConnections, which is its job. Of course, EdgeTargets will need to
    appear on the bigraph somewhere if any of this stuff is to work

--- src/dk/itu/big_red/model/Edge.java
+++ src/dk/itu/big_red/model/Edge.java
@@ -66,6 +66,39 @@ public class Edge implements IAdaptable, IPropertyChangeNotifier, IXMLisable {
 		return null;
 	}
 
+	/**
+	 * Adds the given {@link IConnectable} to this Edge's set of points, and
+	 * creates a new {@link EdgeConnection} joining it to this Edge's {@link
+	 * EdgeTarget}.
+	 * @param point an IConnectable
+	 */
+	public void addPoint(IConnectable point) {
+		EdgeConnection c = new EdgeConnection(this);
+		c.setSource(point);
+		points.add(point);
+		connections.add(c);
+		point.addConnection(c);
+	}
+	
+	/**
+	 * Removes the given {@link IConnectable} from this Edge's set of points
+	 * and destroys its {@link EdgeConnection}.
+	 * @param point an IConnectable
+	 */
+	public void removePoint(IConnectable point) {
+		if (points.contains(point)) {
+			for (EdgeConnection e : point.getConnections()) {
+				int index = connections.indexOf(e);
+				if (index != -1) {
+					points.remove(point);
+					connections.remove(index);
+					point.removeConnection(e);
+					break;
+				}
+			}
+		}
+	}
+	
 	public void addPropertyChangeListener(PropertyChangeListener listener) {
 		listeners.addPropertyChangeListener(listener);
 	}
