29e6e0252a6a3c91378d46c9da410d6860fb0104
Alec Faithfull <alef@itu.dk>
2010-06-14 13:23:37 +0200

    [EdgeTargetPart] Added figure-updating and model-listening
    The model-listening is particularly stupid at the moment ("has anything at
    all happened? DO EVERYTHING!!"), but it'll do as an interim solution

--- src/dk/itu/big_red/part/EdgeTargetPart.java
+++ src/dk/itu/big_red/part/EdgeTargetPart.java
@@ -1,15 +1,27 @@
 package dk.itu.big_red.part;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.gef.DragTracker;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 
+import dk.itu.big_red.figure.EdgeConnectionFigure;
 import dk.itu.big_red.figure.EdgeTargetFigure;
+import dk.itu.big_red.model.EdgeConnection;
+import dk.itu.big_red.model.EdgeTarget;
 
-public class EdgeTargetPart extends AbstractGraphicalEditPart {
-
+public class EdgeTargetPart extends AbstractGraphicalEditPart implements PropertyChangeListener {
+	@Override
+	public EdgeTarget getModel() {
+		return (EdgeTarget)super.getModel();
+	}
+	
 	@Override
 	protected IFigure createFigure() {
 		// TODO Auto-generated method stub
@@ -22,4 +34,33 @@ public class EdgeTargetPart extends AbstractGraphicalEditPart {
 		
 	}
 
+	public void activate() {
+		super.activate();
+		getModel().addPropertyChangeListener(this);
+		refreshVisuals();
+	}
+
+	public void deactivate() {
+		getModel().removePropertyChangeListener(this);
+		super.deactivate();
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		refreshVisuals();
+		refreshTargetConnections();
+	}
+	
+	public void refreshVisuals() {
+		EdgeTargetFigure figure = (EdgeTargetFigure)getFigure();
+		EdgeTarget model = getModel();
+		
+		figure.setLayout(model.getLayout());
+		figure.setToolTip(model.getParent().getComment());
+	}
+
+	@Override
+	protected List<EdgeConnection> getModelTargetConnections() {
+        return getModel().getConnections();
+    }
 }
