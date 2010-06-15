6bcef210051b34d6e0482182cc96a607c5dae1f1
Alec Faithfull <alef@itu.dk>
2010-06-15 17:29:32 +0200

    [EdgeTargetPart] Added the appropriate connection anchor providers
    Now dragging-and-dropping edges from EdgeTargetParts actually looks good,
    rather than just drawing a random line through the origin

--- src/dk/itu/big_red/part/EdgeTargetPart.java
+++ src/dk/itu/big_red/part/EdgeTargetPart.java
@@ -5,10 +5,14 @@ import java.beans.PropertyChangeListener;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.draw2d.ChopboxAnchor;
+import org.eclipse.draw2d.ConnectionAnchor;
 import org.eclipse.draw2d.IFigure;
+import org.eclipse.gef.ConnectionEditPart;
 import org.eclipse.gef.DragTracker;
 import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPolicy;
+import org.eclipse.gef.NodeEditPart;
 import org.eclipse.gef.Request;
 import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 
@@ -18,7 +22,7 @@ import dk.itu.big_red.figure.EdgeTargetFigure;
 import dk.itu.big_red.model.EdgeConnection;
 import dk.itu.big_red.model.EdgeTarget;
 
-public class EdgeTargetPart extends AbstractGraphicalEditPart implements PropertyChangeListener {
+public class EdgeTargetPart extends AbstractGraphicalEditPart implements NodeEditPart, PropertyChangeListener {
 	@Override
 	public EdgeTarget getModel() {
 		return (EdgeTarget)super.getModel();
@@ -65,5 +69,21 @@ public class EdgeTargetPart extends AbstractGraphicalEditPart implements Propert
 	@Override
 	protected List<EdgeConnection> getModelTargetConnections() {
         return getModel().getConnections();
+    }
+	
+	public ConnectionAnchor getSourceConnectionAnchor(ConnectionEditPart connection) {
+		return new ChopboxAnchor(getFigure());
+    }
+    
+	public ConnectionAnchor getSourceConnectionAnchor(Request request) {
+		return new ChopboxAnchor(getFigure());
+    }
+	
+	public ConnectionAnchor getTargetConnectionAnchor(ConnectionEditPart connection) {
+		return new ChopboxAnchor(getFigure());
+    }
+    
+	public ConnectionAnchor getTargetConnectionAnchor(Request request) {
+		return new ChopboxAnchor(getFigure());
     }
 }
