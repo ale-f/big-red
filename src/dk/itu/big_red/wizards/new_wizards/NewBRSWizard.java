11bbed1a262e9792e7a59efb94f272b4388c948a
Alec Faithfull <alef@itu.dk>
2010-06-17 23:31:16 +0200

    [plugin, NewBRSWizard] Created an Eclipse wizard for building BRSes
    Eventually, a BRS will be an Eclipse project like any other. This is an
    important first step (I do say that a lot in these logs, don't I?) towards
    this goal

---
+++ src/dk/itu/big_red/wizards/new_wizards/NewBRSWizard.java
@@ -0,0 +1,147 @@
+package dk.itu.big_red.wizards.new_wizards;
+
+import org.eclipse.jface.dialogs.IDialogSettings;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.IWizardContainer;
+import org.eclipse.jface.wizard.IWizardPage;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.swt.graphics.RGB;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class NewBRSWizard implements INewWizard {
+
+	@Override
+	public void init(IWorkbench workbench, IStructuredSelection selection) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addPages() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean canFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void createPageControls(Composite pageContainer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void dispose() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public IWizardContainer getContainer() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Image getDefaultPageImage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IDialogSettings getDialogSettings() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IWizardPage getNextPage(IWizardPage page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IWizardPage getPage(String pageName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int getPageCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public IWizardPage[] getPages() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IWizardPage getPreviousPage(IWizardPage page) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public IWizardPage getStartingPage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RGB getTitleBarColor() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getWindowTitle() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isHelpAvailable() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean needsPreviousAndNextButtons() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean needsProgressMonitor() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean performCancel() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean performFinish() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void setContainer(IWizardContainer wizardContainer) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
