88afdebaa57be9db787bcb67f53b6fc13d7a3216
Alec Faithfull <alef@itu.dk>
2010-06-18 10:56:13 +0200

    [plugin] Added support for the Common Navigator
    The Common Navigator is the Eclipse component that shows the nice, friendly
    tree view of the workspace. Of course, it's not really an Eclipse
    component, it's an Eclipse *IDE* component (totally different), and so it
    requires an inordinate amount of XML fiddling to get working

--- src/dk/itu/big_red/intro/ApplicationWorkbenchAdvisor.java
+++ src/dk/itu/big_red/intro/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,8 @@
 package dk.itu.big_red.intro;
 
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
@@ -17,10 +20,17 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
     
     public void initialize(IWorkbenchConfigurer configurer) {
         super.initialize(configurer);
+        org.eclipse.ui.ide.IDE.registerAdapters();
     }
 
 	public String getInitialWindowPerspectiveId() {
 		return PERSPECTIVE_ID;
+	}
+	
+	@Override
+	public IAdaptable getDefaultPageInput() {
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		return workspace.getRoot();
 	}
 	
 	public void postStartup() {
--- src/dk/itu/big_red/intro/Perspective.java
+++ src/dk/itu/big_red/intro/Perspective.java
@@ -5,16 +5,20 @@ import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
 public class Perspective implements IPerspectiveFactory {
-	private static String ID_TABS_FOLDER = "big_red.intro.tabs";
-	
 	public void createInitialLayout(IPageLayout layout) {
 		String editorArea = layout.getEditorArea();
 		layout.setEditorAreaVisible(true);
 		
-		IFolderLayout tabs = layout.createFolder(
-			ID_TABS_FOLDER, IPageLayout.LEFT, 0.3f, editorArea);
-		tabs.addView(IPageLayout.ID_OUTLINE);
-		tabs.addPlaceholder(IPageLayout.ID_PROP_SHEET);
+		IFolderLayout topLeft = layout.createFolder("topLeft", IPageLayout.LEFT, 0.20f,
+				editorArea);
+		
+		topLeft.addView("dk.itu.big_red.CommonNavigator");
+		topLeft.addPlaceholder(IPageLayout.ID_BOOKMARKS);
+		
+		IFolderLayout topRight = layout.createFolder(
+			"topRight", IPageLayout.RIGHT, 0.75f, editorArea);
+		topRight.addPlaceholder(IPageLayout.ID_OUTLINE);
+		topRight.addPlaceholder(IPageLayout.ID_PROP_SHEET);
 		
 		layout.addShowViewShortcut(IPageLayout.ID_OUTLINE);
 	}
