980ad5de4a9a34998d5267cf836f407a9936617c
Alec Faithfull <alef@itu.dk>
2010-06-21 13:36:30 +0200

    [plugin] Added placeholder editors for modifying rules and signatures

---
+++ src/dk/itu/big_red/editors/SignatureEditorActionBarContributor.java
@@ -0,0 +1,19 @@
+package dk.itu.big_red.editors;
+
+import org.eclipse.gef.ui.actions.ActionBarContributor;
+
+public class SignatureEditorActionBarContributor extends ActionBarContributor {
+
+	@Override
+	protected void buildActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void declareGlobalActionKeys() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
