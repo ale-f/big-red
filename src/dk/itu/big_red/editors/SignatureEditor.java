cd42373ea33964723cad59941a1d496d1705f912
Alec Faithfull <alef@itu.dk>
2010-06-22 14:51:46 +0200

    [SignatureEditor] Added a placeholder control to this placeholder wizard
    The placeholders are piling up thick and fast! This one at least has the
    virtue of showing the name of its input

--- src/dk/itu/big_red/editors/SignatureEditor.java
+++ src/dk/itu/big_red/editors/SignatureEditor.java
@@ -1,14 +1,29 @@
 package dk.itu.big_red.editors;
 
+import java.util.EventObject;
+
 import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.gef.commands.CommandStackListener;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Font;
+import org.eclipse.swt.graphics.FontData;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorSite;
+import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchPartConstants;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.part.EditorPart;
 
-public class SignatureEditor extends EditorPart {
-
+public class SignatureEditor extends EditorPart implements CommandStackListener, ISelectionListener {
+	public static final String ID = "dk.itu.big_red.SignatureEditor";
+	
 	@Override
 	public void doSave(IProgressMonitor monitor) {
 		// TODO Auto-generated method stub
@@ -25,7 +40,10 @@ public class SignatureEditor extends EditorPart {
 	public void init(IEditorSite site, IEditorInput input)
 			throws PartInitException {
 		// TODO Auto-generated method stub
-
+		setSite(site);
+		setInput(input);
+		firePropertyChange(IWorkbenchPartConstants.PROP_INPUT);
+		getSite().getWorkbenchWindow().getSelectionService().addSelectionListener(this);
 	}
 
 	@Override
@@ -42,14 +60,32 @@ public class SignatureEditor extends EditorPart {
 
 	@Override
 	public void createPartControl(Composite parent) {
-		// TODO Auto-generated method stub
-
+		GridLayout layout = new GridLayout(1, false);
+		parent.setLayout(layout);
+		
+		Text label = new Text(parent, SWT.CENTER | SWT.MULTI);
+		label.setText("SignatureEditor\nplaceholder\n\nEditor input is\n" + getEditorInput().getName());
+		label.setEditable(false);
+		label.setBackground(parent.getBackground());
+		label.setLayoutData(new GridData(SWT.CENTER, SWT.CENTER, true, true));
 	}
 
 	@Override
 	public void setFocus() {
 		// TODO Auto-generated method stub
 
+	}
+
+	@Override
+	public void commandStackChanged(EventObject event) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void selectionChanged(IWorkbenchPart part, ISelection selection) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
