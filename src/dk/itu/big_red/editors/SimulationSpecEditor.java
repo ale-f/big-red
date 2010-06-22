801dc9050038fc38ecc6c26d69f01d0e01687ace
Alec Faithfull <alef@itu.dk>
2010-06-22 11:34:27 +0200

    [plugin] Added a placeholder editor for modifying simulation specs

---
+++ src/dk/itu/big_red/editors/SimulationSpecEditor.java
@@ -0,0 +1,31 @@
+package dk.itu.big_red.editors;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.ui.forms.editor.FormEditor;
+
+public class SimulationSpecEditor extends FormEditor {
+
+	@Override
+	protected void addPages() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doSave(IProgressMonitor monitor) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doSaveAs() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean isSaveAsAllowed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
---
+++ src/dk/itu/big_red/editors/SimulationSpecEditorActionBarContributor.java
@@ -0,0 +1,20 @@
+package dk.itu.big_red.editors;
+
+import org.eclipse.gef.ui.actions.ActionBarContributor;
+
+public class SimulationSpecEditorActionBarContributor extends
+		ActionBarContributor {
+
+	@Override
+	protected void buildActions() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void declareGlobalActionKeys() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
