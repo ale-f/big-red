9e4a9b696280db4ef5bc8887aed7ecf5029d01a4
Alec Faithfull <alef@itu.dk>
2010-06-29 11:09:38 +0200

    [NewBRSWizard] Ported to use the new Project methods

--- src/dk/itu/big_red/wizards/new_wizards/NewBRSWizard.java
+++ src/dk/itu/big_red/wizards/new_wizards/NewBRSWizard.java
@@ -1,5 +1,6 @@
 package dk.itu.big_red.wizards.new_wizards;
 
+import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -10,29 +11,28 @@ import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 
 import dk.itu.big_red.util.NullInputStream;
+import dk.itu.big_red.util.Project;
 
 public class NewBRSWizard extends Wizard implements INewWizard {
 	private NewBRSWizardPage page = null;
 	
 	@Override
 	public boolean performFinish() {
-		IWorkspaceRoot r = ResourcesPlugin.getWorkspace().getRoot();
-		IProject p = r.getProject(page.getProjectName());
-		if (p.exists()) {
+		if (Project.projectExists(page.getProjectName())) {
 			page.setErrorMessage("A project with this name already exists.");
 			return false;
 		} else {
 			try {
-				p.create(null);
-				p.open(null);
-				p.getFolder("signatures").create(0, true, null);
-				p.getFolder("signatures").getFile(page.getProjectName() + ".bigraph-signature").create(NullInputStream.getInstance(), 0, null);
-				p.getFolder("agents").create(0, true, null);
-				p.getFolder("agents").getFile("test.bigraph-agent").create(NullInputStream.getInstance(), 0, null);
-				p.getFolder("rules").create(0, true, null);
-				p.getFile(page.getProjectName() + ".bigraph-simulation-spec").create(NullInputStream.getInstance(), 0, null);
+				IProject p = Project.getProject(page.getProjectName());
+				IFolder signatures = Project.getFolder(p, "signatures"),
+				        agents = Project.getFolder(p, "agents"),
+				        rules = Project.getFolder(p, "rules");
+				Project.getFile(signatures, page.getProjectName() + ".bigraph-signature");
+				Project.getFile(agents, "test.bigraph-agent");
+				Project.getFile(p, page.getProjectName() + ".bigraph-simulation-spec");
 				return true;
 			} catch (CoreException e) {
+				e.printStackTrace();
 				page.setErrorMessage(e.getLocalizedMessage());
 				return false;
 			}
