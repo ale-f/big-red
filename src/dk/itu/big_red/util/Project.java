5ef58c53ad21a490fc80f799ed26d906b5116a33
Alec Faithfull <alef@itu.dk>
2010-06-29 11:34:15 +0200

    [Project] Added some methods to convert IPaths to IResources
    These will be necessary for the various NewWizards, which express the
    user's selected container as an IPath

--- src/dk/itu/big_red/util/Project.java
+++ src/dk/itu/big_red/util/Project.java
@@ -4,9 +4,11 @@ import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.Path;
 
 public class Project {
@@ -91,5 +93,46 @@ public class Project {
 	 */
 	public static boolean fileExists(IContainer c, String name) {
 		return c.getFile(new Path(name)).exists();
+	}
+	
+	/**
+	 * Indicates whether or not <code>c</code> contains a resource with the
+	 * given path.
+	 * @param c an {@link IContainer}
+	 * @param path the path of a would-be child
+	 * @return whether the path identifies an extant resource or not
+	 */
+	public static boolean pathExists(IContainer c, IPath path) {
+		return (c.findMember(path) != null);
+	}
+	
+	/**
+	 * Gets a resource with the given path (relative to <code>c</code>). Note
+	 * that this method will <i>not</i> create the resource if it doesn't
+	 * already exist.
+	 * @param c the container to search in, or - if <code>null</code> - the workspace root
+	 * @param path the path of a would-be resource
+	 * @return the {@link IResource} requested, or <code>null</code> if it
+	 * doesn't exist
+	 */
+	public static IResource findResourceByPath(IContainer c, IPath path) {
+		if (c == null)
+			c = getWorkspaceRoot();
+		return c.findMember(path);
+	}
+	
+	/**
+	 * Gets a container with the given path (relative to <code>c</code>). Note
+	 * that this method will <i>not</i> create the container if it doesn't
+	 * already exist.
+	 * @param c the container to search in, or - if <code>null</code> - the workspace root
+	 * @param path the path of a would-be container
+	 * @return the {@link IContainer} requested, or <code>null</code> if it
+	 * doesn't exist
+	 * @see Project#findResourceByPath
+	 */
+	public static IContainer findContainerByPath(IContainer c, IPath path) {
+		IResource r = findResourceByPath(c, path);
+		return (r instanceof IContainer ? (IContainer)r : null);
 	}
 }
