ffa40b65bd7731b4b2f6c6feed862f94e4942460
Alec Faithfull <alef@itu.dk>
2010-06-29 11:49:40 +0200

    [new_wizards] Added real implementations of performFinish to everything
    Agents, rules, and simulation specs can now be created - all as empty files
    for the time being, admittedly, but the Project methods are working nicely

--- src/dk/itu/big_red/wizards/new_wizards/NewRuleWizard.java
+++ src/dk/itu/big_red/wizards/new_wizards/NewRuleWizard.java
@@ -1,9 +1,13 @@
 package dk.itu.big_red.wizards.new_wizards;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+
+import dk.itu.big_red.util.Project;
 
 
 public class NewRuleWizard extends Wizard implements INewWizard {
@@ -11,7 +15,16 @@ public class NewRuleWizard extends Wizard implements INewWizard {
 	
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
+		IContainer c =
+			Project.findContainerByPath(null, page.getContainerFullPath());
+		if (c != null) {
+			try {
+				Project.getFile(c, page.getFileName());
+				return true;
+			} catch (CoreException e) {
+				page.setErrorMessage(e.getLocalizedMessage());
+			}
+		}
 		return false;
 	}
 	
@@ -21,6 +34,7 @@ public class NewRuleWizard extends Wizard implements INewWizard {
 		
 		page.setTitle("Rule");
 		page.setDescription("Create a new rule in an existing bigraphical reactive system.");
+		page.setFileExtension("bigraph-rule");
 		
 		addPage(page);
 	}
--- src/dk/itu/big_red/wizards/new_wizards/NewSimulationSpecWizard.java
+++ src/dk/itu/big_red/wizards/new_wizards/NewSimulationSpecWizard.java
@@ -1,16 +1,29 @@
 package dk.itu.big_red.wizards.new_wizards;
 
+import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
+
+import dk.itu.big_red.util.Project;
 
 public class NewSimulationSpecWizard extends Wizard implements INewWizard {
 	private NewSimulationSpecWizardPage page = null;
 	
 	@Override
 	public boolean performFinish() {
-		// TODO Auto-generated method stub
+		IContainer c =
+			Project.findContainerByPath(null, page.getContainerFullPath());
+		if (c != null) {
+			try {
+				Project.getFile(c, page.getFileName());
+				return true;
+			} catch (CoreException e) {
+				page.setErrorMessage(e.getLocalizedMessage());
+			}
+		}
 		return false;
 	}
 
@@ -20,6 +33,7 @@ public class NewSimulationSpecWizard extends Wizard implements INewWizard {
 		
 		page.setTitle("Simulation spec");
 		page.setDescription("Create a simulation spec for an existing bigraphical reactive system.");
+		page.setFileExtension("bigraph-simulation-spec");
 		
 		addPage(page);
 	}
