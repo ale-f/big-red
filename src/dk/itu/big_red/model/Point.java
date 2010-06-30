13d432ee0f4211056bd2dbf32d6045dd2586f81b
Alec Faithfull <alef@itu.dk>
2010-06-30 14:44:37 +0200

    [Point] Added a Point class
    This introduces a concept from the bigraph model that will also mean that
    Port and InnerName don't have to have duplicate copies of the same code.
    What's not to like?

---
+++ src/dk/itu/big_red/model/Point.java
@@ -0,0 +1,107 @@
+package dk.itu.big_red.model;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.draw2d.geometry.Rectangle;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+
+import dk.itu.big_red.model.interfaces.IConnectable;
+import dk.itu.big_red.model.interfaces.IPropertyChangeNotifier;
+import dk.itu.big_red.model.interfaces.IXMLisable;
+import dk.itu.big_red.util.DOM;
+
+/**
+ * Points are objects which can be connected to <em>at most one</em> {@link
+ * Link} - {@link Port}s and {@link InnerName}s.
+ * @author alec
+ *
+ */
+public class Point implements IConnectable, IPropertyChangeNotifier, IXMLisable {
+
+	@Override
+	public Rectangle getLayout() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setLayout(Rectangle layout) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Bigraph getBigraph() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setBigraph(Bigraph bigraph) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	protected EdgeConnection connection = null;
+	
+	@Override
+	public void addConnection(EdgeConnection e) {
+		if (connection != null)
+			connection.getParent().removePoint(this);
+		connection = e;
+		listeners.firePropertyChange(IConnectable.PROPERTY_SOURCE_EDGE, null, e);
+	}
+
+	@Override
+	public void removeConnection(EdgeConnection e) {
+		if (connection == e) {
+			connection = null;
+			listeners.firePropertyChange(IConnectable.PROPERTY_SOURCE_EDGE, e, null);
+		}
+	}
+	
+	@Override
+	public List<EdgeConnection> getConnections() {
+		ArrayList<EdgeConnection> e = new ArrayList<EdgeConnection>();
+		if (connection != null)
+			e.add(connection);
+		return e;
+	}
+
+	protected PropertyChangeSupport listeners = new PropertyChangeSupport(this);
+	
+	@Override
+	public void addPropertyChangeListener(PropertyChangeListener listener) {
+		listeners.addPropertyChangeListener(listener);
+	}
+
+	@Override
+	public void removePropertyChangeListener(PropertyChangeListener listener) {
+		listeners.removePropertyChangeListener(listener);
+	}
+
+	@Override
+	public Node toXML(Node d) {
+		Document doc = d.getOwnerDocument();
+		
+		Element r = doc.createElement(getClass().getSimpleName().toLowerCase());
+		if (connection != null) {
+			DOM.applyAttributesToElement(r,
+					"name", Integer.toString(hashCode(), 16),
+					"link", Integer.toString(connection.getParent().hashCode(), 16));
+		}
+		
+		return r;
+	}
+
+	@Override
+	public void fromXML(Node d) {
+		// TODO Auto-generated method stub
+		
+	}
+}
