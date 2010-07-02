23b2cfb53fc43bd333394a7bb7d92fdc56bbbfb3
Alec Faithfull <alef@itu.dk>
2010-07-02 12:59:05 +0200

    [PortPart] Made PortPart extend AbstractPart
    Since AbstractPart is genuinely abstract now, this actually works!

--- src/dk/itu/big_red/part/PortPart.java
+++ src/dk/itu/big_red/part/PortPart.java
@@ -5,13 +5,11 @@ import java.beans.PropertyChangeListener;
 import java.util.List;
 
 import org.eclipse.draw2d.ConnectionAnchor;
-import org.eclipse.draw2d.EllipseAnchor;
 import org.eclipse.draw2d.IFigure;
 import org.eclipse.gef.ConnectionEditPart;
 import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.NodeEditPart;
 import org.eclipse.gef.Request;
-import org.eclipse.gef.editparts.AbstractGraphicalEditPart;
 
 import dk.itu.big_red.editpolicies.EdgeCreationPolicy;
 import dk.itu.big_red.figure.PortFigure;
@@ -20,7 +18,7 @@ import dk.itu.big_red.model.EdgeConnection;
 import dk.itu.big_red.model.Port;
 import dk.itu.big_red.model.interfaces.IConnectable;
 
-public class PortPart extends AbstractGraphicalEditPart implements NodeEditPart, PropertyChangeListener {
+public class PortPart extends AbstractPart implements NodeEditPart, PropertyChangeListener {
 	@Override
 	public Port getModel() {
 		return (Port)super.getModel();
@@ -29,16 +27,6 @@ public class PortPart extends AbstractGraphicalEditPart implements NodeEditPart,
 	@Override
 	protected IFigure createFigure() {
 		return new PortFigure();
-	}
-
-	public void activate() {
-		super.activate();
-		getModel().addPropertyChangeListener(this);
-	}
-
-	public void deactivate() {
-		getModel().removePropertyChangeListener(this);
-		super.deactivate();
 	}
 	
 	@Override
