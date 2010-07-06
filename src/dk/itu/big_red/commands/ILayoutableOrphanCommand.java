a890fa50e1e306fc3967678dfef18ec2eadad4f7
Alec Faithfull <alef@itu.dk>
2010-07-06 15:00:47 +0200

    [ILayoutableOrphanCommand] Added a command for orphaning elements
    This isn't hooked up to the EditPolicy yet, of course - the corresponding
    add command, required to make that work, will arrive in the next commit

---
+++ src/dk/itu/big_red/commands/ILayoutableOrphanCommand.java
@@ -0,0 +1,39 @@
+package dk.itu.big_red.commands;
+
+import java.util.List;
+
+import org.eclipse.gef.EditPart;
+import org.eclipse.gef.commands.Command;
+
+import dk.itu.big_red.model.interfaces.ILayoutable;
+
+public class ILayoutableOrphanCommand extends Command {
+	private ILayoutable parent = null;
+	private List children = null;
+	
+	public void setParent(Object parent) {
+		if (parent instanceof ILayoutable)
+			this.parent = (ILayoutable)parent;
+	}
+	
+	public void setChildren(Object children) {
+		if (children instanceof List)
+			this.children = (List)children;
+	}
+	
+	public boolean canExecute() {
+		return (this.parent != null && this.children != null);
+	}
+	
+	public void execute() {
+		for (Object i : children)
+			if (i instanceof EditPart)
+				parent.removeChild((ILayoutable)((EditPart)i).getModel());
+	}
+	
+	public void undo() {
+		for (Object i : children)
+			if (i instanceof EditPart)
+				parent.addChild((ILayoutable)((EditPart)i).getModel());
+	}
+}
