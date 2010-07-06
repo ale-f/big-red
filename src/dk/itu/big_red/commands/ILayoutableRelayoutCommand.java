d063dda55f1b526db3176bba1890ebffd100de53
Alec Faithfull <alef@itu.dk>
2010-07-06 15:26:34 +0200

    [ILayoutableRelayoutCommand] Added a parent layout size check
    As you may have been able to guess, the check is taken entirely from
    ILayoutableAddCommand. It's not quite good enough (in both contexts, it
    should also check for overlaps with other children), but it's a start! This
    commit also changes the public methods of the class to take Objects, since
    that seems to work nicely for Commands

--- src/dk/itu/big_red/commands/ILayoutableRelayoutCommand.java
+++ src/dk/itu/big_red/commands/ILayoutableRelayoutCommand.java
@@ -7,23 +7,35 @@ import org.eclipse.gef.commands.Command;
 
 import dk.itu.big_red.model.interfaces.ILayoutable;
 
-
-
 public class ILayoutableRelayoutCommand extends Command {
 	private ILayoutable model;
 	private Rectangle layout, oldLayout;
 	
-	public void execute() {
-		model.setLayout(layout);
-	}
-	
-	public void setConstraint(Rectangle rect) {
-		this.layout = rect;
+	public void setConstraint(Object rect) {
+		if (rect instanceof Rectangle)
+			this.layout = (Rectangle)rect;
 	}
 
 	public void setModel(Object model) {
-		this.model = (ILayoutable)model;
-		this.oldLayout = this.model.getLayout();
+		if (model instanceof ILayoutable) {
+			this.model = (ILayoutable)model;
+			this.oldLayout = this.model.getLayout();
+		}
+	}
+	
+	public boolean parentLayoutCanContainChildLayout() {
+		return (layout.x >= 0 && layout.y >= 0 &&
+				layout.x + layout.width <= model.getParent().getLayout().width &&
+				layout.y + layout.height <= model.getParent().getLayout().height);
+	}
+	
+	public boolean canExecute() {
+		return (model != null && layout != null && oldLayout != null &&
+				parentLayoutCanContainChildLayout());
+	}
+	
+	public void execute() {
+		model.setLayout(layout);
 	}
 
 	public void undo() {
