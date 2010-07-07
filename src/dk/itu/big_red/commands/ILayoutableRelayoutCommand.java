111ace829add182c3c983dd69b89d1c43127e019
Alec Faithfull <alef@itu.dk>
2010-07-07 09:47:10 +0200

    [ILayoutableRelayoutCommand] Added special handling for Bigraph constraints
    Bigraphs don't really have a constraint, but it should still be possible to
    move things around on them

--- src/dk/itu/big_red/commands/ILayoutableRelayoutCommand.java
+++ src/dk/itu/big_red/commands/ILayoutableRelayoutCommand.java
@@ -5,6 +5,7 @@ package dk.itu.big_red.commands;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.commands.Command;
 
+import dk.itu.big_red.model.Bigraph;
 import dk.itu.big_red.model.interfaces.ILayoutable;
 
 public class ILayoutableRelayoutCommand extends Command {
@@ -24,9 +25,10 @@ public class ILayoutableRelayoutCommand extends Command {
 	}
 	
 	public boolean parentLayoutCanContainChildLayout() {
-		return (layout.x >= 0 && layout.y >= 0 &&
-				layout.x + layout.width <= model.getParent().getLayout().width &&
-				layout.y + layout.height <= model.getParent().getLayout().height);
+		return (model.getParent() instanceof Bigraph ||
+				(layout.x >= 0 && layout.y >= 0 &&
+				 layout.x + layout.width <= model.getParent().getLayout().width &&
+				 layout.y + layout.height <= model.getParent().getLayout().height));
 	}
 	
 	public boolean canExecute() {
