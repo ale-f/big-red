4617d15a40df7cab1aef9a166020df4088d61841
Alec Faithfull <alef@itu.dk>
2010-07-13 22:02:21 +0200

    [ConnectionDragCreationToolEntry] Added some Javadoc
    In case you're wondering why I'm suddenly adding Javadoc to everything,
    it's because the Javadoc tool is now automatically run whenever something
    is pushed to the repository. If it's going to be visible, then it should
    probably be helpful, too...

--- src/dk/itu/big_red/tools/ConnectionDragCreationToolEntry.java
+++ src/dk/itu/big_red/tools/ConnectionDragCreationToolEntry.java
@@ -1,10 +1,20 @@
 package dk.itu.big_red.tools;
 
+import org.eclipse.gef.palette.ConnectionCreationToolEntry;
 import org.eclipse.gef.palette.CreationToolEntry;
 import org.eclipse.gef.requests.CreationFactory;
 import org.eclipse.gef.tools.ConnectionDragCreationTool;
 import org.eclipse.jface.resource.ImageDescriptor;
 
+/**
+ * ConnectionDragCreationToolEntries are very simple wrappers which bind a
+ * {@link CreationToolEntry} together with the {@link
+ * ConnectionDragCreationTool} class, providing a palette entry for the
+ * ConnectionDragCreationTool.
+ *
+ * @author alec
+ *
+ */
 public class ConnectionDragCreationToolEntry extends CreationToolEntry {
 
 	public ConnectionDragCreationToolEntry(String label, String shortDesc,
