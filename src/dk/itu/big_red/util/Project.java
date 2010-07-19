bd887e1353b4863016cdc07f94587a28e55b682a
Alec Faithfull <alef@itu.dk>
2010-07-19 10:20:19 +0200

    [Project] Added a getWorkspace method
    Resource change listeners are registered on the workspace itself, so it
    makes sense to have a utility method to retrieve it

--- src/dk/itu/big_red/util/Project.java
+++ src/dk/itu/big_red/util/Project.java
@@ -5,6 +5,7 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
@@ -18,8 +19,12 @@ import org.eclipse.core.runtime.Path;
  *
  */
 public class Project {
+	public static IWorkspace getWorkspace() {
+		return ResourcesPlugin.getWorkspace();
+	}
+	
 	public static IWorkspaceRoot getWorkspaceRoot() {
-		return ResourcesPlugin.getWorkspace().getRoot();
+		return getWorkspace().getRoot();
 	}
 	
 	/**
