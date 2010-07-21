b592bf6ec41e117baa49d5e38a151c9872cd4ee8
Alec Faithfull <alef@itu.dk>
2010-07-21 11:13:34 +0200

    [SignatureXMLImport] Added an Import class for loading signatures from XML
    This is woefully untested at the moment, but once it's committed, saving
    and loading support can reasonably be added to SignatureEditor

---
+++ src/dk/itu/big_red/model/import_export/SignatureXMLImport.java
@@ -0,0 +1,45 @@
+package dk.itu.big_red.model.import_export;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import dk.itu.big_red.exceptions.ImportFailedException;
+import dk.itu.big_red.model.Control;
+import dk.itu.big_red.model.Signature;
+import dk.itu.big_red.model.assistants.ModelFactory;
+import dk.itu.big_red.util.DOM;
+
+public class SignatureXMLImport extends Import<Signature> {
+
+	@Override
+	public Signature importModel() throws ImportFailedException {
+		try {
+			Document d = DOM.parse(source);
+			return (Signature)process(d.getDocumentElement());
+		} catch (Exception e) {
+			throw new ImportFailedException(e);
+		}
+	}
+
+	private void processControl(Element e, Control model) throws ImportFailedException {
+	}
+	
+	private void processSignature(Element e, Signature model) throws ImportFailedException {
+		for (int j = 0; j < e.getChildNodes().getLength(); j++) {
+			if (!(e.getChildNodes().item(j) instanceof Element))
+				continue;
+			Object i = process((Element)e.getChildNodes().item(j));
+			if (i instanceof Control)
+				model.addControl((Control)i);
+		}
+	}
+	
+	private Object process(Element e) throws ImportFailedException {
+		Object model = ModelFactory.getNewObject(e.getTagName());
+		if (model instanceof Signature)
+			processSignature(e, (Signature)model);
+		else if (model instanceof Control)
+			processControl(e, (Control)model);
+		return model;
+	}
+}
