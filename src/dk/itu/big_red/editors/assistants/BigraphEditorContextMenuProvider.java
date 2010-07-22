99115e491f27ca72f1d764b88fc2933e99417661
Alec Faithfull <alef@itu.dk>
2010-07-22 16:15:07 +0200

    [BigraphEditorContextMenuProvider] Added shortcuts to the palette's tools
    Twenty minutes of work to implement a month-old feature request. Not bad!

--- src/dk/itu/big_red/editors/assistants/BigraphEditorContextMenuProvider.java
+++ src/dk/itu/big_red/editors/assistants/BigraphEditorContextMenuProvider.java
@@ -2,9 +2,15 @@ package dk.itu.big_red.editors.assistants;
 
 import org.eclipse.gef.ContextMenuProvider;
 import org.eclipse.gef.EditPartViewer;
+import org.eclipse.gef.palette.PaletteContainer;
+import org.eclipse.gef.palette.PaletteSeparator;
+import org.eclipse.gef.palette.ToolEntry;
 import org.eclipse.gef.ui.actions.ActionRegistry;
 import org.eclipse.gef.ui.actions.GEFActionConstants;
+import org.eclipse.gef.ui.palette.PaletteViewer;
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.Separator;
 import org.eclipse.ui.actions.ActionFactory;
 
 public class BigraphEditorContextMenuProvider extends ContextMenuProvider {
@@ -39,6 +45,36 @@ public class BigraphEditorContextMenuProvider extends ContextMenuProvider {
 		
 		menu.appendToGroup(GEFActionConstants.GROUP_EDIT,
 				getActionRegistry().getAction(ActionFactory.PROPERTIES.getId()));
+		
+		final PaletteViewer pv = getViewer().getEditDomain().getPaletteViewer();
+		for (Object i :
+			pv.getPaletteRoot().getChildren()) {
+			if (i instanceof PaletteContainer) {
+				PaletteContainer pc = (PaletteContainer)i;
+				
+				Action menuAction = new Action() {
+				};
+				menuAction.setEnabled(false);
+				menuAction.setText(pc.getLabel());
+				menu.appendToGroup(GEFActionConstants.GROUP_REST, menuAction);
+				
+				for (Object j : pc.getChildren()) {
+					if (j instanceof ToolEntry) {
+						final ToolEntry k = (ToolEntry)j;
+						Action toolAction = new Action() {
+							@Override
+							public void run() {
+								pv.setActiveTool(k);
+							}
+						};
+						toolAction.setText(k.getLabel());
+						menu.appendToGroup(GEFActionConstants.GROUP_REST, toolAction);
+					}
+				}
+			} else if (i instanceof PaletteSeparator) {
+				menu.appendToGroup(GEFActionConstants.GROUP_REST, new Separator());
+			}
+		}
 	}
 
 	private ActionRegistry getActionRegistry() {
