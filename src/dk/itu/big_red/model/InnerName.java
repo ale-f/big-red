8616fa0634322df081d54d467c2b6f8ac0d8908f
Alec Faithfull <alef@itu.dk>
2010-07-23 14:40:39 +0200

    [InnerName] Added an INameable implementation
    Since Ports don't have such a strong uniqueness guarantee on their names,
    this implementation has to be here and not in the Point base class. I might
    think about changing that later

--- src/dk/itu/big_red/model/InnerName.java
+++ src/dk/itu/big_red/model/InnerName.java
@@ -1,6 +1,10 @@
 package dk.itu.big_red.model;
 
-public class InnerName extends Point {
+import dk.itu.big_red.model.interfaces.ILayoutable;
+import dk.itu.big_red.model.interfaces.INameable;
+
+
+public class InnerName extends Point implements INameable {
 	@Override
 	public InnerName clone() {
 		System.out.println("! Clone?");
@@ -12,7 +16,30 @@ public class InnerName extends Point {
 	}
 	
 	@Override
+	public void setParent(ILayoutable p) {
+		super.setParent(p);
+		getBigraph().getNamespace().newName(getClass(), this);
+	}
+	
+	@Override
 	public Bigraph getBigraph() {
 		return getParent().getBigraph();
+	}
+	
+	public String getName() {
+		return getBigraph().getNamespace().getName(getClass(), this);
+	}
+	
+	public void setName(String name) {
+		NamespaceManager nm = getBigraph().getNamespace();
+		String oldName = nm.getName(getClass(), this);
+		if (name != null) {
+			if (nm.setName(getClass(), name, this))
+				listeners.firePropertyChange(PROPERTY_NAME, oldName, name);
+		} else {
+			String newName = nm.newName(getClass(), this);
+			if (!newName.equals(oldName))
+				listeners.firePropertyChange(PROPERTY_NAME, oldName, newName);
+		}
 	}
 }
