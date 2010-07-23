d75fc0b443567b9bd8ab0b1961116801e408d841
Alec Faithfull <alef@itu.dk>
2010-07-23 15:25:16 +0200

    [BigraphXMLExport] Changed to use Edge.getName instead of hashCode
    This commit also fleshes out the <edge /> element a bit by adding a name
    attribute

--- src/dk/itu/big_red/model/import_export/BigraphXMLExport.java
+++ src/dk/itu/big_red/model/import_export/BigraphXMLExport.java
@@ -6,6 +6,7 @@ import org.w3c.dom.Element;
 
 import dk.itu.big_red.exceptions.ExportFailedException;
 import dk.itu.big_red.model.Bigraph;
+import dk.itu.big_red.model.Edge;
 import dk.itu.big_red.model.EdgeConnection;
 import dk.itu.big_red.model.InnerName;
 import dk.itu.big_red.model.Node;
@@ -63,9 +64,16 @@ public class BigraphXMLExport extends ModelExport<Bigraph> {
 				doc.createElement(p.getClass().getSimpleName().toLowerCase());
 			DOM.applyAttributesToElement(e,
 					"name", p.getName(),
-					"link", Integer.toString(connection.getParent().hashCode(), 16));
+					"link", connection.getParent().getName());
 			return e;
 		} else return null;
+	}
+	
+	private Element process(Edge e) {
+		Element el = doc.createElement("edge");
+		DOM.applyAttributesToElement(el,
+				"name", e.getName());
+		return el;
 	}
 	
 	private Element process(ILayoutable obj) {
@@ -76,6 +84,8 @@ public class BigraphXMLExport extends ModelExport<Bigraph> {
 			e = process((Node)obj);
 		} else if (obj instanceof InnerName) {
 			e = process((Point)obj);
+		} else if (obj instanceof Edge) {
+			e = process((Edge)obj);
 		} else {
 			e = doc.createElement(obj.getClass().getSimpleName().toLowerCase());
 		}
