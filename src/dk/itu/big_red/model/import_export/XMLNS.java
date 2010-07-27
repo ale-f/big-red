93c10de577a7ccf21ef9ccc1f17cb8651aad420c
Alec Faithfull <alef@itu.dk>
2010-07-27 15:35:42 +0200

    [XMLNS] Improved the Javadoc

--- src/dk/itu/big_red/model/import_export/XMLNS.java
+++ src/dk/itu/big_red/model/import_export/XMLNS.java
@@ -1,12 +1,26 @@
 package dk.itu.big_red.model.import_export;
 
+/**
+ * A collection of bigraph-related XML namespaces.
+ * @author alec
+ *
+ */
 public interface XMLNS {
+	/**
+	 * The XML namespace for {@code <bigraph>} documents.
+	 */
 	public static final String BIGRAPH =
 		"http://pls.itu.dk/bigraphs/2010/bigraph";
 	
+	/**
+	 * The XML namespace for {@code <signature>} documents.
+	 */
 	public static final String SIGNATURE =
 		"http://pls.itu.dk/bigraphs/2010/signature";
 	
+	/**
+	 * The XML namespace for Big Red's extensions to the other formats.
+	 */
 	public static final String BIG_RED =
 		"http://pls.itu.dk/bigraphs/2010/big-red";
 }
