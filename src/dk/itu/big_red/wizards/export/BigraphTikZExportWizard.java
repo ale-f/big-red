738477363307a7ed20adc2e078a260b329f64c6e
Alec Faithfull <alef@itu.dk>
2010-07-27 17:28:40 +0200

    [BigraphTikZExportWizard] Started using BigraphTikZExport in performFinish
    Exporting to TikZ works now; the only thing left to do is to make the TikZ
    itself work...

--- src/dk/itu/big_red/wizards/export/BigraphTikZExportWizard.java
+++ src/dk/itu/big_red/wizards/export/BigraphTikZExportWizard.java
@@ -5,6 +5,10 @@ import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.IExportWizard;
 import org.eclipse.ui.IWorkbench;
 
+import dk.itu.big_red.model.Bigraph;
+import dk.itu.big_red.model.import_export.BigraphTikZExport;
+import dk.itu.big_red.model.import_export.BigraphXMLImport;
+import dk.itu.big_red.util.Project;
 import dk.itu.big_red.wizards.export.assistants.WizardBigraphTikZExportPage;
 
 public class BigraphTikZExportWizard extends Wizard implements IExportWizard {
@@ -12,7 +16,28 @@ public class BigraphTikZExportWizard extends Wizard implements IExportWizard {
 	
 	@Override
 	public boolean performFinish() {
-		return false;
+		Bigraph model;
+		
+		BigraphXMLImport im = new BigraphXMLImport();
+		try {
+			im.setInputStream(Project.findFileByPath(null, page.getBigraphPath()).getContents());
+			model = im.importObject();
+		} catch (Exception e) {
+			page.setErrorMessage(e.getLocalizedMessage());
+			return false;
+		}
+		
+		BigraphTikZExport ex = new BigraphTikZExport();
+		try {
+			ex.setModel(model);
+			ex.setOutputFile(page.getTargetPath());
+			ex.exportObject();
+		} catch (Exception e) {
+			page.setErrorMessage(e.getLocalizedMessage());
+			return false;
+		}
+		
+		return true;
 	}
 	
 	@Override
