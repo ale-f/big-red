540abd6c14b94da5459f6642e0782d238830a0ae
Alec Faithfull <alef@itu.dk>
2010-07-30 16:55:50 +0200

    [OuterNamePart] Made OuterNamePart more like InnerNamePart

--- src/dk/itu/big_red/part/OuterNamePart.java
+++ src/dk/itu/big_red/part/OuterNamePart.java
@@ -1,5 +1,92 @@
 package dk.itu.big_red.part;
 
-public class OuterNamePart extends EdgePart {
+import java.beans.PropertyChangeEvent;
+import java.util.List;
 
+import org.eclipse.draw2d.ColorConstants;
+import org.eclipse.draw2d.ConnectionAnchor;
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.gef.ConnectionEditPart;
+import org.eclipse.gef.EditPolicy;
+import org.eclipse.gef.Request;
+
+import dk.itu.big_red.editpolicies.EdgeCreationPolicy;
+import dk.itu.big_red.editpolicies.ILayoutableDeletePolicy;
+import dk.itu.big_red.editpolicies.ILayoutableLayoutPolicy;
+import dk.itu.big_red.figure.OuterNameFigure;
+import dk.itu.big_red.figure.adornments.FixedPointAnchor;
+import dk.itu.big_red.figure.adornments.FixedPointAnchor.Orientation;
+import dk.itu.big_red.model.EdgeConnection;
+import dk.itu.big_red.model.InnerName;
+import dk.itu.big_red.model.OuterName;
+import dk.itu.big_red.model.interfaces.ICommentable;
+import dk.itu.big_red.model.interfaces.IConnectable;
+
+public class OuterNamePart extends AbstractPart {
+	@Override
+	public OuterName getModel() {
+		return (OuterName)super.getModel();
+	}
+	
+	@Override
+	protected IFigure createFigure() {
+		return new OuterNameFigure();
+	}
+
+	@Override
+	protected void createEditPolicies() {
+		installEditPolicy(EditPolicy.LAYOUT_ROLE, new ILayoutableLayoutPolicy());
+		installEditPolicy(EditPolicy.COMPONENT_ROLE, new ILayoutableDeletePolicy());
+		installEditPolicy(EditPolicy.GRAPHICAL_NODE_ROLE, new EdgeCreationPolicy());
+	}
+
+	@Override
+	public void propertyChange(PropertyChangeEvent evt) {
+		super.propertyChange(evt);
+		if (evt.getPropertyName().equals(InnerName.PROPERTY_NAME) ||
+			evt.getPropertyName().equals(ICommentable.PROPERTY_COMMENT)) {
+	    	refreshVisuals();
+	    } else if (evt.getPropertyName().equals(IConnectable.PROPERTY_SOURCE_EDGE)) {
+	    	refreshSourceConnections();
+	    }
+	}
+	
+	@Override
+	protected void refreshVisuals(){
+		super.refreshVisuals();
+		
+		OuterNameFigure figure = (OuterNameFigure)getFigure();
+		OuterName model = getModel();
+		
+		figure.setName(model.getName());
+		figure.setConstraint(model.getLayout());
+		
+		String toolTip = "Outer name (\"" + model.getName() + "\")";
+		if (model.getComment() != null)
+			toolTip += "\n\n" + model.getComment();
+		figure.setToolTip(toolTip);
+		
+		figure.setBackgroundColor(ColorConstants.red);
+	}
+	
+	@Override
+	protected List<EdgeConnection> getModelSourceConnections() {
+        return getModel().getConnections();
+    }
+	
+	public ConnectionAnchor getSourceConnectionAnchor(ConnectionEditPart connection) {
+		return new FixedPointAnchor(getFigure(), Orientation.SOUTH);
+    }
+    
+	public ConnectionAnchor getSourceConnectionAnchor(Request request) {
+		return new FixedPointAnchor(getFigure(), Orientation.SOUTH);
+    }
+	
+	public ConnectionAnchor getTargetConnectionAnchor(ConnectionEditPart connection) {
+		return new FixedPointAnchor(getFigure(), Orientation.SOUTH);
+    }
+    
+	public ConnectionAnchor getTargetConnectionAnchor(Request request) {
+		return new FixedPointAnchor(getFigure(), Orientation.SOUTH);
+    }
 }
