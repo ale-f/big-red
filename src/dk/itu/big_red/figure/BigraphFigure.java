eb1b7cfbf85384b223179379793c7a8c9bb1bfd1
Alec Faithfull <alef@itu.dk>
2010-07-30 12:57:08 +0200

    [BigraphFigure] Replaced the coloured boundary regions with simple lines
    The lines make the bigraph look much less cluttered

--- src/dk/itu/big_red/figure/BigraphFigure.java
+++ src/dk/itu/big_red/figure/BigraphFigure.java
@@ -1,9 +1,11 @@
 package dk.itu.big_red.figure;
 
-import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.XYLayout;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.swt.SWT;
+
+import dk.itu.big_red.util.Utility;
 
 public class BigraphFigure extends AbstractFigure {
 	protected int upperRootBoundary = Integer.MIN_VALUE,
@@ -18,29 +20,39 @@ public class BigraphFigure extends AbstractFigure {
 	
 	@Override
 	protected void fillShape(Graphics g) {
-		Rectangle r = start(g);
-		int width = r.width, height = r.height;
-		try {
-			g.setAlpha(63);
-			
-			g.setBackgroundColor(ColorConstants.red);
-			g.fillRectangle(0, 0, width, lowerOuterNameBoundary);
-			
-			g.setBackgroundColor(ColorConstants.green);
-			g.fillRectangle(0, upperRootBoundary, width, lowerRootBoundary - upperRootBoundary);
-			
-			g.setBackgroundColor(ColorConstants.blue);
-			g.fillRectangle(0, upperInnerNameBoundary, width, height - upperInnerNameBoundary);
-			
-		} finally {
-			stop(g);
-		}
+		// TODO Auto-generated method stub
+		
 	}
 
 	@Override
-	protected void outlineShape(Graphics graphics) {
-		// TODO Auto-generated method stub
-
+	protected void outlineShape(Graphics g) {
+		Rectangle r = start(g);
+		int width = r.width, height = r.height;
+		try {
+			g.setLineStyle(SWT.LINE_DASH);
+			
+			g.setFont(Utility.tweakFont(g.getFont(), 8, SWT.ITALIC));
+			
+			g.drawLine(0, lowerOuterNameBoundary, width, lowerOuterNameBoundary);
+			g.drawText("outer name boundary", 10, lowerOuterNameBoundary + 2);
+			
+			if (upperRootBoundary >= 0) {
+				g.drawLine(0, upperRootBoundary, width, upperRootBoundary);
+				g.drawText("upper root boundary", 10, upperRootBoundary + 2);
+			}
+			
+			g.drawLine(0, lowerRootBoundary, width, lowerRootBoundary);
+			g.drawText("lower root boundary", 10, lowerRootBoundary + 2);
+			
+			if (upperInnerNameBoundary >= 0) {
+				g.drawLine(0, upperInnerNameBoundary, width, upperInnerNameBoundary);
+				g.drawText("inner name boundary", 10, upperInnerNameBoundary + 2);
+			}
+			
+			g.getFont().dispose();
+		} finally {
+			stop(g);
+		}
 	}
 
 	public void setUpperRootBoundary(int upperRootBoundary) {
