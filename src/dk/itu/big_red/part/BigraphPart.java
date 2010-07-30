e4a7700390c28f672117e8c9a047663d80bc3ce2
Alec Faithfull <alef@itu.dk>
2010-07-30 12:59:42 +0200

    [BigraphPart] Forced the whole bigraph to be repainted more often
    GEF normally makes quite good decisions about this kind of thing, but
    updates to the boundaries affect the entire BigraphFigure

--- src/dk/itu/big_red/part/BigraphPart.java
+++ src/dk/itu/big_red/part/BigraphPart.java
@@ -46,5 +46,6 @@ public class BigraphPart extends ThingPart {
 		figure.setUpperRootBoundary(model.getUpperRootBoundary());
 		figure.setLowerRootBoundary(model.getLowerRootBoundary());
 		figure.setUpperInnerNameBoundary(model.getUpperInnerNameBoundary());
+		figure.repaint();
 	}
 }
