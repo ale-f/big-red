37eb3f6d50bcd485f5ebd6d7c49ba7cc2d45bf41
Alec Faithfull <alef@itu.dk>
2010-07-30 11:42:07 +0200

    [ILayoutableDeleteCommand] Added boundary updates

--- src/dk/itu/big_red/commands/ILayoutableDeleteCommand.java
+++ src/dk/itu/big_red/commands/ILayoutableDeleteCommand.java
@@ -2,15 +2,12 @@ package dk.itu.big_red.commands;
 
 import org.eclipse.gef.commands.Command;
 
+import dk.itu.big_red.model.Bigraph;
 import dk.itu.big_red.model.interfaces.ILayoutable;
 
 public class ILayoutableDeleteCommand extends Command {
 	private ILayoutable model = null;
 	private ILayoutable parentModel = null;
-	
-	public void execute() {
-		this.parentModel.removeChild(model);
-	}
 	
 	public void setModel(Object model) {
 		if (model instanceof ILayoutable)
@@ -22,7 +19,15 @@ public class ILayoutableDeleteCommand extends Command {
 			this.parentModel = (ILayoutable)model;
 	}
 	
+	public void execute() {
+		this.parentModel.removeChild(model);
+		if (parentModel instanceof Bigraph)
+			this.parentModel.getBigraph().updateBoundaries();
+	}
+	
 	public void undo() {
 		this.parentModel.addChild(model);
+		if (parentModel instanceof Bigraph)
+			this.parentModel.getBigraph().updateBoundaries();
 	}
 }
