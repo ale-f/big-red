b322f1eadd86617c78b2e8839cef0c46ed416ebc
Alec Faithfull <alef@itu.dk>
2010-08-03 11:02:09 +0200

    [NewAgentWizard] Made new bigraphs into actual bigraph XML documents
    This code is... not beautiful, certainly, but it gets the job done.
    Besides, this is the only place where bigraphs need to be created, so it
    probably doesn't make sense to put a lot of effort into making the process
    easier

--- src/dk/itu/big_red/wizards/creation/NewAgentWizard.java
+++ src/dk/itu/big_red/wizards/creation/NewAgentWizard.java
@@ -1,13 +1,19 @@
 package dk.itu.big_red.wizards.creation;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
 import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 
+import dk.itu.big_red.exceptions.ExportFailedException;
+import dk.itu.big_red.exceptions.ImportFailedException;
+import dk.itu.big_red.import_export.assistants.FileResourceOutputStream;
 import dk.itu.big_red.model.Bigraph;
+import dk.itu.big_red.model.import_export.BigraphXMLExport;
+import dk.itu.big_red.model.import_export.SignatureXMLImport;
 import dk.itu.big_red.util.Project;
 import dk.itu.big_red.util.UI;
 import dk.itu.big_red.wizards.creation.assistants.WizardNewAgentCreationPage;
@@ -27,9 +33,19 @@ public class NewAgentWizard extends Wizard implements INewWizard {
 			Project.findContainerByPath(null, page.getFolderPath());
 		if (c != null) {
 			try {
-				UI.openInEditor(Project.getFile(c, page.getFileName()));
+				IFile sigFile =
+					Project.findFileByPath(null, page.getSignaturePath());
+				IFile bigFile = Project.getFile(c, page.getFileName());
+				Bigraph b = new Bigraph();
+				b.setSignature(sigFile, new SignatureXMLImport().setInputStream(sigFile.getContents()).importObject());
+				new BigraphXMLExport().setModel(b).setOutputStream(new FileResourceOutputStream(bigFile)).exportObject();
+				UI.openInEditor(bigFile);
 				return true;
 			} catch (CoreException e) {
+				page.setErrorMessage(e.getLocalizedMessage());
+			} catch (ImportFailedException e) {
+				page.setErrorMessage(e.getLocalizedMessage());
+			} catch (ExportFailedException e) {
 				page.setErrorMessage(e.getLocalizedMessage());
 			}
 		}
