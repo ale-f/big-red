cf9e90ac708396958c2f9e8ac8f888f9328b2721
Alec Faithfull <alef@itu.dk>
2010-08-04 13:43:56 +0200

    [XMLNS] Updated the namespace names

--- src/dk/itu/big_red/model/import_export/XMLNS.java
+++ src/dk/itu/big_red/model/import_export/XMLNS.java
@@ -10,17 +10,17 @@ public interface XMLNS {
 	 * The XML namespace for {@code <bigraph>} documents.
 	 */
 	public static final String BIGRAPH =
-		"http://pls.itu.dk/bigraphs/2010/bigraph";
+		"http://www.itu.dk/research/pls/xmlns/2010/bigraph";
 	
 	/**
 	 * The XML namespace for {@code <signature>} documents.
 	 */
 	public static final String SIGNATURE =
-		"http://pls.itu.dk/bigraphs/2010/signature";
+		"http://www.itu.dk/research/pls/xmlns/2010/signature";
 	
 	/**
 	 * The XML namespace for Big Red's extensions to the other formats.
 	 */
 	public static final String BIG_RED =
-		"http://pls.itu.dk/bigraphs/2010/big-red";
+		"http://www.itu.dk/research/pls/xmlns/2010/big-red";
 }
