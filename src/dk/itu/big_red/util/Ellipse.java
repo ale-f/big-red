f28b650fac38385d1c44e52635f2920d66797c74
Alec Faithfull <alef@itu.dk>
2010-08-16 13:35:36 +0200

    [Ellipse] Added a new utility class for dealing with ellipse geometry

--- src/dk/itu/big_red/util/Ellipse.java
+++ src/dk/itu/big_red/util/Ellipse.java
@@ -22,15 +22,4 @@ public class Ellipse {
 		return new Point(centre.x + (a * Math.cos(t)),
 				centre.y + (b * Math.sin(t)));
 	}
-	
-	public double getOffsetFromPoint(Point p) {
-		Point centre = bb.getCenter();
-		double a = bb.width / 2, b = bb.height / 2,
-		       x = p.x - centre.x, y = p.y - centre.y;
-		/* x = a * Math.cos(t), y = b * Math.cos(t) */
-		x /= a; y /= b; /* x = Math.cos(t), y = Math.sin(t) */
-		x = Math.acos(x); y = Math.asin(y); /* x = t, y = t */
-		System.out.println(x + ", " + y);
-		return (x + y) / 2;
-	}
 }
