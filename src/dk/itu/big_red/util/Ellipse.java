dbc33b7b6f93c580e7fb1901926ffad17b763b29
Alec Faithfull <alef@itu.dk>
2010-08-20 13:43:42 +0200

    [Ellipse] Added a speculative implementation of getClosestOffset
    When I wrote this code in Lua, it worked; let's see what Java makes of it

--- src/dk/itu/big_red/util/Ellipse.java
+++ src/dk/itu/big_red/util/Ellipse.java
@@ -9,6 +9,7 @@ public class Ellipse {
 	public Ellipse() {
 	}
 	
+	
 	public Ellipse(Rectangle bb) {
 		this.bb.setBounds(bb);
 	}
@@ -21,5 +22,60 @@ public class Ellipse {
 		       b = bb.height / 2;
 		return new Point(centre.x + (a * Math.cos(t)),
 				centre.y + (b * Math.sin(t)));
+	}
+	
+	private static double sgn(double x) {
+		return (x < 0 ? -1 : 1);
+	}
+	
+	private static double sq(double x) {
+		return Math.pow(x, 2);
+	}
+	
+	/**
+	 * Returns the offset value specifying the point on this ellipse closest to
+	 * the given point.
+	 * @param p a Point
+	 * @return an offset
+	 */
+	public double getClosestOffset(Point p) {
+		Point el = bb.getTopLeft(),
+		      elc = bb.getCenter();
+		double elw = bb.width, elh = bb.height;
+		double xscale = 1, yscale = 1;
+		
+		if (bb.width > bb.height)
+			xscale = elh / elh;
+		else yscale = elw / elh;
+		
+		p.scale(xscale, yscale);
+		el.scale(xscale, yscale);
+		elc.scale(xscale, yscale);
+		elw *= xscale; elh *= yscale;
+		
+		Point tr = elc.getNegated();
+		p.translate(tr);
+		el.translate(tr);
+		elc.translate(tr);
+		
+		double dx = -p.x, dy = -p.y,
+		       dr2 = sq(dx) + sq(dy),
+		       r = bb.width / 2,
+		       sqrt = Math.sqrt(sq(r) * dr2),
+		       x0 = (sgn(dy) * dx * sqrt) / dr2,
+		       x1 = (-(sgn(dy) * dx * sqrt)) / dr2,
+		       y0 = (Math.abs(dy) * sqrt) / dr2,
+		       y1 = (-(Math.abs(dy) * sqrt)) / dr2;
+		
+		Point p0 = new Point(x0, y0),
+		      p1 = new Point(x1, y1);
+		
+		double d0 = p.getDistance(p0),
+		       d1 = p.getDistance(p1),
+		       t = (d0 < d1 ? Math.atan2(y0, x0) : Math.atan2(y1, x1));
+		
+		if (t < 0)
+			t += Math.PI * 2;
+		return t / (Math.PI * 2);
 	}
 }
