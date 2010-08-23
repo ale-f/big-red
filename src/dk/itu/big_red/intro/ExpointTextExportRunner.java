2b01ce92bbbb39e06779d231e38e9b2f3460167f
Alec Faithfull <alef@itu.dk>
2010-08-23 14:59:09 +0200

    [plugin.xml] Added an extension point for text-based model exporters
    This does nothing at the moment, but it does let third-party plugins define
    model exporters; once I work out how to create export wizards as a result,
    this extension point will become genuinely useful

---
+++ src/dk/itu/big_red/intro/ExpointTextExportRunner.java
@@ -0,0 +1,24 @@
+package dk.itu.big_red.intro;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.core.runtime.RegistryFactory;
+
+import dk.itu.big_red.model.import_export.ModelExport;
+
+public class ExpointTextExportRunner {
+	public static void registerExporters() {
+		IConfigurationElement exporters[] =
+			RegistryFactory.getRegistry().getConfigurationElementsFor("dk.itu.big_red.export.text");
+		try {
+			for (IConfigurationElement e : exporters) {
+				Object ex = e.createExecutableExtension("class");
+				if (ex instanceof ModelExport) {
+					System.out.println("Hooraj!");
+				}
+			}
+		} catch (CoreException e) {
+			return;
+		}
+	}
+}
