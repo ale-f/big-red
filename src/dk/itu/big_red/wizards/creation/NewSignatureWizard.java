f3d575564f6ffb691a3218c4e3b32a7c54579a64
Alec Faithfull <alef@itu.dk>
2010-08-25 11:36:40 +0200

    [NewSignatureWizard] Stopped generating empty signature XML documents
    NewBRSWizard doesn't use any of this new, clever code, so that problem
    should probably be addressed soon

--- src/dk/itu/big_red/wizards/creation/NewSignatureWizard.java
+++ src/dk/itu/big_red/wizards/creation/NewSignatureWizard.java
@@ -1,6 +1,7 @@
 package dk.itu.big_red.wizards.creation;
 
 import org.eclipse.core.resources.IContainer;
+import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.wizard.Wizard;
@@ -8,6 +9,10 @@ import org.eclipse.ui.INewWizard;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.dialogs.WizardNewFileCreationPage;
 
+import dk.itu.big_red.exceptions.ExportFailedException;
+import dk.itu.big_red.import_export.assistants.FileResourceOutputStream;
+import dk.itu.big_red.model.Signature;
+import dk.itu.big_red.model.import_export.SignatureXMLExport;
 import dk.itu.big_red.util.Project;
 import dk.itu.big_red.util.UI;
 
@@ -20,9 +25,13 @@ public class NewSignatureWizard extends Wizard implements INewWizard {
 			Project.findContainerByPath(null, page.getContainerFullPath());
 		if (c != null) {
 			try {
+				IFile sigFile = Project.getFile(c, page.getFileName());
+				new SignatureXMLExport().setModel(new Signature()).setOutputStream(new FileResourceOutputStream(sigFile)).exportObject();
 				UI.openInEditor(Project.getFile(c, page.getFileName()));
 				return true;
 			} catch (CoreException e) {
+				page.setErrorMessage(e.getLocalizedMessage());
+			} catch (ExportFailedException e) {
 				page.setErrorMessage(e.getLocalizedMessage());
 			}
 		}
