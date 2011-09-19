19a3ef03ef756858e5860a7edbf128c8d8a17772
Alec Faithfull <alexander.faithfull@gmail.com>
2011-09-19 14:13:40 +0100

    [LinkConnectionCreateCommand] Massively simplified
    This new version is much less horrible than all previous versions! As a
    bonus, it also fixes a bug that allowed more than one Link to appear to be
    connected to a Point

--- src/dk/itu/big_red/editors/bigraph/commands/LinkConnectionCreateCommand.java
+++ src/dk/itu/big_red/editors/bigraph/commands/LinkConnectionCreateCommand.java
@@ -2,78 +2,79 @@ package dk.itu.big_red.editors.bigraph.commands;
 
 import org.eclipse.gef.commands.Command;
 
+import dk.itu.big_red.model.Bigraph;
 import dk.itu.big_red.model.Edge;
 import dk.itu.big_red.model.Link;
 import dk.itu.big_red.model.LinkConnection;
 import dk.itu.big_red.model.Point;
 
 /**
- * A LinkConnectionCreateCommand 
+ * A LinkConnectionCreateCommand is in charge of creating and updating {@link
+ * Link}s on a {@link Bigraph} in response to user input. It can either join a
+ * {@link Point} to an existing {@link Link}, or join two {@link Point}s
+ * together, creating a new {@link Edge} in the process.
+ * </ul>
  * @author alec
  *
  */
 public class LinkConnectionCreateCommand extends Command {
-	private Object first = null, second = null;
-	private Link modifiedLink = null;
+	private Point point1 = null, point2 = null;
+	private Link link = null, modifiedLink = null;
 	
 	public LinkConnectionCreateCommand() {
 		super();
 	}
 
 	public void setFirst(Object e) {
-		if (!(e instanceof LinkConnection))
-			first = e;
-		else first = ((LinkConnection)e).getLink();
+		if (e instanceof Point) {
+			point1 = (Point)e;
+			if (point1.getLink() != null)
+				point1 = null;
+		} else if (e instanceof Link) {
+			link = (Link)e;
+		} else if (e instanceof LinkConnection) {
+			link = ((LinkConnection)e).getLink();
+		}
 	}
 	
 	public void setSecond(Object e) {
-		if (!(e instanceof LinkConnection))
-			second = e;
-		else second = ((LinkConnection)e).getLink();
+		if (e instanceof Point) {
+			point2 = (Point)e;
+			if (point2.getLink() != null)
+				point2 = null;
+		} else if (e instanceof Link) {
+			link = (Link)e;
+		} else if (e instanceof LinkConnection) {
+			link = ((LinkConnection)e).getLink();
+		}
 	}
 	
 	@Override
 	public boolean canExecute() {
-		return (first != null && second != null &&
-				first != second &&
-				((first instanceof Point && second instanceof Point) ||
-				 (first instanceof Point && second instanceof Link) ||
-				 (first instanceof Link && second instanceof Point)));
+		return (point1 != null && point2 != null) ||
+				(point1 != null && link != null) ||
+				(link != null && point2 != null);
 	}
 	
 	@Override
 	public void execute() {
-		if (modifiedLink != null) {
-			if (first instanceof Point)
-				modifiedLink.addPoint((Point)first);
-			if (second instanceof Point)
-				modifiedLink.addPoint((Point)second);
-		} else
-		/*
-		 * If either point2 or point1 is an EdgeTarget, then we can simply add
-		 * a new LinkConnection to the existing Edge.
-		 */
-		if (first instanceof Link) {
-			Link link = (Link)first;
-			link.addPoint((Point)second);
-			
-			modifiedLink = link;
-		} else if (second instanceof Link) {
-			Link link = (Link)second;
-			link.addPoint((Point)first);
-			
-			modifiedLink = link;
-		} else if (first instanceof Point && second instanceof Point) {
-			/*
-			 * Create a new Edge.
-			 */
+		if (modifiedLink != null) { /* redo */
+			modifiedLink.addPoint(point1);
+			modifiedLink.addPoint(point2);
+		} else if (point1 != null && point2 != null) {
 			Edge e = new Edge();
-			((Point)first).getBigraph().addChild(e);
-			e.addPoint((Point)second);
-			e.addPoint((Point)first);
+			point1.getBigraph().addChild(e);
+			e.addPoint(point1);
+			e.addPoint(point2);
 			e.averagePosition();
 			
 			modifiedLink = e;
+		} else if (point1 != null && link != null) {
+			modifiedLink = link;
+			link.addPoint(point1);
+		} else if (link != null && point2 != null) {
+			modifiedLink = link;
+			link.addPoint(point2);
 		}
 	}
 	
@@ -84,9 +85,7 @@ public class LinkConnectionCreateCommand extends Command {
 	
 	@Override
 	public void undo() {
-		if (first instanceof Point)
-			modifiedLink.removePoint((Point)first);
-		if (second instanceof Point)
-			modifiedLink.removePoint((Point)second);
+		modifiedLink.removePoint(point1);
+		modifiedLink.removePoint(point2);
 	}
 }
