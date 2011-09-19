0e47e28e52cb7a07a8aae69508b42caeb6f3787e
Alec Faithfull <alexander.faithfull@gmail.com>
2011-09-19 21:53:32 +0100

    [LayoutableDeleteCommand] Links should behave well when Containers containing Points are deleted
    Nodes can now be deleted and undeleted with impunity, and the Links their
    Ports are connected to are unaffected

--- src/dk/itu/big_red/editors/bigraph/commands/LayoutableDeleteCommand.java
+++ src/dk/itu/big_red/editors/bigraph/commands/LayoutableDeleteCommand.java
@@ -1,12 +1,15 @@
 package dk.itu.big_red.editors.bigraph.commands;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map.Entry;
 
 import org.eclipse.gef.commands.Command;
 
 import dk.itu.big_red.model.Container;
 import dk.itu.big_red.model.LayoutableModelObject;
 import dk.itu.big_red.model.Link;
+import dk.itu.big_red.model.Node;
 import dk.itu.big_red.model.Point;
 
 public class LayoutableDeleteCommand extends Command {
@@ -30,6 +33,27 @@ public class LayoutableDeleteCommand extends Command {
 	private Link link = null;
 	private ArrayList<Point> points = null;
 	
+	private HashMap<Point, Link> connections = null;
+	
+	private void iterativelyRemoveConnections(Container c) {
+		if (connections == null)
+			connections = new HashMap<Point, Link>();
+		if (c instanceof Node) {
+			Node j = (Node)c;
+			for (Point p : j.getPorts()) {
+				Link l = p.getLink();
+				if (l != null) {
+					connections.put(p, l);
+					l.removePoint(p);
+				}
+			}
+		}
+		for (LayoutableModelObject i : c.getChildren()) {
+			if (i instanceof Container)
+				iterativelyRemoveConnections((Container)i);
+		}
+	}
+	
 	@Override
 	public void execute() {
 		if (object instanceof Link) {
@@ -44,6 +68,8 @@ public class LayoutableDeleteCommand extends Command {
 			link = object.getLink();
 			if (link != null)
 				link.removePoint(object);
+		} else if (object instanceof Container) {
+			iterativelyRemoveConnections((Container)object);
 		}
 		parent.removeChild(object);
 	}
@@ -58,6 +84,10 @@ public class LayoutableDeleteCommand extends Command {
 			points.clear();
 		} else if (object instanceof Point && link != null) {
 			link.addPoint((Point)this.object);
+		} else if (object instanceof Container && connections != null) {
+			for (Entry<Point, Link> i : connections.entrySet())
+				i.getValue().addPoint(i.getKey());
+			connections.clear();
 		}
 	}
 	
