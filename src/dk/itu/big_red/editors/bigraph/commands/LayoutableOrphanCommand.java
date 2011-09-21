d7f96b798f6f401cc6404db94887a18db3027c34
Alec Faithfull <alexander.faithfull@gmail.com>
2011-09-21 12:39:34 +0100

    [LayoutableOrphanCommand] Reimplemented as a ChangeCommand

--- src/dk/itu/big_red/editors/bigraph/commands/LayoutableOrphanCommand.java
+++ src/dk/itu/big_red/editors/bigraph/commands/LayoutableOrphanCommand.java
@@ -1,44 +1,50 @@
 package dk.itu.big_red.editors.bigraph.commands;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import org.eclipse.gef.EditPart;
-import org.eclipse.gef.commands.Command;
-
-import dk.itu.big_red.editors.bigraph.parts.EdgePart;
+import dk.itu.big_red.editors.bigraph.parts.AbstractPart;
 import dk.itu.big_red.model.Container;
 import dk.itu.big_red.model.LayoutableModelObject;
+import dk.itu.big_red.model.changes.ChangeGroup;
+import dk.itu.big_red.model.changes.bigraph.BigraphChangeRemoveChild;
 
-public class LayoutableOrphanCommand extends Command {
+public class LayoutableOrphanCommand extends ChangeCommand {
+	private ChangeGroup cg = new ChangeGroup();
+	
+	public LayoutableOrphanCommand() {
+		setChange(cg);
+	}
+	
 	private Container parent = null;
-	private List children = null;
+	private ArrayList<LayoutableModelObject> children =
+			new ArrayList<LayoutableModelObject>();
 	
 	public void setParent(Object parent) {
 		if (parent instanceof Container)
 			this.parent = (Container)parent;
+		prepareGroup();
 	}
 	
+	@SuppressWarnings("rawtypes")
 	public void setChildren(Object children) {
-		if (children instanceof List)
-			this.children = (List)children;
+		if (children instanceof List) {
+			List list = (List)children;
+			this.children.clear();
+			for (Object i : list) {
+				if (i instanceof AbstractPart)
+					this.children.add(((AbstractPart)i).getModel());
+			}
+		}
+		prepareGroup();
 	}
 	
-	@Override
-	public boolean canExecute() {
-		return (parent != null && children != null);
-	}
-	
-	@Override
-	public void execute() {
-		for (Object i : children)
-			if (i instanceof EditPart && !(i instanceof EdgePart))
-				parent.removeChild((LayoutableModelObject)((EditPart)i).getModel());
-	}
-	
-	@Override
-	public void undo() {
-		for (Object i : children)
-			if (i instanceof EditPart && !(i instanceof EdgePart))
-				parent.addChild((LayoutableModelObject)((EditPart)i).getModel());
+	private void prepareGroup() {
+		cg.clear();
+		if (parent != null && children.size() != 0) {
+			setTarget(parent.getBigraph());
+			for (LayoutableModelObject i : children)
+				cg.add(new BigraphChangeRemoveChild(parent, i));
+		}
 	}
 }
