6afb665f35e85bc2718b76edbe6a5593b43d2ca4
Alec Faithfull <alexander.faithfull@gmail.com>
2011-09-21 11:36:15 +0100

    [RedPlugin] Removed some unhelpful automatically generated comments

--- src/dk/itu/big_red/application/plugin/RedPlugin.java
+++ src/dk/itu/big_red/application/plugin/RedPlugin.java
@@ -18,26 +18,16 @@ import org.osgi.framework.BundleContext;
  */
 public class RedPlugin extends AbstractUIPlugin {
 
-	// The plug-in ID
 	public static final String PLUGIN_ID = "dk.itu.big_red";
 
-	// The shared instance
 	private static RedPlugin plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
 	@Override
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
 	}
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
 	@Override
 	public void stop(BundleContext context) throws Exception {
 		plugin = null;
