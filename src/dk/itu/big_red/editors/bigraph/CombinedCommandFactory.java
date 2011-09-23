5d685637c300ec0085ab4762eb3aa4bad7ea7a97
Alec Faithfull <alexander.faithfull@gmail.com>
2011-09-23 11:35:49 +0100

    [CombinedCommandFactory] Added some Javadoc

--- src/dk/itu/big_red/editors/bigraph/CombinedCommandFactory.java
+++ src/dk/itu/big_red/editors/bigraph/CombinedCommandFactory.java
@@ -1,13 +1,40 @@
 package dk.itu.big_red.editors.bigraph;
 
+import org.eclipse.gef.EditPolicy;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.requests.GroupRequest;
 
+import dk.itu.big_red.editors.bigraph.commands.ChangeCommand;
 import dk.itu.big_red.editors.bigraph.commands.ModelDeleteCommand;
 import dk.itu.big_red.editors.bigraph.parts.AbstractPart;
 import dk.itu.big_red.editors.bigraph.parts.LinkConnectionPart;
+import dk.itu.big_red.model.Bigraph;
+import dk.itu.big_red.model.changes.IChangeable;
 
+/**
+ * {@link Bigraph}s are instances of {@link IChangeable}: they can validate
+ * changes being applied to them to make sure that the model remains
+ * consistent. This doesn't work well with the GEF's normal way of handling
+ * commands, though! A {@link GroupRequest} is normally satisfied by
+ * generating several commands, in no particular order: each of these might
+ * by itself pass validation, but the combination of them is likely to break
+ * the model (two {@link Command}s might both try to delete the same object;
+ * undoing them would mean that the same child is added twice, which isn't
+ * allowed).
+ * <p>This class, the <strong>CombinedCommandFactory</strong>, solves this
+ * problem: when EditPolicies defer to the static methods of this class, at
+ * most one {@link ChangeCommand} will be generated for each {@link
+ * GroupRequest}, so the validation works properly and the model is safe.
+ * @author alec
+ *
+ */
 public class CombinedCommandFactory {
+	/**
+	 * Creates the single {@link Command} for this {@link GroupRequest}.
+	 * @param r a {@link GroupRequest}
+	 * @return the {@link Command}, or <code>null</code> if it's already been
+	 * created by another {@link EditPolicy}
+	 */
 	@SuppressWarnings("unchecked")
 	public static Command createDeleteCommand(GroupRequest r) {
 		if (r.getExtendedData().containsKey(ModelDeleteCommand.GROUP_MAP_ID))
