a1e62177bec2439195fa9613a9cb0704ecc32997
Alec Faithfull <alexander.faithfull@gmail.com>
2011-09-26 23:53:54 +0100

    [BigraphRelayoutAction, BigraphRelayoutCommand] Reintroduced the relayout action and command
    Slightly less than forty-eight hours after it was deleted, the relayout
    command is back -- and less unhelpful than ever!

---
+++ src/dk/itu/big_red/editors/bigraph/actions/BigraphRelayoutAction.java
@@ -0,0 +1,64 @@
+package dk.itu.big_red.editors.bigraph.actions;
+
+import java.util.List;
+
+import org.eclipse.gef.commands.Command;
+import org.eclipse.gef.ui.actions.SelectionAction;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.ISharedImages;
+import org.eclipse.ui.IWorkbenchPart;
+import dk.itu.big_red.editors.bigraph.commands.BigraphRelayoutCommand;
+import dk.itu.big_red.editors.bigraph.parts.BigraphPart;
+import dk.itu.big_red.util.UI;
+
+public class BigraphRelayoutAction extends SelectionAction {
+	public static final String ID =
+			"dk.itu.big_red.editors.bigraph.actions.BigraphRelayoutAction";
+	
+	public BigraphRelayoutAction(IWorkbenchPart part) {
+		super(part);
+		setLazyEnablementCalculation(true);
+	}
+	
+	@Override
+	protected void init() {
+		setText("&Relayout");
+		setToolTipText("Relayout");
+		
+		setId(ID);
+		
+		ImageDescriptor icon =
+			UI.getImageDescriptor(ISharedImages.IMG_OBJ_ELEMENT);
+		if (icon != null)
+			setImageDescriptor(icon);
+		setEnabled(false);
+	}
+
+	@SuppressWarnings("rawtypes")
+	private Command createCopyCommand(List selectedObjects) {
+		if (selectedObjects == null || selectedObjects.isEmpty() ||
+				selectedObjects.size() != 1)
+			return null;
+		
+		Object i = selectedObjects.get(0);
+		if (i instanceof BigraphPart) {
+			BigraphRelayoutCommand cmd = new BigraphRelayoutCommand();
+			cmd.setBigraph(((BigraphPart)i).getModel());
+			cmd.prepare();
+			return cmd;
+		} else return null;
+	}
+	
+	@Override
+	protected boolean calculateEnabled() {
+		Command cmd = createCopyCommand(getSelectedObjects());
+		return cmd != null && cmd.canExecute();
+	}
+
+	@Override
+	public void run() {
+		Command cmd = createCopyCommand(getSelectedObjects());
+		if (cmd != null && cmd.canExecute())
+			execute(cmd);
+	}
+}
---
+++ src/dk/itu/big_red/editors/bigraph/commands/BigraphRelayoutCommand.java
@@ -0,0 +1,21 @@
+package dk.itu.big_red.editors.bigraph.commands;
+
+import dk.itu.big_red.model.Bigraph;
+
+public class BigraphRelayoutCommand extends ChangeCommand {
+	private Bigraph bigraph = null;
+	
+	public void setBigraph(Bigraph bigraph) {
+		if (bigraph != null)
+			this.bigraph = bigraph;
+	}
+	
+	@Override
+	public void prepare() {
+		if (bigraph != null) {
+			setTarget(bigraph);
+			setChange(bigraph.relayout());
+		}
+	}
+
+}
