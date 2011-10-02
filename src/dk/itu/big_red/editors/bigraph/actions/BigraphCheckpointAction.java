6e2c67c4e134bdf856b29851f4b11ca0805775db
Alec Faithfull <alexander.faithfull@gmail.com>
2011-10-02 17:48:44 +0100

    [BigraphCheckpointAction] Print some basic information about the checkpoint to the status line

--- src/dk/itu/big_red/editors/bigraph/actions/BigraphCheckpointAction.java
+++ src/dk/itu/big_red/editors/bigraph/actions/BigraphCheckpointAction.java
@@ -1,13 +1,17 @@
 package dk.itu.big_red.editors.bigraph.actions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.gef.ui.actions.SelectionAction;
+import org.eclipse.jface.action.IStatusLineManager;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.IWorkbenchPart;
 
 import dk.itu.big_red.editors.bigraph.parts.BigraphPart;
+import dk.itu.big_red.model.Bigraph;
+import dk.itu.big_red.model.changes.Change;
 import dk.itu.big_red.util.UI;
 
 public class BigraphCheckpointAction extends SelectionAction {
@@ -44,7 +48,19 @@ public class BigraphCheckpointAction extends SelectionAction {
 
 	@Override
 	public void run() {
-		System.out.println("Checkpoint");
-		System.out.println(((BigraphPart)getSelectedObjects().get(0)).getModel().checkpoint());
+		Bigraph b = ((BigraphPart)getSelectedObjects().get(0)).getModel();
+		ArrayList<Change> ch = b.checkpoint();
+		IStatusLineManager sl = UI.getActiveStatusLine();
+		ISharedImages si = UI.getSharedImages();
+		if (ch == null) {
+			sl.setMessage(si.getImage(ISharedImages.IMG_OBJS_INFO_TSK), "First checkpoint set");
+		} else if (ch.size() == 0) {
+			sl.setMessage(si.getImage(ISharedImages.IMG_OBJS_WARN_TSK), "No changes since last checkpoint");
+		} else {
+			String form = "change";
+			if (ch.size() > 1)
+				form += "s";
+			sl.setMessage(si.getImage(ISharedImages.IMG_OBJS_INFO_TSK), ch.size() + " " + form + " since last checkpoint");
+		}
 	}
 }
