a01660309491b5d5f50e4ad9e525d39f35f9ec14
Alec Faithfull <alexander.faithfull@gmail.com>
2011-10-28 22:00:35 +0100

    [LinkConnectionFigure] Massively improved the routing code
    Clicking LinkConnections always used to be really annoying; although they
    were displayed as curves, Eclipse thought of them as being straight lines.
    But no longer: incredibly, I've managed to implement quadratic-Bézier-curve
    routing. Me using maths to solve a problem! Imagine that

--- src/dk/itu/big_red/editors/bigraph/figures/LinkConnectionFigure.java
+++ src/dk/itu/big_red/editors/bigraph/figures/LinkConnectionFigure.java
@@ -1,39 +1,17 @@
 package dk.itu.big_red.editors.bigraph.figures;
 
-import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.MarginBorder;
 import org.eclipse.draw2d.PolylineConnection;
-import org.eclipse.draw2d.geometry.Dimension;
-import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.swt.SWT;
+
+import dk.itu.big_red.editors.bigraph.figures.assistants.CurvyConnectionRouter;
 
 public class LinkConnectionFigure extends PolylineConnection {
 	public LinkConnectionFigure() {
 		setAntialias(SWT.ON);
         setLineStyle(org.eclipse.swt.SWT.LINE_SOLID);
-	}
-	
-	@Override
-	public void outlineShape(Graphics g) {
-		g.pushState();
-		try {
-			Rectangle sr = getClientArea();
-			sr.height *= 2; sr.width *= 2;
-			Dimension d = getStart().getDifference(getEnd());
-			
-			if (d.height > 0)
-				sr.y -= getClientArea().height;
-			
-			if (d.width < 0)
-				sr.x -= getClientArea().width;
-			
-			g.setLineWidth(1);
-			sr.width -= 1; sr.height -= 1;
-			g.drawOval(sr);
-		} finally {
-			g.popState();
-		}
+        setConnectionRouter(new CurvyConnectionRouter());
 	}
 	
 	public void setToolTip(String content) {
