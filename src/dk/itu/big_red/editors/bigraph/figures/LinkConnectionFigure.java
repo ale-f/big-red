d97b78e10fdb8f8ff515b042eb4e99a7747730fc
Alec Faithfull <alexander.faithfull@gmail.com>
2011-10-29 00:29:14 +0100

    [LinkConnectionFigure] Use the advanced graphics system to render an actual Bézier curve rather than a polyline approximation
    Ta-da! This is the best of both worlds: the polyline gives good selection
    behaviour, and the advanced graphics system makes it look nice

--- src/dk/itu/big_red/editors/bigraph/figures/LinkConnectionFigure.java
+++ src/dk/itu/big_red/editors/bigraph/figures/LinkConnectionFigure.java
@@ -1,9 +1,12 @@
 package dk.itu.big_red.editors.bigraph.figures;
 
+import org.eclipse.draw2d.Graphics;
 import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.MarginBorder;
 import org.eclipse.draw2d.PolylineConnection;
+import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Path;
 
 import dk.itu.big_red.editors.bigraph.figures.assistants.CurvyConnectionRouter;
 
@@ -12,6 +15,22 @@ public class LinkConnectionFigure extends PolylineConnection {
 		setAntialias(SWT.ON);
         setLineStyle(org.eclipse.swt.SWT.LINE_SOLID);
         setConnectionRouter(new CurvyConnectionRouter());
+	}
+	
+	@Override
+	protected void outlineShape(Graphics g) {
+		g.pushState();
+		try {
+			Point start = getStart(),
+					end = getEnd();
+			
+			Path p = new Path(null);
+			p.moveTo(start.x(), start.y());
+			p.quadTo(end.x, start.y, end.x(), end.y());
+			g.drawPath(p);
+		} finally {
+			g.popState();
+		}
 	}
 	
 	public void setToolTip(String content) {
