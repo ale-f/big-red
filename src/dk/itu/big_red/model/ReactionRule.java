dcb02c87b5940eb82e9c16ba9305fc9d2eabebe7
Alec Faithfull <alexander.faithfull@gmail.com>
2011-10-31 18:05:22 +0100

    [ReactionRule] Added a new model object for reaction rules
    Again, this does nothing useful at the moment. Still, great oaks from tiny
    acorns grow, eh what?

---
+++ src/dk/itu/big_red/model/ReactionRule.java
@@ -0,0 +1,35 @@
+package dk.itu.big_red.model;
+
+import org.eclipse.core.resources.IFile;
+import dk.itu.big_red.model.changes.ChangeGroup;
+import dk.itu.big_red.util.resources.IFileBackable;
+
+public class ReactionRule extends ModelObject implements IFileBackable {
+	private Bigraph redex;
+	private ChangeGroup changes;
+	
+	public static final String PROPERTY_REDEX = "ReactionRuleRedex";
+	
+	public Bigraph getRedex() {
+		return redex;
+	}
+	
+	public void setRedex(Bigraph redex) {
+		Bigraph old = this.redex;
+		this.redex = redex;
+		firePropertyChange(PROPERTY_REDEX, old, redex);
+	}
+
+	protected IFile file = null;
+	
+	@Override
+	public IFile getFile() {
+		return file;
+	}
+
+	@Override
+	public ReactionRule setFile(IFile file) {
+		this.file = file;
+		return this;
+	}
+}
