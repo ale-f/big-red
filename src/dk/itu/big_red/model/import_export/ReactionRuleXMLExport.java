f7e74ce56cbd3f5988365c69eeb9047a7d779ece
Alec Faithfull <alexander.faithfull@gmail.com>
2011-12-08 14:06:30 +0100

    [ReactionRuleXMLExport] Started work on exporting changes
    This is all still very early and experimental, so don't expect to be able
    to build proper reaction rules yet -- amongst other gargantuan problems,
    there's still no way to reimport changes

--- src/dk/itu/big_red/model/import_export/ReactionRuleXMLExport.java
+++ src/dk/itu/big_red/model/import_export/ReactionRuleXMLExport.java
@@ -5,7 +5,15 @@ import org.w3c.dom.Element;
 import dk.itu.big_red.import_export.ExportFailedException;
 import dk.itu.big_red.import_export.XMLExport;
 import dk.itu.big_red.model.Bigraph;
+import dk.itu.big_red.model.Colourable;
+import dk.itu.big_red.model.Container;
+import dk.itu.big_red.model.Edge;
+import dk.itu.big_red.model.Layoutable;
 import dk.itu.big_red.model.ReactionRule;
+import dk.itu.big_red.model.Root;
+import dk.itu.big_red.model.assistants.BigraphScratchpad;
+import dk.itu.big_red.model.changes.Change;
+import dk.itu.big_red.model.changes.ChangeGroup;
 import dk.itu.big_red.util.DOM;
 
 public class ReactionRuleXMLExport extends XMLExport<ReactionRule> {
@@ -20,6 +28,8 @@ public class ReactionRuleXMLExport extends XMLExport<ReactionRule> {
 	public Element processRule(Element e, ReactionRule rr) throws ExportFailedException {
 		DOM.appendChildIfNotNull(e,
 			processRedex(newElement(XMLNS.RULE, "rule:redex"), rr.getRedex()));
+		DOM.appendChildIfNotNull(e,
+			processChanges(newElement(XMLNS.RULE, "rule:changes"), rr.getChanges()));
 		return e;
 	}
 	
@@ -30,5 +40,45 @@ public class ReactionRuleXMLExport extends XMLExport<ReactionRule> {
 		ex.setModel(redex);
 		ex.setDocument(getDocument());
 		return ex.processBigraph(e, ex.getModel());
+	}
+	
+	@SuppressWarnings("unchecked")
+	private static <T, V> T ac(V i) {
+		return (T)i;
+	}
+	
+	private Element processChanges(Element e, ChangeGroup changes) {
+		DOM.applyAttributes(e,
+				"xmlns:change", XMLNS.CHANGE);
+		
+		BigraphScratchpad scratch =
+				new BigraphScratchpad(getModel().getRedex());
+		
+		for (Change i_ : changes) {
+			Element f = null;
+			
+			if (i_ instanceof Colourable.ChangeColour ||
+					i_ instanceof Layoutable.ChangeLayout) {
+				continue;
+			} else if (i_ instanceof ChangeGroup) {
+				processChanges(e, (ChangeGroup)i_);
+			} else if (i_ instanceof Container.ChangeAddChild) {
+				Container.ChangeAddChild i = ac(i_);
+				if (i.child instanceof Root) {
+					f = DOM.applyAttributes(
+							newElement(XMLNS.CHANGE, "change:add-root"),
+							"name", i.name);
+				} else if (i.child instanceof Edge) {
+					f = DOM.applyAttributes(
+							newElement(XMLNS.CHANGE, "change:add-edge"),
+							"name", i.name);
+				} else throw new RuntimeException("Fatal");
+				scratch.addChildFor(i.parent, i.child, i.name);
+			} else throw new RuntimeException("Fatal");
+			
+			DOM.appendChildIfNotNull(e, f);
+		}
+		
+		return e;
 	}
 }
