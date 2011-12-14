0b96211fa6c8f918ff351350191be09d0e495a07
Alec Faithfull <alexander.faithfull@gmail.com>
2011-12-14 14:11:11 +0100

    [ReactionRuleXMLImport] Make a (very early) attempt to import a reaction rule's list of changes
    This might work! Eventually. For now, though, it does a bad job of
    supporting a very small number of changes

--- src/dk/itu/big_red/model/import_export/ReactionRuleXMLImport.java
+++ src/dk/itu/big_red/model/import_export/ReactionRuleXMLImport.java
@@ -4,11 +4,18 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import dk.itu.big_red.import_export.Import;
 import dk.itu.big_red.import_export.ImportFailedException;
 import dk.itu.big_red.model.Bigraph;
+import dk.itu.big_red.model.Edge;
+import dk.itu.big_red.model.InnerName;
+import dk.itu.big_red.model.OuterName;
 import dk.itu.big_red.model.ReactionRule;
+import dk.itu.big_red.model.Root;
+import dk.itu.big_red.model.changes.Change;
+import dk.itu.big_red.model.changes.ChangeRejectedException;
 import dk.itu.big_red.util.DOM;
 
 public class ReactionRuleXMLImport extends Import<ReactionRule> {
@@ -32,6 +39,7 @@ public class ReactionRuleXMLImport extends Import<ReactionRule> {
 		
 		rr.setRedex(
 			makeRedex(DOM.getNamedChildElement(e, XMLNS.RULE, "redex")));
+		updateReactum(rr, DOM.getNamedChildElement(e, XMLNS.RULE, "changes"));
 		
 		return rr;
 	}
@@ -39,6 +47,42 @@ public class ReactionRuleXMLImport extends Import<ReactionRule> {
 	private Bigraph makeRedex(Element e) throws ImportFailedException {
 		BigraphXMLImport im = new BigraphXMLImport();
 		return im.makeBigraph(e);
+	}
+	
+	private static String chattr(Element e, String name) {
+		return DOM.getAttributeNS(e, XMLNS.CHANGE, name);
+	}
+	
+	private void updateReactum(ReactionRule rr, Element e) throws ImportFailedException {
+		Bigraph reactum = rr.getReactum();
+		for (Node i : DOM.iterableChildren(e)) {
+			Change c = null;
+			if (i instanceof Element &&
+					i.getNamespaceURI().equals(XMLNS.CHANGE)) {
+				Element el = (Element)i;
+				if (el.getLocalName().equals("add-root")) {
+					c = reactum.changeAddChild(new Root(),
+							chattr(el, "name"));
+				} else if (el.getLocalName().equals("add-edge")) {
+					c = reactum.changeAddChild(new Edge(),
+							chattr(el, "name"));
+				} else if (el.getLocalName().equals("add-inner-name")) {
+					c = reactum.changeAddChild(new InnerName(),
+							chattr(el, "name"));
+				} else if (el.getLocalName().equals("add-outer-name")) {
+					c = reactum.changeAddChild(new OuterName(),
+							chattr(el, "name"));
+				}
+			}
+			if (c != null) {
+				try {
+					reactum.tryApplyChange(c);
+					rr.getChanges().add(c);
+				} catch (ChangeRejectedException cre) {
+					throw new ImportFailedException(cre);
+				}
+			}
+		}
 	}
 	
 	public static ReactionRule importFile(IFile file) throws ImportFailedException {
