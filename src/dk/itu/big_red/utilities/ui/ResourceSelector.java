c1f46bf424ce22023471d56c82d2cd478a6c8002
Alec Faithfull <alexander.faithfull@gmail.com>
2012-01-11 13:41:17 +0000

    [ResourceSelector] Added a resource change notification mechanism
    Despite the fact that this is marked as a public interface, I reserve the
    right to change or remove it at any time. Much like the rest of Big Red

--- src/dk/itu/big_red/utilities/ui/ResourceSelector.java
+++ src/dk/itu/big_red/utilities/ui/ResourceSelector.java
@@ -1,5 +1,8 @@
 package dk.itu.big_red.utilities.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.jface.dialogs.Dialog;
@@ -13,6 +16,10 @@ import dk.itu.big_red.utilities.resources.ResourceTreeSelectionDialog;
 import dk.itu.big_red.utilities.resources.ResourceTreeSelectionDialog.Mode;
 
 public class ResourceSelector {
+	public static interface ResourceListener {
+		public void resourceChanged(IResource newValue);
+	}
+	
 	private Button button;
 	private IProject project;
 	private Mode mode;
@@ -46,10 +53,29 @@ public class ResourceSelector {
 		if (status == Dialog.OK) {
 			resource = rtsd.getFirstResult();
 			button.setText(resource.getProjectRelativePath().toString());
+			for (ResourceListener l : getListeners())
+				l.resourceChanged(resource);
 		}
 	}
 	
 	public IResource getResource() {
 		return resource;
+	}
+	
+	private ArrayList<ResourceListener> listeners =
+		new ArrayList<ResourceListener>();
+	
+	public ResourceSelector addListener(ResourceListener l) {
+		listeners.add(l);
+		return this;
+	}
+	
+	public ResourceSelector removeListener(ResourceListener l) {
+		listeners.remove(l);
+		return this;
+	}
+	
+	public List<ResourceListener> getListeners() {
+		return listeners;
 	}
 }
