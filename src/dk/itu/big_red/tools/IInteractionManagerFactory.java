1c61d0801df96ae247929571e70074aee159b285
Alec Faithfull <alef@itu.dk>
2012-02-02 13:24:44 +0000

    [IInteractionManagerFactory] Improved the Javadoc

--- src/dk/itu/big_red/tools/IInteractionManagerFactory.java
+++ src/dk/itu/big_red/tools/IInteractionManagerFactory.java
@@ -1,7 +1,21 @@
 package dk.itu.big_red.tools;
 
+/**
+ * Classes implementing <strong>IInteractionManagerFactory</strong> can create
+ * a specific kind of {@link IInteractionManager}.
+ * @author alec
+ *
+ */
 public interface IInteractionManagerFactory {
+	/**
+	 * Returns the display name for this factory.
+	 * @return this factory's name
+	 */
 	public String getName();
 	
+	/**
+	 * Creates a new {@link IInteractionManager}.
+	 * @return a new {@link IInteractionManager}
+	 */
 	public IInteractionManager createInteractionManager();
 }
