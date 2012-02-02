5923ec586196f0e18cdbe49f9cb3556a4d2c049b
Alec Faithfull <alef@itu.dk>
2012-02-02 11:31:24 +0000

    [RedPlugin] Made the getResource method more useful
    A new overloaded variant of this method can now be used to get resources
    from arbitrary bundles

--- src/dk/itu/big_red/application/plugin/RedPlugin.java
+++ src/dk/itu/big_red/application/plugin/RedPlugin.java
@@ -16,6 +16,7 @@ import org.eclipse.core.runtime.RegistryFactory;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 
 /**
@@ -91,9 +92,12 @@ public class RedPlugin extends AbstractUIPlugin {
 	 * @return an InputStream, or <code>null</code> if the file wasn't found
 	 */
 	public static InputStream getResource(String path) {
+		return getResource(getInstance().getBundle(), path);
+	}
+	
+	public static InputStream getResource(Bundle bundle, String path) {
 		try {
-			URL u = FileLocator.find(
-					getInstance().getBundle(), new Path(path), null);
+			URL u = FileLocator.find(bundle, new Path(path), null);
 			if (u != null)
 				return u.openStream();
 			else return null;
