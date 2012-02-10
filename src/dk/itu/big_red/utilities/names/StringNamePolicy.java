b95b88bd63bce9b3891382a93e61a5e099e25a02
Alec Faithfull <alef@itu.dk>
2012-02-10 13:50:53 +0000

    [StringNamePolicy] Added a safety check to prevent overflows
    Regrettably, StringNamePolicy.getName can now produce only 308,915,776
    unique names. This is an unreasonable, burdensome restriction, I know

--- src/dk/itu/big_red/utilities/names/StringNamePolicy.java
+++ src/dk/itu/big_red/utilities/names/StringNamePolicy.java
@@ -10,9 +10,10 @@ public class StringNamePolicy implements INamePolicy {
 	
 	@Override
 	public String getName(int value) {
+		value = value % /* 26^6 = */ 308915776;
 		String s = "";
 		boolean nonZeroEncountered = false;
-		for (int i = 6; i >= 0; i--) {
+		for (int i = 5; i >= 0; i--) {
 			int y = (int)Math.pow(26, i);
 			int z = value / y;
 
