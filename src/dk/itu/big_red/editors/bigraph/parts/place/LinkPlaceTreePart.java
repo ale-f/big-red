2ad801c883d522f604d80e20a6ff51b02f5e51a4
Alec Faithfull <alef@itu.dk>
2012-02-15 13:08:19 +0000

    [LinkPlaceTreePart] Added a new place tree EditPart for links
    As OuterName and Edge are so similar, this will work for both... at least
    for now

---
+++ src/dk/itu/big_red/editors/bigraph/parts/place/LinkPlaceTreePart.java
@@ -0,0 +1,27 @@
+package dk.itu.big_red.editors.bigraph.parts.place;
+
+import org.eclipse.gef.EditPolicy;
+import org.eclipse.swt.graphics.Image;
+import org.eclipse.ui.ISharedImages;
+
+import dk.itu.big_red.editors.bigraph.LayoutableDeletePolicy;
+import dk.itu.big_red.editors.bigraph.parts.AbstractTreePart;
+import dk.itu.big_red.model.Link;
+import dk.itu.big_red.utilities.ui.UI;
+
+public class LinkPlaceTreePart extends AbstractTreePart {
+	@Override
+	protected void createEditPolicies() {
+		installEditPolicy(EditPolicy.COMPONENT_ROLE, new LayoutableDeletePolicy());
+	}
+	
+	@Override
+	public String getText() {
+		return ((Link)getModel()).getName();
+	}
+	
+	@Override
+	public Image getImage() {
+		return UI.getImage(ISharedImages.IMG_OBJ_ELEMENT);
+	}
+}
