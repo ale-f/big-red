79b68ef7dcef6644532324d49a3462a376667efd
Alec Faithfull <alef@itu.dk>
2012-02-15 13:55:01 +0000

    [BigraphLinkTreePart] Display all extant Links as children
    I'm... not sure where to go with the link tree from here, but this is a
    start!

--- src/dk/itu/big_red/editors/bigraph/parts/link/BigraphLinkTreePart.java
+++ src/dk/itu/big_red/editors/bigraph/parts/link/BigraphLinkTreePart.java
@@ -1,23 +1,25 @@
 package dk.itu.big_red.editors.bigraph.parts.link;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.ISharedImages;
 
 import dk.itu.big_red.editors.bigraph.parts.place.AbstractTreePart;
-import dk.itu.big_red.model.Container;
+import dk.itu.big_red.model.Bigraph;
+import dk.itu.big_red.model.Link;
+import dk.itu.big_red.utilities.Lists;
 import dk.itu.big_red.utilities.ui.UI;
 
 public class BigraphLinkTreePart extends AbstractTreePart {
 	@Override
-	protected List<Container> getModelChildren() {
-		/*
-		 * FIXME: notifications are only fired for *children* of the Bigraph
-		 * (i.e., Roots), not further descendants.
-		 */
-		return new ArrayList<Container>();
+	public Bigraph getModel() {
+		return (Bigraph)super.getModel();
+	}
+	
+	@Override
+	protected List<Link> getModelChildren() {
+		return Lists.only(getModel().getChildren(), Link.class);
 	}
 	
 	@Override
