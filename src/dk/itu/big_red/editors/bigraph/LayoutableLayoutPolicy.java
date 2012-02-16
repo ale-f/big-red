2f58a95338a05e1e2175398a3c3a2d6c9b325d54
Alec Faithfull <alef@itu.dk>
2012-02-16 15:59:36 +0000

    [LayoutableReparentCommand] Unified the orphan and add commands
    Changes work much better when there's only one command to worry about; to
    this end, the new LayoutableReparentCommand takes care of both orphaning
    and adding. And it generates all the appropriate Changes, too, so now
    reparenting should work properly in reaction rules

--- src/dk/itu/big_red/editors/bigraph/LayoutableLayoutPolicy.java
+++ src/dk/itu/big_red/editors/bigraph/LayoutableLayoutPolicy.java
@@ -3,23 +3,18 @@ package dk.itu.big_red.editors.bigraph;
 import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.EditPart;
-import org.eclipse.gef.Request;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.editpolicies.XYLayoutEditPolicy;
 import org.eclipse.gef.requests.ChangeBoundsRequest;
 import org.eclipse.gef.requests.CreateRequest;
-import org.eclipse.gef.requests.GroupRequest;
-
-import dk.itu.big_red.editors.bigraph.commands.LayoutableAddCommand;
 import dk.itu.big_red.editors.bigraph.commands.LayoutableCreateCommand;
-import dk.itu.big_red.editors.bigraph.commands.LayoutableOrphanCommand;
 import dk.itu.big_red.editors.bigraph.commands.LayoutableRelayoutCommand;
+import dk.itu.big_red.editors.bigraph.commands.LayoutableReparentCommand;
 import dk.itu.big_red.editors.bigraph.parts.BigraphPart;
 import dk.itu.big_red.model.Container;
 import dk.itu.big_red.model.Layoutable;
 
 public class LayoutableLayoutPolicy extends XYLayoutEditPolicy {
-	
 	@Override
 	protected Command createChangeConstraintCommand(
 			ChangeBoundsRequest cbr, EditPart child, Object constraint) {
@@ -37,30 +32,16 @@ public class LayoutableLayoutPolicy extends XYLayoutEditPolicy {
 	@Override
 	protected Command createAddCommand(
 			ChangeBoundsRequest cbr, EditPart child, Object constraint) {
-		LayoutableAddCommand command = new LayoutableAddCommand();
-		command.setParent(getHost().getModel());
-		command.setChild(child.getModel());
-		command.setConstraint(constraint);
-		command.prepare();
-		return command;
+		return createReparentCommand(cbr, child, constraint);
 	}
 	
-	@Override
-	protected Command getOrphanChildrenCommand(Request request) {
-		LayoutableOrphanCommand command = null;
-		if (request.getType() == REQ_ORPHAN_CHILDREN && request instanceof GroupRequest) {
-			GroupRequest groupRequest = (GroupRequest)request;
-			command = new LayoutableOrphanCommand();
-			
-			Object model =
-				((EditPart)groupRequest.getEditParts().get(0)).getModel();
-			if (model instanceof Layoutable)
-				command.setParent(((Layoutable)model).getParent());
-			
-			command.setChildren(groupRequest.getEditParts());
-			command.prepare();
-		}
-		return command;
+	protected Command createReparentCommand(
+			ChangeBoundsRequest cbr, EditPart child, Object constraint) {
+		LayoutableReparentCommand cmd = new LayoutableReparentCommand();
+		cmd.setChild(child.getModel());
+		cmd.setParent(getHost().getModel());
+		cmd.setConstraint(constraint);
+		return cmd.prepare();
 	}
 	
 	@Override
---
+++ src/dk/itu/big_red/editors/bigraph/commands/LayoutableReparentCommand.java
@@ -0,0 +1,81 @@
+package dk.itu.big_red.editors.bigraph.commands;
+
+import dk.itu.big_red.model.Container;
+import dk.itu.big_red.model.Layoutable;
+import dk.itu.big_red.model.Link;
+import dk.itu.big_red.model.Node;
+import dk.itu.big_red.model.Port;
+import dk.itu.big_red.model.changes.ChangeGroup;
+import dk.itu.big_red.utilities.geometry.Rectangle;
+
+public class LayoutableReparentCommand extends ChangeCommand {
+	private ChangeGroup cg = new ChangeGroup();
+	
+	public LayoutableReparentCommand() {
+		setChange(cg);
+	}
+	
+	@Override
+	public ChangeGroup getChange() {
+		return (ChangeGroup)super.getChange();
+	}
+	
+	private Container parent = null;
+	private Layoutable child = null;
+	private Rectangle constraint = null;
+	
+	public void setParent(Object parent) {
+		if (parent instanceof Container)
+			this.parent = (Container)parent;
+	}
+	
+	public void setChild(Object child) {
+		if (child instanceof Layoutable)
+			this.child = (Layoutable)child;
+	}
+	
+	public void setConstraint(Object constraint) {
+		if (constraint instanceof Rectangle)
+			this.constraint = (Rectangle)constraint;
+		else if (constraint instanceof org.eclipse.draw2d.geometry.Rectangle)
+			this.constraint = new Rectangle((org.eclipse.draw2d.geometry.Rectangle)constraint);
+	}
+	
+	private ChangeGroup post = new ChangeGroup();
+	
+	private void remove(Layoutable l, boolean root) {
+		if (l instanceof Container)
+			for (Layoutable k : ((Container)l).getChildren())
+				remove(k, false);
+		
+		if (l instanceof Node) {
+			for (Port p : ((Node)l).getPorts()) {
+				Link li = p.getLink();
+				if (li != null) {
+					cg.add(p.changeDisconnect(li));
+					post.prepend(p.changeConnect(li));
+				}
+			}
+		}
+		
+		cg.add(l.getParent().changeRemoveChild(l));
+		if (!root)
+			post.prepend(l.getParent().changeAddChild(l, l.getName()));
+	}
+	
+	@Override
+	public LayoutableReparentCommand prepare() {
+		cg.clear(); post.clear();
+		if (parent == null || child == null || constraint == null)
+			return this;
+		setTarget(parent.getBigraph());
+		
+		remove(child, true);
+		
+		cg.add(parent.changeAddChild(child, child.getName()));
+		cg.add(child.changeLayout(constraint));
+		cg.add(post);
+		
+		return this;
+	}
+}
