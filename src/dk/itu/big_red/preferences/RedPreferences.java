8238df8f7fe12025e097070b2c87962198e51369
Alec Faithfull <alef@itu.dk>
2012-02-20 15:19:09 +0000

    [RedPreferences] Simplified the implementation of the joinString method
    joinString was the last user of the IterableWrapper class, so this commit
    also deletes that

--- src/dk/itu/big_red/preferences/RedPreferences.java
+++ src/dk/itu/big_red/preferences/RedPreferences.java
@@ -1,12 +1,9 @@
 package dk.itu.big_red.preferences;
 
-import java.util.Iterator;
-
 import org.eclipse.core.runtime.preferences.AbstractPreferenceInitializer;
 import org.eclipse.jface.preference.IPreferenceStore;
 
 import dk.itu.big_red.application.plugin.RedPlugin;
-import dk.itu.big_red.utilities.IterableWrapper;
 
 public class RedPreferences extends AbstractPreferenceInitializer {
 	public static final String PREFERENCE_EXTERNAL_TOOLS =
@@ -36,12 +33,11 @@ public class RedPreferences extends AbstractPreferenceInitializer {
 	}
 	
 	static String joinString(String[] items) {
-		Iterator<String> it =
-			IterableWrapper.createArrayIterator(items);
 		String result = "";
-		while (it.hasNext()) {
-			result += it.next().toString();
-			if (it.hasNext())
+		int i = 0;
+		while (i < items.length) {
+			result += items[i++];
+			if (i < items.length)
 				result += ":";
 		}
 		return result;
