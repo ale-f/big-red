ee43a30e2cb70b786e5224b472d6b5b91e7031b6
Alec Faithfull <alef@itu.dk>
2012-02-21 16:00:21 +0000

    [INamespace] Improved the Javadoc

--- src/dk/itu/big_red/utilities/names/INamespace.java
+++ src/dk/itu/big_red/utilities/names/INamespace.java
@@ -8,9 +8,37 @@ package dk.itu.big_red.utilities.names;
  * @see INamePolicy
  */
 public interface INamespace<T> {
+	/**
+	 * Indicates whether or not the given name is valid and in use.
+	 * @param name a name
+	 * @return <code>true</code> if the name is valid and in use, or
+	 * <code>false</code> otherwise
+	 */
 	public boolean has(String name);
+	
+	/**
+	 * Returns the object in this {@link INamespace} with the given name, if
+	 * there is one.
+	 * @param name a name, which must be valid and in use
+	 * @return an object, or <code>null</code>
+	 */
 	public T get(String name);
+	
+	/**
+	 * Adds an object to this {@link INamespace} with the given name.
+	 * @param name a name, which must be valid and not already in use
+	 * @param value an object, which must not be <code>null</code>
+	 * @return <code>true</code> if the object was added, or <code>false</code>
+	 * otherwise
+	 */
 	public boolean put(String name, T value);
+	
+	/**
+	 * Removes a mapping from this {@link INamespace}.
+	 * @param name a name, which must be valid and in use
+	 * @return <code>true</code> if the mapping was removed, or
+	 * <code>false</code> otherwise
+	 */
 	public boolean remove(String name);
 
 	/**
