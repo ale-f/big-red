122ae77cb5b6ac4ce3ebd8fe44bb5b036957c9d1
Alec Faithfull <alef@itu.dk>
2012-02-23 13:01:37 +0000

    [TotalReadStrategy] Tweaked LineReadStrategy to read everything from an InputStream
    LineReadStrategy still exists and still works, of course, but now it's
    based on this new TotalReadStrategy class

--- src/dk/itu/big_red/utilities/io/LineReadStrategy.java
+++ src/dk/itu/big_red/utilities/io/LineReadStrategy.java
@@ -1,35 +1,8 @@
 package dk.itu.big_red.utilities.io;
 
-import java.io.IOException;
-import java.io.InputStream;
-
-public class LineReadStrategy implements IReadStrategy {
+public class LineReadStrategy extends TotalReadStrategy {
 	@Override
-	public byte[] read(InputStream is) throws IOException {
-		int length = 0, allocated = 1;
-		byte[] buffer = new byte[1],
-				i = new byte[1],
-				tmp;
-		
-		while (is.read(i) != -1) {
-			byte b = i[0];
-			
-			if (length == allocated) {
-				allocated *= 2;
-				tmp = new byte[allocated];
-				System.arraycopy(buffer, 0, tmp, 0, length);
-				buffer = tmp;
-			}
-			
-			buffer[length++] = b;
-			if (b == '\n')
-				break;
-		}
-		
-		if (length > 0) {
-			tmp = new byte[length];
-			System.arraycopy(buffer, 0, tmp, 0, length);
-			return tmp;
-		} else return null;
+	protected boolean shouldBreak(byte b) {
+		return (b == '\n');
 	}
 }
---
+++ src/dk/itu/big_red/utilities/io/TotalReadStrategy.java
@@ -0,0 +1,39 @@
+package dk.itu.big_red.utilities.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class TotalReadStrategy implements IReadStrategy {
+	protected boolean shouldBreak(byte b) {
+		return false;
+	}
+	
+	@Override
+	public byte[] read(InputStream is) throws IOException {
+		int length = 0, allocated = 1;
+		byte[] buffer = new byte[1],
+				i = new byte[1],
+				tmp;
+		
+		while (is.read(i) != -1) {
+			byte b = i[0];
+			
+			if (length == allocated) {
+				allocated *= 2;
+				tmp = new byte[allocated];
+				System.arraycopy(buffer, 0, tmp, 0, length);
+				buffer = tmp;
+			}
+			
+			buffer[length++] = b;
+			if (shouldBreak(b))
+				break;
+		}
+		
+		if (length > 0) {
+			tmp = new byte[length];
+			System.arraycopy(buffer, 0, tmp, 0, length);
+			return tmp;
+		} else return null;
+	}
+}
