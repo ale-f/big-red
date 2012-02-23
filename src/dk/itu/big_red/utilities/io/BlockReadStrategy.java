6bd7a925b1ab0dcc8c2419f246596a151ad7a1e2
Alec Faithfull <alef@itu.dk>
2012-02-23 11:14:05 +0000

    [BlockReadStrategy] Added an implementation of IReadStrategy which provides the current behaviour

---
+++ src/dk/itu/big_red/utilities/io/BlockReadStrategy.java
@@ -0,0 +1,25 @@
+package dk.itu.big_red.utilities.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+public class BlockReadStrategy implements IReadStrategy {
+	private int maxSize;
+	
+	public BlockReadStrategy() {
+		maxSize = 128;
+	}
+	
+	public BlockReadStrategy(int maxSize) {
+		this.maxSize = Math.max(1, maxSize);
+	}
+	
+	@Override
+	public byte[] read(InputStream is) throws IOException {
+		byte[] buffer = new byte[maxSize];
+		int r = is.read(buffer);
+		return (r != -1 ?
+				(r == maxSize ? buffer : Arrays.copyOf(buffer, r)) : null);
+	}
+}
