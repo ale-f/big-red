1d4a7719d6a4427e20f30377c15dbfe6f66c8a27
Alec Faithfull <alef@itu.dk>
2012-02-23 12:53:12 +0000

    [IReadStrategy] Added some Javadoc

--- src/dk/itu/big_red/utilities/io/IReadStrategy.java
+++ src/dk/itu/big_red/utilities/io/IReadStrategy.java
@@ -3,6 +3,18 @@ package dk.itu.big_red.utilities.io;
 import java.io.IOException;
 import java.io.InputStream;
 
+/**
+ * Classes implementing <strong>IReadStrategy</strong> have a strategy for
+ * reading data from an {@link InputStream}.
+ * @author alec
+ */
 public interface IReadStrategy {
+	/**
+	 * Reads some data from the given {@link InputStream}.
+	 * @param is an {@link InputStream}
+	 * @return an array of bytes, exactly as long as the number of bytes read
+	 * from the stream
+	 * @throws IOException if an I/O error occurs
+	 */
 	public byte[] read(InputStream is) throws IOException;
 }
