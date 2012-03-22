ab7c3456636d9521b33f706b59c373823ea979eb
Alec Faithfull <alexander.faithfull@gmail.com>
2012-03-22 13:35:51 +0000

    [AbstractNonGEFEditor] Added a new base class for the editors not built on GEF
    This class contains nothing but a Change-based implementation of
    IRedoImplementor and IUndoImplementor; while SimulationSpecEditor can be
    ported immediately, SignatureEditor will take some more work

---
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/editors/AbstractNonGEFEditor.java
@@ -0,0 +1,91 @@
+package dk.itu.big_red.editors;
+
+import java.util.ArrayDeque;
+import dk.itu.big_red.model.changes.Change;
+import dk.itu.big_red.model.changes.ChangeRejectedException;
+
+public abstract class AbstractNonGEFEditor extends AbstractEditor {
+	private boolean dirty = false;
+	private Change savePoint = null;
+	private ArrayDeque<Change>
+			undoBuffer = new ArrayDeque<Change>(),
+			redoBuffer = new ArrayDeque<Change>();
+	
+	protected Change getSavePoint() {
+		return savePoint;
+	}
+	
+	protected void setSavePoint() {
+		savePoint = undoBuffer.peek();
+		checkDirt();
+	}
+	
+	@Override
+	public boolean canUndo() {
+		return (undoBuffer.size() != 0);
+	}
+	
+	@Override
+	public boolean canRedo() {
+		return (redoBuffer.size() != 0);
+	}
+	
+	protected abstract void tryApplyChange(Change c)
+			throws ChangeRejectedException;
+	
+	protected void doChange(Change c) {
+		try {
+			tryApplyChange(c);
+			redoBuffer.clear();
+			undoBuffer.push(c);
+		} catch (ChangeRejectedException cre) {
+			cre.printStackTrace();
+			throw new Error("Unhandled Change application failure", cre);
+		}
+		checkDirt();
+		updateActions(getStateActions());
+	}
+	
+	@Override
+	public void undo() {
+		try {
+			if (!canUndo())
+				return;
+			Change c;
+			redoBuffer.push(c = undoBuffer.pop());
+			tryApplyChange(c.inverse());
+		} catch (ChangeRejectedException cre) {
+			throw new Error("Unhandled Change undo failure", cre);
+		}
+		checkDirt();
+		updateActions(getStateActions());
+	}
+	
+	@Override
+	public void redo() {
+		try {
+			if (!canRedo())
+				return;
+			Change c;
+			tryApplyChange(c = redoBuffer.pop());
+			undoBuffer.push(c);
+		} catch (ChangeRejectedException cre) {
+			throw new Error("Unhandled Change redo failure", cre);
+		}
+		checkDirt();
+		updateActions(getStateActions());
+	}
+	
+	@Override
+	public boolean isDirty() {
+		return dirty;
+	}
+	
+	private void checkDirt() {
+		boolean newDirty = (undoBuffer.peek() != getSavePoint());
+		if (newDirty != dirty) {
+			dirty = newDirty;
+			firePropertyChange(PROP_DIRTY);
+		}
+	}
+}
