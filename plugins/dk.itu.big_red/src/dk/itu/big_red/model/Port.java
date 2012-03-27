f7d00fa2b31c7bf867f2c59a5bb7d5620fe0648e
Alec Faithfull <alexander.faithfull@gmail.com>
2012-03-27 11:06:04 +0100

    [Port] Removed most of the redundant accessors
    Nothing apart from Port.getLayout() actually cares about the segment or
    distance values, and they can both be retrieved directly from the PortSpec.
    This commit also removes unnecessary behaviour from the overridden
    setLayout method

--- plugins/dk.itu.big_red/src/dk/itu/big_red/model/Port.java
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/model/Port.java
@@ -16,18 +16,6 @@ import dk.itu.big_red.model.interfaces.IPort;
  *
  */
 public class Port extends Point implements IPort {
-	/**
-	 * The property name fired when this Port's {@link #segment} changes. The
-	 * property values are {@link Integer}s.
-	 */
-	public static final String PROPERTY_SEGMENT = "PortSegment";
-	
-	/**
-	 * The property name fired when this Port's {@link #distance} changes. The
-	 * property values are {@link Double}s.
-	 */
-	public static final String PROPERTY_DISTANCE = "PortDistance";
-	
 	private PortSpec spec;
 	
 	private void setSpec(PortSpec spec) {
@@ -38,17 +26,12 @@ public class Port extends Point implements IPort {
 		return spec;
 	}
 	
-	public Port() {
-		setLayout(new Rectangle(5, 5, 10, 10));
-	}
-	
 	@Override
 	public void setLayout(Rectangle newLayout) {
-		super.setLayout(newLayout.setSize(10, 10));
+		/* do nothing */
 	}
 	
 	public Port(PortSpec i) {
-		setLayout(new Rectangle(5, 5, 10, 10));
 		setSpec(i);
 	}
 	
@@ -62,38 +45,22 @@ public class Port extends Point implements IPort {
 		return getSpec().getName();
 	}
 	
-	/**
-	 * Gets this Port's {@link #distance distance}.
-	 * @see #distance
-	 */
-	public double getDistance() {
-		return getSpec().getDistance();
-	}
-
-	/**
-	 * Gets this Port's {@link #segment segment}.
-	 * @see #segment
-	 * @return
-	 */
-	public int getSegment() {
-		return getSpec().getSegment();
-	}
-	
 	@Override
 	public Rectangle getLayout() {
-		Rectangle r = super.getLayout().getCopy();
+		Rectangle r = new Rectangle(0, 0, 10, 10);
 		PointList polypt = getParent().getFittedPolygon();
+		double distance = getSpec().getDistance();
 		if (polypt != null) {
-			int segment = getSegment();
+			int segment = getSpec().getSegment();
 			org.eclipse.draw2d.geometry.Point p1 = polypt.getPoint(segment),
 			      p2 = polypt.getPoint((segment + 1) % polypt.size());
 			r.setLocation(new Line(p1, p2).
-					getPointFromOffset(getDistance()).translate(-5, -5));
+					getPointFromOffset(distance).translate(-5, -5));
 		} else {
 			r.setLocation(
 				new Ellipse(
 						getParent().getLayout().getCopy().setLocation(0, 0)).
-					getPointFromOffset(getDistance()).translate(-5, -5));
+					getPointFromOffset(distance).translate(-5, -5));
 		}
 		return r;
 	}
@@ -110,14 +77,5 @@ public class Port extends Point implements IPort {
 	@Override
 	public Point clone() {
 		return null;
-	}
-	
-	@Override
-	public Object getProperty(String name) {
-		if (PROPERTY_DISTANCE.equals(name)) {
-			return getDistance();
-		} else if (PROPERTY_SEGMENT.equals(name)) {
-			return getSegment();
-		} else return super.getProperty(name);
 	}
 }
