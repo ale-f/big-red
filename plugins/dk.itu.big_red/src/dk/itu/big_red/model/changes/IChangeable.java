db08864ef0ca3a6a5daf0a61f2689e0ef6b14925
Alec Faithfull <alexander.faithfull@gmail.com>
2012-03-28 12:39:15 +0100

    [IChangeable] Improved the Javadoc
    This commit also adds a getChangeExecutor(IPropertyProviderProxy) method

--- plugins/dk.itu.big_red/src/dk/itu/big_red/model/changes/IChangeable.java
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/model/changes/IChangeable.java
@@ -1,5 +1,27 @@
 package dk.itu.big_red.model.changes;
 
+import dk.itu.big_red.model.assistants.IPropertyProviderProxy;
+
+/**
+ * Classes implementing <strong>IChangeable</strong> define {@link Change}s
+ * which can be used to modify them, and can indicate the {@link
+ * IChangeExecutor} that should be used to apply those changes.
+ * @author alec
+ *
+ */
 public interface IChangeable {
-	public IChangeExecutor getChangeExecutor();
+	/**
+	 * Returns the {@link IChangeExecutor} that can apply changes to this
+	 * object.
+	 * @return an {@link IChangeExecutor} (can be <code>null</code>)
+	 */
+	IChangeExecutor getChangeExecutor();
+	
+	/**
+	 * Returns the {@link IChangeExecutor} that could, in the given context,
+	 * apply changes to this object.
+	 * @param context a context
+	 * @return an {@link IChangeExecutor} (can be <code>null</code>)
+	 */
+	IChangeExecutor getChangeExecutor(IPropertyProviderProxy context);
 }
