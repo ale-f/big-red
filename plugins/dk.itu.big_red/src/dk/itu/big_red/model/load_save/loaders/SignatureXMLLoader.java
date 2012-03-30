ale-f/big-red

commit 01cabe77a73cf2ab40a60a78fcb9758c6af35f1e

[Control] Added a "name" property to ChangeAddPort
This is the same approach taken by Container.ChangeAddChild: requiring that
newly-added ports have names will make it much easier to avoid null values

plugins/dk.itu.big_red/src/dk/itu/big_red/model/load_save/loaders/SignatureXMLLoader.java
@@ -74,9 +74,10 @@ public class SignatureXMLLoader extends XMLLoader {
 		
 		for (Element j :
 			getNamedChildElements(e, SIGNATURE, "port")) {
+			String name = getAttributeNS(e, SIGNATURE, "name");
 			PortSpec i = makePortSpec(j, generatePolygon);
 			if (i != null)
-				cg.add(model.changeAddPort(i));
+				cg.add(model.changeAddPort(i, name));
 		}
 		
 		if (generatePolygon) {
@@ -113,8 +114,6 @@ public class SignatureXMLLoader extends XMLLoader {
 	
 	private PortSpec makePortSpec(Element e, boolean ignoreAppearanceData) {
 		PortSpec model = new PortSpec();
-		
-		cg.add(model.changeName(getAttributeNS(e, SIGNATURE, "name")));
 		
 		Element el = removeNamedChildElement(e, BIG_RED, "port-appearance");
 		if (el != null && !ignoreAppearanceData) {

plugins/dk.itu.big_red/src/dk/itu/big_red/editors/signature/SignatureEditorPolygonCanvas.java
@@ -256,8 +256,7 @@ MenuListener, PropertyChangeListener {
 	
 	private void opAddPort(PortSpec port, String name, int segment, double distance) {
 		ChangeGroup cg = new ChangeGroup();
-		cg.add(getModel().changeAddPort(port));
-		cg.add(port.changeName(name));
+		cg.add(getModel().changeAddPort(port, name));
 		cg.add(port.changeSegment(segment));
 		cg.add(port.changeDistance(distance));
 		doChange(cg);

plugins/dk.itu.big_red/src/dk/itu/big_red/model/Control.java
@@ -235,8 +235,10 @@ public class Control extends Colourable implements IControl {
 	}
 	
 	public class ChangeAddPort extends PortChange {
-		public ChangeAddPort(PortSpec port) {
+		public String name;
+		public ChangeAddPort(PortSpec port, String name) {
 			this.port = port;
+			this.name = name;
 		}
 		
 		@Override
@@ -255,9 +257,20 @@ public class Control extends Colourable implements IControl {
 			this.port = port;
 		}
 		
+		private String oldName;
+		@Override
+		public void beforeApply() {
+			oldName = getCreator().getName();
+		}
+		
+		@Override
+		public boolean canInvert() {
+			return (oldName != null);
+		}
+		
 		@Override
 		public ChangeAddPort inverse() {
-			return new ChangeAddPort(port);
+			return new ChangeAddPort(port, oldName);
 		}
 		
 		@Override
@@ -641,8 +654,8 @@ public class Control extends Colourable implements IControl {
 		return new ChangeKind(kind);
 	}
 	
-	public ChangeAddPort changeAddPort(PortSpec port) {
-		return new ChangeAddPort(port);
+	public ChangeAddPort changeAddPort(PortSpec port, String name) {
+		return new ChangeAddPort(port, name);
 	}
 	
 	public ChangeRemovePort changeRemovePort(PortSpec port) {

plugins/dk.itu.big_red/src/dk/itu/big_red/model/Signature.java
@@ -189,6 +189,7 @@ public class Signature extends ModelObject implements ISignature, IChangeExecuto
 		} else if (b instanceof ChangeAddPort) {
 			ChangeAddPort c = (ChangeAddPort)b;
 			c.getCreator().addPort(c.port);
+			c.port.setName(c.name);
 		} else if (b instanceof ChangeRemovePort) {
 			ChangeRemovePort c = (ChangeRemovePort)b;
 			c.getCreator().removePort(c.port.getName());

plugins/dk.itu.big_red/src/dk/itu/big_red/model/interfaces/SignatureBuilder.java
@@ -18,8 +18,8 @@ public class SignatureBuilder {
 	}
 	
 	public IPort newPort(IControl control, String name) {
-		PortSpec p = new PortSpec(name, 0, 0);
-		cg.add(((Control)control).changeAddPort(p));
+		PortSpec p = new PortSpec();
+		cg.add(((Control)control).changeAddPort(p, name));
 		return p;
 	}
 	

commit 82a1b0c007ad43fa59c93aadb9b9a79d5f2a1f35

[SignatureXMLLoader] Assign the correct names to PortSpecs
Commit 01cabe77a73cf2ab40a60a78fcb9758c6af35f1e broke PortSpec naming by
reading the "name" attribute from the wrong element, giving all PortSpecs
the name of their containing Control

plugins/dk.itu.big_red/src/dk/itu/big_red/model/load_save/loaders/SignatureXMLLoader.java
@@ -74,7 +74,7 @@ public class SignatureXMLLoader extends XMLLoader {
 		
 		for (Element j :
 			getNamedChildElements(e, SIGNATURE, "port")) {
-			String name = getAttributeNS(e, SIGNATURE, "name");
+			String name = getAttributeNS(j, SIGNATURE, "name");
 			PortSpec i = makePortSpec(j, generatePolygon);
 			if (i != null)
 				cg.add(model.changeAddPort(i, name));