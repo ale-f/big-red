a50abff22f0c593d1fb3e69f0571a3238b0ac023
Alec Faithfull <alexander.faithfull@gmail.com>
2012-04-03 13:19:33 +0100

    [SignatureScratchpad] Added a scratchpad for signature properties
    Like BigraphScratchpad, this is built upon the IPropertyProxyProvider and
    PropertyScratchpad infrastructure; not all of the properties are supported
    yet, though

---
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/model/assistants/SignatureScratchpad.java
@@ -0,0 +1,61 @@
+package dk.itu.big_red.model.assistants;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import dk.itu.big_red.model.Control;
+import dk.itu.big_red.model.PortSpec;
+import dk.itu.big_red.model.Signature;
+
+public class SignatureScratchpad extends PropertyScratchpad {
+	private Signature signature;
+	public SignatureScratchpad(Signature signature) {
+		this.signature = signature;
+	}
+	
+	public Signature getSignature() {
+		return signature;
+	}
+	
+	private List<Control> getModifiableControls() {
+		List<Control> c;
+		if (hasValue(getSignature(), Signature.PROPERTY_CONTROL)) {
+			c = getSignature().getControls(this);
+		} else {
+			setValue(getSignature(), Signature.PROPERTY_CONTROL,
+					c = new ArrayList<Control>(getSignature().getControls()));
+		}
+		return c;
+	}
+	
+	private List<PortSpec> getModifiablePorts(Control b) {
+		List<PortSpec> c;
+		if (hasValue(getSignature(), Control.PROPERTY_PORT)) {
+			c = b.getPorts(this);
+		} else {
+			setValue(getSignature(), Control.PROPERTY_PORT,
+					c = new ArrayList<PortSpec>(b.getPorts()));
+		}
+		return c;
+	}
+	
+	public void addControl(Control c) {
+		getModifiableControls().add(c);
+	}
+	
+	public void removeControl(Control c) {
+		getModifiableControls().remove(c);
+	}
+	
+	public void setNameFor(Control c, String name) {
+		setValue(c, Control.PROPERTY_NAME, name);
+	}
+	
+	public void addPortFor(Control c, PortSpec p) {
+		getModifiablePorts(c).add(p);
+	}
+	
+	public void removePortFor(Control c, PortSpec p) {
+		getModifiablePorts(c).remove(p);
+	}
+}
