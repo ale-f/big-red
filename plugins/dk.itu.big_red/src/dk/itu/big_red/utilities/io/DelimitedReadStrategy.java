bb9ffa0a90fc4e51b71555c10eccad4dbe00e733
Alec Faithfull <alexander.faithfull@gmail.com>
2012-04-26 10:27:06 +0100

    [DelimitedReadStrategy] Introduced a new base class for delimited reading
    It was always a bit odd that LineReadStrategy was a subclass of
    TotalReadStrategy; this new base class fixes that problem nicely

---
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/utilities/io/DelimitedReadStrategy.java
@@ -0,0 +1,33 @@
+package dk.itu.big_red.utilities.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+abstract class DelimitedReadStrategy implements IReadStrategy {
+	protected abstract boolean shouldBreak(byte b);
+	
+	@Override
+	public byte[] read(InputStream is) throws IOException {
+		int length = 0, allocated = 1;
+		byte[] buffer = new byte[1],
+				i = new byte[1];
+		
+		while (is.read(i) != -1) {
+			byte b = i[0];
+			
+			if (length == allocated) {
+				allocated *= 2;
+				buffer = Arrays.copyOf(buffer, allocated);
+			}
+			
+			buffer[length++] = b;
+			if (shouldBreak(b))
+				break;
+		}
+		
+		if (length > 0) {
+			return Arrays.copyOf(buffer, length);
+		} else return null;
+	}
+}
--- plugins/dk.itu.big_red/src/dk/itu/big_red/utilities/io/LineReadStrategy.java
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/utilities/io/LineReadStrategy.java
@@ -1,6 +1,6 @@
 package dk.itu.big_red.utilities.io;
 
-public class LineReadStrategy extends TotalReadStrategy {
+public class LineReadStrategy extends DelimitedReadStrategy {
 	@Override
 	protected boolean shouldBreak(byte b) {
 		return (b == '\n');
