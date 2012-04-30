078a3bd2cf205ec9fdddf929e5d2dc3ea80fd9d7
Alec Faithfull <alexander.faithfull@gmail.com>
2012-04-30 12:25:33 +0100

    [ExtendedDataUtilities] Added support for comments as extended data
    Comments are precisely the kind of thing that extended data should be used
    for

--- plugins/dk.itu.big_red/src/dk/itu/big_red/editors/assistants/ExtendedDataUtilities.java
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/editors/assistants/ExtendedDataUtilities.java
@@ -1,21 +1,39 @@
 package dk.itu.big_red.editors.assistants;
 
 import org.eclipse.core.resources.IFile;
-
 import dk.itu.big_red.model.ModelObject;
+import dk.itu.big_red.model.changes.Change;
 
 public final class ExtendedDataUtilities {
 	private ExtendedDataUtilities() {}
 	
-	private static final String ED_FILE =
-		"dk.itu.big_red.model.ModelObject.file";
+	private static Object require(Object o, Class<?> klass) {
+		return (klass.isInstance(o) ? o : null);
+	}
+	
+	public static final String FILE =
+			"dk.itu.big_red.model.ModelObject.file";
 	
 	public static IFile getFile(ModelObject m) {
-		Object o = m.getExtendedData(ED_FILE);
-		return (o instanceof IFile ? (IFile)o : null);
+		return (IFile)require(m.getExtendedData(FILE), IFile.class);
 	}
 	
 	public static void setFile(ModelObject m, IFile f) {
-		m.setExtendedData(ED_FILE, f);
+		m.setExtendedData(FILE, f);
+	}
+	
+	public static final String COMMENT =
+			"dk.itu.big_red.model.ModelObject.comment";
+	
+	public static String getComment(ModelObject m) {
+		return (String)require(m.getExtendedData(COMMENT), String.class);
+	}
+	
+	public static void setComment(ModelObject m, String s) {
+		m.setExtendedData(COMMENT, s);
+	}
+	
+	public static Change changeComment(ModelObject m, String s) {
+		return m.changeExtendedData(COMMENT, s);
 	}
 }
