46dea68dbd8d9139e6e746f803d2bb0a796ea491
Alec Faithfull <alexander.faithfull@gmail.com>
2012-05-07 15:09:17 +0100

    [RedResizableEditPart] Make sure that Edges and Ports can never be resized
    Although this should already be handled by existing code, apparently there
    are a few cases that occasionally slip through

--- plugins/dk.itu.big_red/src/dk/itu/big_red/editors/bigraph/RedResizableEditPolicy.java
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/editors/bigraph/RedResizableEditPolicy.java
@@ -1,6 +1,17 @@
 package dk.itu.big_red.editors.bigraph;
 
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.editpolicies.ResizableEditPolicy;
+import dk.itu.big_red.editors.bigraph.parts.EdgePart;
+import dk.itu.big_red.editors.bigraph.parts.PortPart;
 
 public class RedResizableEditPolicy extends ResizableEditPolicy {
+	@Override
+	public void setHost(EditPart host) {
+		super.setHost(host);
+		if (host instanceof EdgePart || host instanceof PortPart)
+			setResizeDirections(0);
+		if (host instanceof PortPart)
+			setDragAllowed(false);
+	}
 }
