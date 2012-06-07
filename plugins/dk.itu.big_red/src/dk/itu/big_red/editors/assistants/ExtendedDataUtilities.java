ale-f/big-red

commit f9636ddf98e9b615caeffab97088d6a024229ef1

[ExtendedDataUtilities] Enforce parameter constraints in the getter methods

plugins/dk.itu.big_red/src/dk/itu/big_red/editors/assistants/ExtendedDataUtilities.java
@@ -218,13 +218,14 @@ public final class ExtendedDataUtilities {
 	
 	public static String getParameter(
 			IPropertyProvider context, Node n) {
-		String s = (String)require(context, n, PARAMETER, String.class);
-		if (s == null) {
-			INamePolicy p = getParameterPolicy(context, n.getControl());
-			if (p != null)
-				set(context, n, PARAMETER, s = p.get(0));
-		}
-		return s;
+		INamePolicy p = getParameterPolicy(context, n.getControl());
+		String s = (String)require(context, n, PARAMETER, String.class),
+				t = null;
+		if (p != null)
+			t = p.normalise(s);
+		if (s != null ? !s.equals(t) : s != t)
+			set(context, n, PARAMETER, t);
+		return t;
 	}
 	
 	public static void setParameter(Node n, String s) {

commit 272360ff71a21ed1b69a0c17ed08229d316cfe79

[ExtendedDataUtilities] Assign default parameter values when necessary
Commit f9636ddf98e9b615caeffab97088d6a024229ef1 inadvertently removed this
functionality from the getParameter(IPropertyProvider, Node) method

plugins/dk.itu.big_red/src/dk/itu/big_red/editors/assistants/ExtendedDataUtilities.java
@@ -230,8 +230,11 @@ public final class ExtendedDataUtilities {
 		INamePolicy p = getParameterPolicy(context, n.getControl());
 		String s = (String)require(context, n, PARAMETER, String.class),
 				t = null;
-		if (p != null)
+		if (p != null) {
 			t = p.normalise(s);
+			if (t == null)
+				t = p.get(0);
+		}
 		if (s != null ? !s.equals(t) : s != t)
 			set(context, n, PARAMETER, t);
 		return t;