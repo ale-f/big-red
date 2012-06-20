dd5ddf8bffc94f486e291b12764c0a7f771bd468
zesster <zesster@gmail.com>
2012-06-20 17:18:27 +0200

    [Playground] progress on the edge-matching

--- plugins/org.bigraph.bigwb.playground/src/it/uniud/bigredit/model/MatchData.java
+++ plugins/org.bigraph.bigwb.playground/src/it/uniud/bigredit/model/MatchData.java
@@ -7,43 +7,41 @@ import org.bigraph.model.Link;
 import org.bigraph.model.ModelObject;
 import org.bigraph.uniud.bigraph.match.BidiMap;
 
-
 public class MatchData {
 
-	
 	/**
 	 * ModelObject in Redex, ModelObject in Agent
 	 */
-	private HashMap <ModelObject,ModelObject> map;
-	private BidiMap <Link, Link> mapLink;
+	private HashMap<ModelObject, ModelObject> map;
+	private BidiMap<Link, Link> mapLink;
 
-	public void addRootMatch(ModelObject rootR, ModelObject elementA){
+	public void addRootMatch(ModelObject rootR, ModelObject elementA) {
 		map.put(rootR, elementA);
 	}
-	
-	public HashMap <ModelObject,ModelObject> getMappingData(){
+
+	public HashMap<ModelObject, ModelObject> getMappingData() {
 		return map;
 	}
-	
-	public MatchData(){
-		this.map= new HashMap<ModelObject,ModelObject>();
-		this.mapLink= new BidiMap<Link, Link>();
+
+	public MatchData() {
+		this.map = new HashMap<ModelObject, ModelObject>();
+		this.mapLink = new BidiMap<Link, Link>();
 	}
-	
-	public ModelObject getRoot(){
-		for (Entry<ModelObject,ModelObject> m: map.entrySet()){
+
+	public ModelObject getRoot() {
+		for (Entry<ModelObject, ModelObject> m : map.entrySet()) {
 			return m.getKey();
 		}
-		
+
 		return null;
 	}
-	
-		public void setLinkMap(BidiMap<Link, Link> mapLink){
-				this.mapLink=mapLink;
-			}
-			
-			public BidiMap<Link, Link> getLinkMap(){
-				return this.mapLink;
-			}
-	
+
+	public void setLinkMap(BidiMap<Link, Link> mapLink) {
+		this.mapLink = mapLink;
+	}
+
+	public BidiMap<Link, Link> getLinkMap() {
+		return this.mapLink;
+	}
+
 }
