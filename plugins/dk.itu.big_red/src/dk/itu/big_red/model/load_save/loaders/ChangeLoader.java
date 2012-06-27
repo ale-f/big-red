145cca4c495779ee449a2241a79334a5014fac36
Alec Faithfull <alexander.faithfull@gmail.com>
2012-06-27 13:55:15 +0200

    [ChangeLoader] Added a new base class for Change-based loaders
    All of the existing loaders rely extensively on Changes, so this new class
    should make it possible to get rid of lots of redundant code

---
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/model/load_save/loaders/ChangeLoader.java
@@ -0,0 +1,30 @@
+package dk.itu.big_red.model.load_save.loaders;
+
+import org.bigraph.model.changes.Change;
+import org.bigraph.model.changes.ChangeGroup;
+import org.bigraph.model.changes.ChangeRejectedException;
+import org.bigraph.model.changes.IChangeExecutor;
+
+import dk.itu.big_red.model.load_save.LoadFailedException;
+import dk.itu.big_red.model.load_save.Loader;
+
+public abstract class ChangeLoader extends Loader {
+	private ChangeGroup cg = new ChangeGroup();
+	
+	protected void addChange(Change c) {
+		cg.add(c);
+	}
+	
+	protected ChangeGroup getChanges() {
+		return cg;
+	}
+	
+	protected void executeChanges(IChangeExecutor ex)
+			throws LoadFailedException {
+		try {
+			ex.tryApplyChange(getChanges());
+		} catch (ChangeRejectedException cre) {
+			throw new LoadFailedException(cre);
+		}
+	}
+}
