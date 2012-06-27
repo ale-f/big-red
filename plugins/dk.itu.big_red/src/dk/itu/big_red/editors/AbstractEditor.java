ale-f/big-red

commit 4276bc5c7dbf44448f81ea62593808dd2f511ce0

[Project] Use Eclipse WorkspaceJobs in the setContents method
This appears to be the recommended way of updating the Eclipse workspace.
As such, this commit also updates lots of bits of Big Red to use this
method

plugins/dk.itu.big_red/src/dk/itu/big_red/editors/AbstractEditor.java
@@ -11,7 +11,6 @@ import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
 import org.eclipse.core.resources.ResourcesPlugin;
-import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.gef.ui.actions.ActionRegistry;
 import org.eclipse.gef.ui.actions.UpdateAction;
@@ -37,6 +36,7 @@ import dk.itu.big_red.model.load_save.Loader;
 import dk.itu.big_red.model.load_save.SaveFailedException;
 import dk.itu.big_red.utilities.io.IOAdapter;
 import dk.itu.big_red.utilities.resources.Project;
+import dk.itu.big_red.utilities.resources.Project.SaveRunnable;
 
 public abstract class AbstractEditor extends EditorPart
 		implements IResourceChangeListener, IActionImplementor {
@@ -223,21 +223,34 @@ public abstract class AbstractEditor extends EditorPart
 	@Override
 	public void doSave(IProgressMonitor monitor) {
 		setSaving(true);
+		IOAdapter io = new IOAdapter();
+		FileEditorInput i = (FileEditorInput)getEditorInput();
 		try {
-			IOAdapter io = new IOAdapter();
-        	FileEditorInput i = (FileEditorInput)getEditorInput();
-        	
-        	doActualSave(i.getFile(), io.getOutputStream());
-        	
-        	Project.setContents(i.getFile(), io.getInputStream());
-    		firePropertyChange(PROP_DIRTY);
-		} catch (SaveFailedException cre) {
-			cre.printStackTrace();
-		} catch (CoreException e) {
-			e.printStackTrace();
-		} finally {
+			doActualSave(i.getFile(), io.getOutputStream());
+			
+			Project.setContents(
+					i.getFile(), io.getInputStream(), new SaveRunnable() {
+				@Override
+				public void onSuccess() {
+					firePropertyChange(PROP_DIRTY);
+				}
+				
+				@Override
+				public void always() {
+					setSaving(false);
+				}
+			});
+		} catch (SaveFailedException e) {
 			setSaving(false);
+			return;
 		}
+	}
+	
+	protected IFile getFile() {
+		IEditorInput i_ = getEditorInput();
+		if (i_ instanceof FileEditorInput) {
+			return ((FileEditorInput)i_).getFile();
+		} else return null;
 	}
 	
 	protected ModelObject loadInput() throws Throwable {

plugins/dk.itu.big_red/src/dk/itu/big_red/utilities/resources/Project.java
@@ -8,13 +8,20 @@ import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceRuleFactory;
+import org.eclipse.core.resources.IWorkspace;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.resources.WorkspaceJob;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
+import org.eclipse.core.runtime.Status;
 
 import dk.itu.big_red.utilities.io.IOAdapter;
+import dk.itu.big_red.utilities.ui.UI;
 
 /**
  * Utility functions for manipulating an Eclipse {@link IProject project} and
@@ -25,12 +32,20 @@ import dk.itu.big_red.utilities.io.IOAdapter;
 public final class Project {
 	private Project() {}
 	
+	public static IWorkspace getWorkspace() {
+		return ResourcesPlugin.getWorkspace();
+	}
+	
 	/**
 	 * Gets the workspace root.
 	 * @return the workspace root
 	 */
 	public static IWorkspaceRoot getWorkspaceRoot() {
-		return ResourcesPlugin.getWorkspace().getRoot();
+		return getWorkspace().getRoot();
+	}
+	
+	public static IResourceRuleFactory getRuleFactory() {
+		return getWorkspace().getRuleFactory();
 	}
 	
 	/**
@@ -108,17 +123,65 @@ public final class Project {
 		return (r instanceof IFile ? (IFile)r : null);
 	}
 	
+	public static class SaveRunnable {
+		public void onSuccess() {}
+		public void always() {}
+	}
+	
+	private static final class SaveJob extends WorkspaceJob {
+		private IFile file;
+		private InputStream contents;
+		private SaveRunnable payload;
+		
+		private SaveJob(IFile file, InputStream contents, SaveRunnable payload) {
+			super("Setting contents");
+			this.file = file;
+			this.contents = contents;
+			this.payload = payload;
+		}
+		
+		@Override
+		public IStatus runInWorkspace(IProgressMonitor monitor)
+				throws CoreException {
+			try {
+				if (file.exists()) {
+					file.setContents(contents, 0, monitor);
+				} else file.create(contents, 0, monitor);
+				
+				if (payload != null)
+					UI.asyncExec(new Runnable() {
+						@Override
+						public void run() {
+							payload.onSuccess();
+						}
+					});
+				return Status.OK_STATUS;
+			} finally {
+				if (payload != null)
+					UI.asyncExec(new Runnable() {
+						@Override
+						public void run() {
+							payload.always();
+						}
+					});
+			}
+		}
+	}
+	
 	/**
 	 * Sets the contents of the given {@link IFile} (which need not already
 	 * exist).
 	 * @param file an {@link IFile}
 	 * @param contents an {@link InputStream} specifying its contents
+	 * @param r a {@link Runnable} to be executed in the UI thread when the
+	 * operation has completed
 	 * @throws CoreException if the file couldn't be created or modified
 	 */
-	public static void setContents(IFile file, InputStream contents) throws CoreException {
-		if (file.exists()) {
-			file.setContents(contents, 0, null);
-		} else file.create(contents, 0, null);
+	public static void setContents(
+			IFile file, InputStream contents, SaveRunnable r) {
+		SaveJob j = new SaveJob(file, contents, r);
+		j.setRule(getRuleFactory().modifyRule(file));
+		j.schedule();
 	}
 	
 	public static IResourceDelta getSpecificDelta(IResourceDelta rootDelta, IResource r) {

plugins/dk.itu.big_red/src/dk/itu/big_red/wizards/creation/NewAgentWizard.java
@@ -59,14 +59,14 @@ public class NewAgentWizard extends Wizard implements INewWizard {
 		addPage(page);
 	}
 
-	protected static void createBigraph(IFile sigFile, IFile bigFile)
+	protected static void createBigraph(IFile sigFile, final IFile bigFile)
 			throws LoadFailedException, SaveFailedException, CoreException {
-		IOAdapter io = new IOAdapter();
+		final IOAdapter io = new IOAdapter();
 		Bigraph b = new Bigraph();
 		
 		b.setSignature((Signature)Loader.fromFile(sigFile));
 		new BigraphXMLSaver().setFile(bigFile).setModel(b).
 			setOutputStream(io.getOutputStream()).exportObject();
-		bigFile.setContents(io.getInputStream(), 0, null);
+		Project.setContents(bigFile, io.getInputStream(), null);
 	}
 }

plugins/dk.itu.big_red/src/dk/itu/big_red/wizards/creation/NewRuleWizard.java
@@ -67,6 +67,6 @@ public class NewRuleWizard extends Wizard implements INewWizard {
 		
 		new ReactionRuleXMLSaver().setModel(rr).setFile(rrFile).
 			setOutputStream(io.getOutputStream()).exportObject();
-		rrFile.setContents(io.getInputStream(), 0, null);
+		Project.setContents(rrFile, io.getInputStream(), null);
 	}
 }

plugins/dk.itu.big_red/src/dk/itu/big_red/wizards/creation/NewSignatureWizard.java
@@ -55,6 +55,6 @@ public class NewSignatureWizard extends Wizard implements INewWizard {
 		
 		new SignatureXMLSaver().setModel(new Signature()).setFile(sigFile).
 			setOutputStream(io.getOutputStream()).exportObject();
-		sigFile.setContents(io.getInputStream(), 0, null);
+		Project.setContents(sigFile, io.getInputStream(), null);
 	}
 }

plugins/dk.itu.big_red/src/dk/itu/big_red/wizards/creation/NewSimulationSpecWizard.java
@@ -56,6 +56,6 @@ public class NewSimulationSpecWizard extends Wizard implements INewWizard {
 		new SimulationSpecXMLSaver().
 			setModel(new SimulationSpec()).setFile(ssFile).
 			setOutputStream(io.getOutputStream()).exportObject();
-		ssFile.setContents(io.getInputStream(), 0, null);
+		Project.setContents(ssFile, io.getInputStream(), null);
 	}
 }

commit f36f92e30beac234016d479c1b871ef9aa2422c7

[AbstractEditor] Print a stack trace when saving fails
Commit 4276bc5c7dbf44448f81ea62593808dd2f511ce0 inadvertently removed this
useful debugging feature

plugins/dk.itu.big_red/src/dk/itu/big_red/editors/AbstractEditor.java
@@ -273,6 +273,7 @@ public abstract class AbstractEditor extends EditorPart
 				}
 			});
 		} catch (SaveFailedException e) {
+			e.printStackTrace();
 			setSaving(false);
 			return;
 		}