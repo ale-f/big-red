7b44835023550f0e2aef5de0cf8eca0cf22d7c6f
Alec Faithfull <alexander.faithfull@gmail.com>
2012-07-02 16:19:11 +0200

    [ChangeGroup] Added, and removed, some utility methods
    ChangeGroup now has methods which expose more array-like behaviour, and two
    of the unused variants of the add method have been removed

--- plugins/org.bigraph.model/src/org/bigraph/model/changes/ChangeGroup.java
+++ plugins/org.bigraph.model/src/org/bigraph/model/changes/ChangeGroup.java
@@ -1,8 +1,8 @@
 package org.bigraph.model.changes;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Iterator;
+import java.util.List;
 
 /**
  * A ChangeGroup is a composite of many {@link Change}s.
@@ -10,8 +10,7 @@ import java.util.Iterator;
  *
  */
 public class ChangeGroup extends Change implements Iterable<Change> {
-	private ArrayList<Change> changes =
-		new ArrayList<Change>();
+	private List<Change> changes = new ArrayList<Change>();
 	
 	/**
 	 * Adds a {@link Change} to the end of this ChangeGroup.
@@ -30,21 +29,21 @@ public class ChangeGroup extends Change implements Iterable<Change> {
 			this.changes.add(c);
 	}
 	
-	/**
-	 * Adds some {@link Change}s to the end of this ChangeGroup.
-	 * @param changes some {@link Change}s to add
-	 */
-	public void add(Collection<? extends Change> changes) {
-		this.changes.addAll(changes);
+	@Override
+	public ChangeGroup clone() {
+		ChangeGroup cg = new ChangeGroup();
+		cg.changes = new ArrayList<Change>(changes);
+		return cg;
 	}
 	
-	/**
-	 * Adds some {@link Change}s to the end of this ChangeGroup.
-	 * @param changes some {@link Change}s to add
-	 */
-	public void add(ChangeGroup changes) {
-		for (Change c : changes)
-			this.changes.add(c);
+	public Change head() {
+		return changes.get(0);
+	}
+	
+	public ChangeGroup tail() {
+		ChangeGroup cg = new ChangeGroup();
+		cg.changes = changes.subList(1, size());
+		return cg;
 	}
 	
 	/**
@@ -86,6 +85,14 @@ public class ChangeGroup extends Change implements Iterable<Change> {
 	@Override
 	public String toString() {
 		return changes.toString();
+	}
+	
+	public Change get(int position) {
+		return changes.get(position);
+	}
+	
+	public Change set(int position, Change c) {
+		return changes.set(position, c);
 	}
 	
 	/**
