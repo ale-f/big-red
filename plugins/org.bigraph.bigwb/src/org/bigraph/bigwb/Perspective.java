d53450dd8e3669c648d8d2286099e582011788b2
zesster <zesster@gmail.com>
2012-07-03 09:47:36 +0200

    [BIGWB] added org.bigraph.bigwb, added new plug-in feature for install/update

---
+++ plugins/org.bigraph.bigwb/src/org/bigraph/bigwb/Perspective.java
@@ -0,0 +1,10 @@
+package org.bigraph.bigwb;
+
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class Perspective implements IPerspectiveFactory {
+
+	public void createInitialLayout(IPageLayout layout) {
+	}
+}
