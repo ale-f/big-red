aafebf82e8a2b8c8ff60fa2e57eeaa92874b7ba0
zesster <zesster@gmail.com>
2012-07-05 16:42:45 +0200

    [BigWb] added splash screen, dependancy on core

--- plugins/org.bigraph.bigwb/src/org/bigraph/bigwb/ApplicationWorkbenchAdvisor.java
+++ plugins/org.bigraph.bigwb/src/org/bigraph/bigwb/ApplicationWorkbenchAdvisor.java
@@ -1,5 +1,9 @@
 package org.bigraph.bigwb;
 
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
@@ -14,5 +18,17 @@ public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
 	public String getInitialWindowPerspectiveId() {
 		return PERSPECTIVE_ID;
+	}
+
+    @Override
+	public void initialize(IWorkbenchConfigurer configurer) {
+        super.initialize(configurer);
+        org.eclipse.ui.ide.IDE.registerAdapters();
+    }
+	
+	@Override
+	public IAdaptable getDefaultPageInput() {
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		return workspace.getRoot();
 	}
 }
