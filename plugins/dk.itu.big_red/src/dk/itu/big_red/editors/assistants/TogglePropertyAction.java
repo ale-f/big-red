78503ebf30c84259c93e6e25c5cb123a9321033a
Alec Faithfull <alexander.faithfull@gmail.com>
2012-07-13 13:51:37 +0200

    [TogglePropertyAction] Added a generic action for viewer property toggling
    This will need some serious polishing, but it'll do for now

---
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/editors/assistants/TogglePropertyAction.java
@@ -0,0 +1,36 @@
+package dk.itu.big_red.editors.assistants;
+
+import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.jface.action.Action;
+
+public class TogglePropertyAction extends Action {
+	public static String getId(String property) {
+		return "TPA!" + property;
+	}
+	
+	private String property;
+	private boolean defaultValue;
+	private GraphicalViewer viewer;
+	
+	public TogglePropertyAction(
+			String property, boolean defaultValue, GraphicalViewer viewer) {
+		super(property, AS_CHECK_BOX);
+		this.viewer = viewer;
+		this.defaultValue = defaultValue;
+		this.property = property;
+		
+		setId(getId(property));
+		setChecked(isChecked());
+	}
+	
+	@Override
+	public boolean isChecked() {
+		Object o = viewer.getProperty(property);
+		return (o instanceof Boolean ? (Boolean)o : defaultValue);
+	}
+	
+	@Override
+	public void run() {
+		viewer.setProperty(property, Boolean.valueOf(!isChecked()));
+	}
+}
