22e2c74b6f54291a320e01a3137a9c027194baef
Alec Faithfull <alexander.faithfull@gmail.com>
2012-07-13 17:30:00 +0200

    [ChangeCreationException] Added an exception for use by IChangeDescriptor
    The IChangeDescriptor.createChange(PropertyScratchpad, Resolver) method
    will shortly begin throwing this new exception when things go wrong

---
+++ plugins/org.bigraph.model/src/org/bigraph/model/changes/descriptors/ChangeCreationException.java
@@ -0,0 +1,26 @@
+package org.bigraph.model.changes.descriptors;
+
+public class ChangeCreationException extends Exception {
+	private static final long serialVersionUID = 7744148841156557485L;
+	
+	private final IChangeDescriptor descriptor;
+	private final String rationale;
+	
+	public ChangeCreationException() {
+		this(null, null);
+	}
+	
+	public ChangeCreationException(
+			IChangeDescriptor descriptor, String rationale) {
+		this.descriptor = descriptor;
+		this.rationale = rationale;
+	}
+	
+	public IChangeDescriptor getChangeDescriptor() {
+		return descriptor;
+	}
+	
+	public String getRationale() {
+		return rationale;
+	}
+}
