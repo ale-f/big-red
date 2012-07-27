14bdcaa6560ec0adcea44000e83aace33cf2163c
Alec Faithfull <alexander.faithfull@gmail.com>
2012-07-27 16:35:44 +0200

    [RedPlugin] Register the file format schemas with XMLLoader on startup
    Naturally, this commit also causes the schemas to be unregistered upon
    shutdown

--- plugins/dk.itu.big_red/src/dk/itu/big_red/application/plugin/RedPlugin.java
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/application/plugin/RedPlugin.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 
+import org.bigraph.model.loaders.XMLLoader;
+import org.bigraph.model.resources.IOpenable;
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Path;
@@ -12,11 +14,34 @@ import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
+import dk.itu.big_red.model.load_save.IRedNamespaceConstants;
+
 /**
  * The RedPlugin class is responsible for starting and stopping the Big Red
  * plugin &mdash; and for keeping track of plugin-wide shared objects.
  */
 public class RedPlugin extends AbstractUIPlugin {
+	private static final class BundleOpenable implements IOpenable {
+		private final String path;
+		
+		public BundleOpenable(String path) {
+			this.path = path;
+		}
+		
+		@Override
+		public InputStream open() {
+			try {
+				URL u = FileLocator.find(
+						getInstance().getBundle(), new Path(path), null);
+				if (u != null)
+					return u.openStream();
+				else return null;
+			} catch (IOException e) {
+				return null;
+			}
+		}
+	}
+	
 	public static final String PLUGIN_ID = "dk.itu.big_red";
 	private static RedPlugin plugin;
 	
@@ -24,10 +49,24 @@ public class RedPlugin extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		
+		XMLLoader.registerSchema(IRedNamespaceConstants.SIGNATURE,
+				new BundleOpenable("resources/schema/signature.xsd"));
+		XMLLoader.registerSchema(IRedNamespaceConstants.BIGRAPH,
+				new BundleOpenable("resources/schema/bigraph.xsd"));
+		XMLLoader.registerSchema(IRedNamespaceConstants.RULE,
+				new BundleOpenable("resources/schema/rule.xsd"));
+		XMLLoader.registerSchema(IRedNamespaceConstants.SPEC,
+				new BundleOpenable("resources/schema/spec.xsd"));
 	}
 
 	@Override
 	public void stop(BundleContext context) throws Exception {
+		XMLLoader.unregisterSchema(IRedNamespaceConstants.SPEC);
+		XMLLoader.unregisterSchema(IRedNamespaceConstants.RULE);
+		XMLLoader.unregisterSchema(IRedNamespaceConstants.BIGRAPH);
+		XMLLoader.unregisterSchema(IRedNamespaceConstants.SIGNATURE);
+		
 		plugin = null;
 		super.stop(context);
 	}
@@ -57,15 +96,7 @@ public class RedPlugin extends AbstractUIPlugin {
 	 * @return an InputStream, or <code>null</code> if the file wasn't found
 	 */
 	public static InputStream getResource(String path) {
-		try {
-			URL u = FileLocator.find(
-					getInstance().getBundle(), new Path(path), null);
-			if (u != null)
-				return u.openStream();
-			else return null;
-		} catch (IOException e) {
-			return null;
-		}
+		return new BundleOpenable(path).open();
 	}
 	
 	/**
