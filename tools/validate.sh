54477ebd43394e47f8309aa5040cc4e079787ab5
Alec Faithfull <alexander.faithfull@gmail.com>
2012-07-30 16:51:50 +0200

    [edit.xsd] Added a (speculative) schema for change descriptors
    At the moment, Big Red provides no information at all about the contents of
    the rule:changes element; this new schema is an early attempt to improve
    that situation

--- tools/validate.sh
+++ tools/validate.sh
@@ -5,7 +5,7 @@ SELFDIR="`dirname "$SELF"`"
 SCHEMAPATH="$SELFDIR/../plugins/dk.itu.big_red/resources/schema"
 
 usage() {
-	echo "Usage: $0 [bigraph|signature|rule|spec] XML-DOCUMENT"
+	echo "Usage: $0 [bigraph|signature|rule|spec|edit] XML-DOCUMENT"
 	exit "$1"
 }
 
@@ -13,7 +13,7 @@ if [ "$1" != "" -a "$2" != "" ]; then
 	FILE="$1"
 	shift 1
 	case "$FILE" in
-	bigraph|signature|rule|spec)
+	bigraph|signature|rule|spec|edit)
 		FILE="$SCHEMAPATH/$FILE.xsd" ;;
 	*)
 		usage 1 ;;
