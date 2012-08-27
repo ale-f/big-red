5d56f8fb6c8e5572ec6a8365494ca86c7cba4c67
Alec Faithfull <alexander.faithfull@gmail.com>
2012-08-27 11:29:22 +0200

    [AliasSupport] Added a decorator and undecorator for site aliases
    I really don't want to move aliases into the core model plugin, and this
    seems like a good compromise for the time being

---
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/model/load_save/AliasSupport.java
@@ -0,0 +1,63 @@
+package dk.itu.big_red.model.load_save;
+
+import org.bigraph.model.ModelObject;
+import org.bigraph.model.Site;
+import org.bigraph.model.changes.IChangeExecutor;
+import org.bigraph.model.loaders.IXMLLoader;
+import org.bigraph.model.loaders.IXMLUndecorator;
+import org.bigraph.model.savers.IXMLDecorator;
+import org.w3c.dom.Element;
+
+import dk.itu.big_red.model.ExtendedDataUtilities;
+
+import static org.bigraph.model.loaders.XMLLoader.getAttributeNS;
+import static org.bigraph.model.loaders.RedNamespaceConstants.BIGRAPH;
+
+public abstract class AliasSupport {
+	private AliasSupport() {}
+	
+	public static final class Undecorator implements IXMLUndecorator {
+		private IXMLLoader loader;
+		
+		@Override
+		public void setLoader(IXMLLoader loader) {
+			this.loader = loader;
+		}
+	
+		@Override
+		public void undecorate(ModelObject object, Element el) {
+			if (object instanceof Site) {
+				String alias = getAttributeNS(el, BIGRAPH, "alias");
+				if (alias != null)
+					loader.addChange(
+							ExtendedDataUtilities.changeAlias((Site)object,
+							alias));
+			}
+		}
+
+		@Override
+		public void finish(IChangeExecutor ex) {
+		}
+
+		@Override
+		public IXMLUndecorator newInstance() {
+			return new Undecorator();
+		}
+	}
+	
+	public static final class Decorator implements IXMLDecorator {
+		@Override
+		public IXMLDecorator newInstance() {
+			return new Decorator();
+		}
+
+		@Override
+		public void decorate(ModelObject object, Element el) {
+			if (object instanceof Site) {
+				String alias = ExtendedDataUtilities.getAlias((Site)object);
+				if (alias != null)
+					el.setAttribute("alias", alias);
+			}
+		}
+	}
+}
