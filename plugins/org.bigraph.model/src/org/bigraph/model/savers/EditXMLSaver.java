a2147d69b58a5e04acd7d9e300473ce12dcb84ff
Alec Faithfull <alexander.faithfull@gmail.com>
2012-09-12 14:57:52 +0200

    [EditXMLSaver] Added a prototype of the saver participation interface

--- plugins/org.bigraph.model/src/org/bigraph/model/savers/EditXMLSaver.java
+++ plugins/org.bigraph.model/src/org/bigraph/model/savers/EditXMLSaver.java
@@ -2,6 +2,9 @@ package org.bigraph.model.savers;
 
 import static org.bigraph.model.loaders.RedNamespaceConstants.EDIT;
 
+import java.util.Collections;
+import java.util.List;
+
 import org.bigraph.model.Edit;
 import org.bigraph.model.ModelObject;
 import org.bigraph.model.changes.descriptors.ChangeDescriptorGroup;
@@ -10,6 +13,17 @@ import org.w3c.dom.Element;
 
 @Deprecated
 public class EditXMLSaver extends XMLSaver {
+	protected interface IParticipant {
+		Element processDescriptor(IChangeDescriptor cd);
+	}
+	
+	private List<IParticipant> participants;
+	
+	protected List<? extends IParticipant> getParticipants() {
+		return (participants != null ? participants :
+				Collections.<IParticipant>emptyList());
+	}
+	
 	@Override
 	public EditXMLSaver setModel(ModelObject model) {
 		if (model instanceof Edit)
@@ -22,6 +36,16 @@ public class EditXMLSaver extends XMLSaver {
 		return (Edit)super.getModel();
 	}
 	
+	protected Element processDescriptor(IChangeDescriptor cd) {
+		Element el = null;
+		for (IParticipant p : getParticipants()) {
+			el = p.processDescriptor(cd);
+			if (el != null)
+				break;
+		}
+		return el;
+	}
+	
 	protected Element processGroup(ChangeDescriptorGroup ed, Element e) {
 		Element ch;
 		for (IChangeDescriptor cd : ed) {
@@ -29,7 +53,7 @@ public class EditXMLSaver extends XMLSaver {
 			if (cd instanceof ChangeDescriptorGroup) {
 				ch = processGroup((ChangeDescriptorGroup)cd,
 						newElement(EDIT, "edit:edit"));
-			}
+			} else ch = processDescriptor(ed);
 			if (ch != null)
 				e.appendChild(ch);
 		}
