e1aad7033480df9d7d0aeabf6cb8da955d3bbd91
Alec Faithfull <alexander.faithfull@gmail.com>
2012-09-13 16:03:45 +0200

    [BigraphEditLoader] Increased the visibility of the ID-loading methods
    This commit also makes these methods static so that they can be used in the
    absence of a BigraphEditLoader instance

--- plugins/org.bigraph.model/src/org/bigraph/model/loaders/BigraphEditLoader.java
+++ plugins/org.bigraph.model/src/org/bigraph/model/loaders/BigraphEditLoader.java
@@ -12,6 +12,7 @@ import org.bigraph.model.Edge;
 import org.bigraph.model.InnerName;
 import org.bigraph.model.Layoutable;
 import org.bigraph.model.Link;
+import org.bigraph.model.ModelObject;
 import org.bigraph.model.OuterName;
 import org.bigraph.model.Point;
 import org.bigraph.model.Port;
@@ -22,40 +23,43 @@ import org.bigraph.model.loaders.EditXMLLoader.Participant;
 import org.w3c.dom.Element;
 
 public class BigraphEditLoader implements Participant {
-	private Root.Identifier getRootIdentifier(Element el) {
-		return new Root.Identifier(EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"));
+	private static Root.Identifier getRootIdentifier(Element el) {
+		return new Root.Identifier(
+				EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"));
 	}
 	
-	private Site.Identifier getSiteIdentifier(Element el) {
-		return new Site.Identifier(EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"));
+	private static Site.Identifier getSiteIdentifier(Element el) {
+		return new Site.Identifier(
+				EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"));
 	}
 	
-	private Edge.Identifier getEdgeIdentifier(Element el) {
-		return new Edge.Identifier(EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"));
+	private static Edge.Identifier getEdgeIdentifier(Element el) {
+		return new Edge.Identifier(
+				EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"));
 	}
 	
-	private InnerName.Identifier getInnerNameIdentifier(Element el) {
+	private static InnerName.Identifier getInnerNameIdentifier(Element el) {
 		return new InnerName.Identifier(
 				EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"));
 	}
 	
-	private OuterName.Identifier getOuterNameIdentifier(Element el) {
+	private static OuterName.Identifier getOuterNameIdentifier(Element el) {
 		return new OuterName.Identifier(
 				EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"));
 	}
 	
-	private Bigraph.Identifier getBigraphIdentifier() {
+	private static Bigraph.Identifier getBigraphIdentifier() {
 		return new Bigraph.Identifier();
 	}
 	
-	private Port.Identifier getPortIdentifier(Element el) {
+	private static Port.Identifier getPortIdentifier(Element el) {
 		org.bigraph.model.Node.Identifier id = getNodeIdentifier(
 				EditXMLLoader.getNamedChildElement(el, EDIT_BIG, "node-id"));
 		return new Port.Identifier(
 				EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"), id);
 	}
 	
-	private org.bigraph.model.Node.Identifier getNodeIdentifier(
+	private static org.bigraph.model.Node.Identifier getNodeIdentifier(
 			Element el) {
 		Control.Identifier id = getControlIdentifier(
 				EditXMLLoader.getNamedChildElement(el, EDIT_SIG, "control-id"));
@@ -63,55 +67,44 @@ public class BigraphEditLoader implements Participant {
 				EditXMLLoader.getAttributeNS(el, EDIT_BIG, "name"), id);
 	}
 	
-	private Control.Identifier getControlIdentifier(Element el) {
+	private static Control.Identifier getControlIdentifier(Element el) {
 		return new Control.Identifier(
 				EditXMLLoader.getAttributeNS(el, EDIT_SIG, "name"));
 	}
 	
-	private Container.Identifier getParentIdentifier(Element el) {
-		String localName = el.getLocalName();
-		if ("bigraph-id".equals(localName)) {
-			return getBigraphIdentifier();
-		} else if ("root-id".equals(localName)) {
-			return getRootIdentifier(el);
-		} else if ("node-id".equals(localName)) {
-			return getNodeIdentifier(el);
+	public static ModelObject.Identifier getIdentifier(Element el) {
+		String
+			localName = el.getLocalName(),
+			namespace = el.getNamespaceURI();
+		if (EDIT_BIG.equals(namespace)) {
+			if ("bigraph-id".equals(localName)) {
+				return getBigraphIdentifier();
+			} else if ("root-id".equals(localName)) {
+				return getRootIdentifier(el);
+			} else if ("edge-id".equals(localName)) {
+				return getEdgeIdentifier(el);
+			} else if ("innername-id".equals(localName)) {
+				return getInnerNameIdentifier(el);
+			} else if ("outername-id".equals(localName)) {
+				return getOuterNameIdentifier(el);
+			} else if ("node-id".equals(localName)) {
+				return getNodeIdentifier(el);
+			} else if ("port-id".equals(localName)) {
+				return getPortIdentifier(el);
+			} else if ("site-id".equals(localName)) {
+				return getSiteIdentifier(el);
+			} else return null;
+		} else if (EDIT_SIG.equals(namespace)) {
+			if ("control-id".equals(localName)) {
+				return getControlIdentifier(el);
+			} else return null;
 		} else return null;
 	}
 	
-	private Layoutable.Identifier getChildIdentifier(Element el) {
-		String localName = el.getLocalName();
-		if ("root-id".equals(localName)) {
-			return getRootIdentifier(el);
-		} else if ("node-id".equals(localName)) {
-			return getNodeIdentifier(el);
-		} else if ("site-id".equals(localName)) {
-			return getSiteIdentifier(el);
-		} else if ("edge-id".equals(localName)) {
-			return getEdgeIdentifier(el);
-		} else if ("innername-id".equals(localName)) {
-			return getInnerNameIdentifier(el);
-		} else if ("outername-id".equals(localName)) {
-			return getOuterNameIdentifier(el);
-		} else return null;
-	}
-	
-	private Point.Identifier getPointIdentifier(Element el) {
-		String localName = el.getLocalName();
-		if ("port-id".equals(localName)) {
-			return getPortIdentifier(el);
-		} else if ("innername-id".equals(localName)) {
-			return getInnerNameIdentifier(el);
-		} else return null;
-	}
-	
-	private Link.Identifier getLinkIdentifier(Element el) {
-		String localName = el.getLocalName();
-		if ("edge-id".equals(localName)) {
-			return getEdgeIdentifier(el);
-		} else if ("outername-id".equals(localName)) {
-			return getOuterNameIdentifier(el);
-		} else return null;
+	public static <T extends ModelObject.Identifier> T getIdentifier(
+			Element el, Class<T> klass) {
+		ModelObject.Identifier id = getIdentifier(el);
+		return (klass.isInstance(id) ? klass.cast(id) : null);
 	}
 	
 	@Override
@@ -122,25 +115,25 @@ public class BigraphEditLoader implements Participant {
 		if (EDIT_BIG.equals(nsURI)) {
 			List<Element> ids = EditXMLLoader.getChildElements(descriptor);
 			if ("add".equals(localName)) {
-				Container.Identifier parent =
-					getParentIdentifier(ids.get(0));
-				Layoutable.Identifier child =
-					getChildIdentifier(ids.get(1));
+				Container.Identifier parent = getIdentifier(
+						ids.get(0), Container.Identifier.class);
+				Layoutable.Identifier child = getIdentifier(
+						ids.get(1), Layoutable.Identifier.class);
 				return new Container.ChangeAddChildDescriptor(
 						parent, child);
 			} else if ("remove".equals(localName)) {
-				Layoutable.Identifier target =
-					getChildIdentifier(ids.get(0));
+				Layoutable.Identifier target = getIdentifier(
+						ids.get(0), Layoutable.Identifier.class);
 				return new Layoutable.ChangeRemoveDescriptor(target);
 			} else if ("connect".equals(localName)) {
-				Point.Identifier point =
-					getPointIdentifier(ids.get(0));
-				Link.Identifier link =
-					getLinkIdentifier(ids.get(1));
+				Point.Identifier point = getIdentifier(
+						ids.get(0), Point.Identifier.class);
+				Link.Identifier link = getIdentifier(
+						ids.get(1), Link.Identifier.class);
 				return new Point.ChangeConnectDescriptor(point, link);
 			} else if ("disconnect".equals(localName)) {
-				Point.Identifier point =
-					getPointIdentifier(ids.get(0));
+				Point.Identifier point = getIdentifier(
+						ids.get(0), Point.Identifier.class);
 				return new Point.ChangeDisconnectDescriptor(point);
 			} else return null;
 		} else return null;
@@ -148,7 +141,8 @@ public class BigraphEditLoader implements Participant {
 	
 	@Override
 	public IChangeDescriptor getRenameDescriptor(Element id, String name) {
-		Layoutable.Identifier lid = getChildIdentifier(id);
+		Layoutable.Identifier
+			lid = getIdentifier(id, Layoutable.Identifier.class);
 		return (lid != null ?
 				new Layoutable.ChangeNameDescriptor(lid, name) : null);
 	}
