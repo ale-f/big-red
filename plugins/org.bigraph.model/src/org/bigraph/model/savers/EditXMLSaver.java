f4a13064f86584788a2216741f5352d7e6c32cf1
Alec Faithfull <alexander.faithfull@gmail.com>
2012-09-13 12:22:16 +0200

    [savers] Promoted the bigraph edit saver into a new top-level class
    I still haven't quite worked out how participants should be propagated down
    through successive EditXMLSavers; I need to put some thought into that

--- plugins/org.bigraph.model/src/org/bigraph/model/savers/EditXMLSaver.java
+++ plugins/org.bigraph.model/src/org/bigraph/model/savers/EditXMLSaver.java
@@ -1,29 +1,13 @@
 package org.bigraph.model.savers;
 
 import static org.bigraph.model.loaders.RedNamespaceConstants.EDIT;
-import static org.bigraph.model.loaders.RedNamespaceConstants.EDIT_BIG;
-import static org.bigraph.model.loaders.RedNamespaceConstants.EDIT_SIG;
 
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-import org.bigraph.model.Container.ChangeAddChildDescriptor;
-import org.bigraph.model.Control;
-import org.bigraph.model.Edge;
 import org.bigraph.model.Edit;
-import org.bigraph.model.InnerName;
 import org.bigraph.model.ModelObject;
-import org.bigraph.model.Node;
-import org.bigraph.model.OuterName;
-import org.bigraph.model.Point.ChangeConnectDescriptor;
-import org.bigraph.model.Point.ChangeDisconnectDescriptor;
-import org.bigraph.model.Port;
-import org.bigraph.model.Root;
-import org.bigraph.model.Site;
-import org.bigraph.model.Layoutable.ChangeNameDescriptor;
-import org.bigraph.model.Layoutable.ChangeRemoveDescriptor;
-import org.bigraph.model.ModelObject.Identifier;
 import org.bigraph.model.changes.descriptors.ChangeDescriptorGroup;
 import org.bigraph.model.changes.descriptors.IChangeDescriptor;
 import org.w3c.dom.Element;
@@ -33,79 +17,6 @@ public class EditXMLSaver extends XMLSaver {
 		void setSaver(IXMLSaver saver);
 		
 		Element processDescriptor(IChangeDescriptor cd);
-	}
-	
-	private final class BigraphEditHandler implements Participant {
-		private IXMLSaver saver;
-		
-		@Override
-		public void setSaver(IXMLSaver saver) {
-			this.saver = saver;
-		}
-		
-		private final Element newElement(String ns, String qn) {
-			return saver.getDocument().createElementNS(ns, qn);
-		}
-		
-		protected Element makeID(Identifier id) {
-			String name = id.getName();
-			Element el = null;
-			if (id instanceof Site.Identifier) {
-				el = newElement(EDIT_BIG, "edit-big:site-id");
-			} else if (id instanceof Root.Identifier) {
-				el = newElement(EDIT_BIG, "edit-big:root-id");
-			} else if (id instanceof Node.Identifier) {
-				el = newElement(EDIT_BIG, "edit-big:node-id");
-				el.appendChild(
-						makeID(((Node.Identifier)id).getControl()));
-			} else if (id instanceof Port.Identifier) {
-				el = newElement(EDIT_BIG, "edit-big:port-id");
-				el.appendChild(
-						makeID(((Port.Identifier)id).getNode()));
-			} else if (id instanceof InnerName.Identifier) {
-				el = newElement(EDIT_BIG, "edit-big:innername-id");
-			} else if (id instanceof OuterName.Identifier) {
-				el = newElement(EDIT_BIG, "edit-big:outername-id");
-			} else if (id instanceof Edge.Identifier) {
-				el = newElement(EDIT_BIG, "edit-big:edge-id");
-			} else if (id instanceof Control.Identifier) {
-				el = newElement(EDIT_SIG, "edit-sig:control-id");
-			}
-			if (el != null && name != null)
-				el.setAttributeNS(null, "name", name);
-			return el;
-		}
-		
-		@Override
-		public Element processDescriptor(IChangeDescriptor cd_) {
-			Element el = null;
-			if (cd_ instanceof ChangeAddChildDescriptor) {
-				ChangeAddChildDescriptor cd = (ChangeAddChildDescriptor)cd_;
-				el = newElement(EDIT_BIG, "edit-big:add");
-				el.appendChild(makeID(cd.getParent()));
-				el.appendChild(makeID(cd.getChild()));
-			} else if (cd_ instanceof ChangeRemoveDescriptor) {
-				ChangeRemoveDescriptor cd = (ChangeRemoveDescriptor)cd_;
-				el = newElement(EDIT_BIG, "edit-big:remove");
-				el.appendChild(makeID(cd.getTarget()));
-			} else if (cd_ instanceof ChangeNameDescriptor) {
-				ChangeNameDescriptor cd = (ChangeNameDescriptor)cd_;
-				el = newElement(EDIT, "edit:rename");
-				el.appendChild(makeID(cd.getTarget()));
-				el.setAttributeNS(null, "name", cd.getNewName());
-			} else if (cd_ instanceof ChangeConnectDescriptor) {
-				ChangeConnectDescriptor cd = (ChangeConnectDescriptor)cd_;
-				el = newElement(EDIT_BIG, "edit-big:connect");
-				el.appendChild(makeID(cd.getPoint()));
-				el.appendChild(makeID(cd.getLink()));
-			} else if (cd_ instanceof ChangeDisconnectDescriptor) {
-				ChangeDisconnectDescriptor cd =
-						(ChangeDisconnectDescriptor)cd_;
-				el = newElement(EDIT_BIG, "edit-big:disconnect");
-				el.appendChild(makeID(cd.getPoint()));
-			}
-			return el;
-		}
 	}
 	
 	private List<Participant> participants = new ArrayList<Participant>();
