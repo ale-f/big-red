7505af2392e94627826fc6ba6f5828480d6e196b
Alec Faithfull <alexander.faithfull@gmail.com>
2012-10-01 17:15:01 +0200

    [org.bigraph.model.tests] Added a project for running tests on the model
    To avoid having to add a fake Eclipse plugin, this project "depends" upon
    the model by including its project on the classpath

---
+++ tests/org.bigraph.model.tests/tests/org/bigraph/model/tests/BigraphTests.java
@@ -0,0 +1,157 @@
+package org.bigraph.model.tests;
+
+import java.util.Arrays;
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import org.bigraph.model.Bigraph;
+import org.bigraph.model.Container;
+import org.bigraph.model.Control;
+import org.bigraph.model.Edge;
+import org.bigraph.model.InnerName;
+import org.bigraph.model.Link;
+import org.bigraph.model.Node;
+import org.bigraph.model.OuterName;
+import org.bigraph.model.PortSpec;
+import org.bigraph.model.Root;
+import org.bigraph.model.Signature;
+import org.bigraph.model.Site;
+import org.bigraph.model.changes.ChangeGroup;
+import org.bigraph.model.changes.ChangeRejectedException;
+import org.bigraph.model.changes.IChange;
+
+public class BigraphTests {
+	private Signature signature;
+	private Control control0, control1;
+	
+	private static ChangeGroup cg(IChange... changes) {
+		return new ChangeGroup(Arrays.asList(changes));
+	}
+	
+	@Before
+	public void createSignature() throws ChangeRejectedException {
+		signature = new Signature();
+		
+		control0 = new Control();
+		signature.tryApplyChange(
+				signature.changeAddControl(control0, "c0"));
+		
+		control1 = new Control();
+		signature.tryApplyChange(cg(
+				signature.changeAddControl(control1, "c1"),
+				control1.changeAddPort(new PortSpec(), "p0"),
+				control1.changeAddPort(new PortSpec(), "p1")));
+	}
+	
+	@Test
+	public void basicAdd() throws ChangeRejectedException {
+		Bigraph b = new Bigraph();
+		b.tryApplyChange(b.changeAddChild(new Root(), "0"));
+	}
+	
+	@Test
+	public void basicAddHierarchy() throws ChangeRejectedException {
+		Bigraph b = new Bigraph();
+		b.setSignature(signature);
+		
+		Root r = new Root();
+		try {
+			b.tryApplyChange(b.changeAddChild(r, "0"));
+		} catch (ChangeRejectedException e) {
+			fail(e.getRationale());
+		}
+		
+		ChangeGroup cg = new ChangeGroup();
+		Container c = r;
+		for (int i = 0; i < 1000; i++) {
+			Node n = new Node(control0);
+			cg.add(
+					c.changeAddChild(n, Integer.toString(i)));
+			c = n;
+		}
+		
+		b.tryApplyChange(cg);
+	}
+	
+	private void tryAddAndConnect(Bigraph b, InnerName in, Link l)
+			throws ChangeRejectedException {
+		b.tryApplyChange(cg(
+				b.changeAddChild(l, "a"),
+				b.changeAddChild(in, "a"),
+				in.changeConnect(l)));
+	}
+	
+	@Test
+	public void connectInnerToEdge() throws ChangeRejectedException {
+		tryAddAndConnect(new Bigraph(), new InnerName(), new Edge());
+	}
+	
+	@Test
+	public void connectInnerToOuter() throws ChangeRejectedException {
+		tryAddAndConnect(new Bigraph(), new InnerName(), new OuterName());
+	}
+	
+	@Test(expected = ChangeRejectedException.class)
+	public void addDuplicateName() throws ChangeRejectedException {
+		Bigraph b = new Bigraph();
+		Root
+			r0 = new Root(),
+			r1 = new Root();
+		b.tryApplyChange(cg(
+				b.changeAddChild(r0, "0"),
+				b.changeAddChild(r1, "0")));
+	}
+	
+	@Test(expected = ChangeRejectedException.class)
+	public void addInvalidRootName() throws ChangeRejectedException {
+		Bigraph b = new Bigraph();
+		b.tryApplyChange(b.changeAddChild(new Root(), "test"));
+	}
+	
+	@Test(expected = ChangeRejectedException.class)
+	public void setInvalidRootName() throws ChangeRejectedException {
+		Bigraph b = new Bigraph();
+		Root r = new Root();
+		try {
+			b.tryApplyChange(b.changeAddChild(r, "0"));
+		} catch (ChangeRejectedException e) {
+			fail("Root insertion failed: " + e.getRationale());
+		}
+		b.tryApplyChange(r.changeName("test"));
+	}
+	
+	@Test(expected = ChangeRejectedException.class)
+	public void addInvalidSiteName() throws ChangeRejectedException {
+		Bigraph b = new Bigraph();
+		Root r = new Root();
+		try {
+			b.tryApplyChange(b.changeAddChild(r, "0"));
+		} catch (ChangeRejectedException e) {
+			fail(e.getRationale());
+		}
+		b.tryApplyChange(r.changeAddChild(new Site(), "test"));
+	}
+	
+	@Test(expected = ChangeRejectedException.class)
+	public void setInvalidSiteName() throws ChangeRejectedException {
+		Bigraph b = new Bigraph();
+		Root r = new Root();
+		Site s = new Site();
+		try {
+			b.tryApplyChange(cg(
+					b.changeAddChild(r, "0"),
+					r.changeAddChild(s, "0")));
+		} catch (ChangeRejectedException e) {
+			fail(e.getRationale());
+		}
+		b.tryApplyChange(s.changeName("test"));
+	}
+	
+	@Test(expected = ChangeRejectedException.class)
+	public void addSiteToBigraph() throws ChangeRejectedException {
+		Bigraph b = new Bigraph();
+		b.tryApplyChange(b.changeAddChild(new Site(), "0"));
+	}
+}
---
+++ tests/org.bigraph.model.tests/tests/org/bigraph/model/tests/ModelTests.java
@@ -0,0 +1,11 @@
+package org.bigraph.model.tests;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BigraphTests.class })
+public class ModelTests {
+
+}
