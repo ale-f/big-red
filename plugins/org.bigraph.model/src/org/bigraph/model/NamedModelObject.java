e6a82829e4d250700cf9b9801c2e7da24d0c1b7a
Alec Faithfull <alexander.faithfull@gmail.com>
2012-10-08 13:48:15 +0200

    [NamedModelObject] Added a new abstract class for named model objects
    Quite a lot of model objects have names, but they share none of the support
    code; this new class is an attempt to address this problem

---
+++ plugins/org.bigraph.model/src/org/bigraph/model/NamedModelObject.java
@@ -0,0 +1,46 @@
+package org.bigraph.model;
+
+import org.bigraph.model.assistants.PropertyScratchpad;
+import org.bigraph.model.assistants.RedProperty;
+
+public abstract class NamedModelObject extends ModelObject {
+	@RedProperty(fired = String.class, retrieved = String.class)
+	public static final String PROPERTY_NAME = "NamedModelObjectName";
+	
+	private String name;
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getName(PropertyScratchpad context) {
+		return (String)getProperty(context, PROPERTY_NAME);
+	}
+	
+	protected void setName(String name) {
+		String oldName = this.name;
+		this.name = name;
+		firePropertyChange(PROPERTY_NAME, oldName, name);
+	}
+	
+	@Override
+	protected NamedModelObject clone() {
+		NamedModelObject o = (NamedModelObject)super.clone();
+		o.setName(getName());
+		return o;
+	}
+	
+	@Override
+	protected Object getProperty(String name) {
+		if (PROPERTY_NAME.equals(name)) {
+			return getName();
+		} else return super.getProperty(name);
+	}
+	
+	@Override
+	public void dispose() {
+		name = null;
+		
+		super.dispose();
+	}
+}
