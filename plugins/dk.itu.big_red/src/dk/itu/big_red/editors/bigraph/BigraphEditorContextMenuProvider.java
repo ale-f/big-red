a038e01a61e771f3a3d2d13764ab5fe4d95754a8
Alec Faithfull <alexander.faithfull@gmail.com>
2012-10-10 17:14:23 +0200

    [BigraphEditorContextMenuProvider] Allow link styles to be changed
    This should probably also appear in the property view for Links

--- plugins/dk.itu.big_red/src/dk/itu/big_red/editors/bigraph/BigraphEditorContextMenuProvider.java
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/editors/bigraph/BigraphEditorContextMenuProvider.java
@@ -1,6 +1,12 @@
 package dk.itu.big_red.editors.bigraph;
 
+import java.util.Iterator;
+
+import org.bigraph.model.Link;
+import org.bigraph.model.utilities.FilteringIterable;
+
 import org.eclipse.gef.ContextMenuProvider;
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.EditPartViewer;
 import org.eclipse.gef.palette.PaletteContainer;
 import org.eclipse.gef.palette.PaletteSeparator;
@@ -9,19 +15,45 @@ import org.eclipse.gef.ui.actions.ActionRegistry;
 import org.eclipse.gef.ui.actions.GEFActionConstants;
 import org.eclipse.gef.ui.palette.PaletteViewer;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.GroupMarker;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.ui.actions.ActionFactory;
 import dk.itu.big_red.editors.bigraph.actions.BigraphRelayoutAction;
+import dk.itu.big_red.editors.bigraph.commands.ChangeCommand;
+import dk.itu.big_red.editors.bigraph.figures.LinkConnectionFigure.Style;
+import dk.itu.big_red.editors.bigraph.parts.BigraphPart;
+import dk.itu.big_red.editors.bigraph.parts.LinkPart;
+import dk.itu.big_red.model.LinkStyleUtilities;
 
 public class BigraphEditorContextMenuProvider extends ContextMenuProvider {
-	private ActionRegistry actionRegistry;
-	
 	public BigraphEditorContextMenuProvider(
 			EditPartViewer viewer, ActionRegistry registry) {
 		super(viewer);
 		setActionRegistry(registry);
+	}
+	
+	private ActionRegistry actionRegistry;
+	
+	protected ActionRegistry getActionRegistry() {
+		return actionRegistry;
+	}
+	
+	protected void setActionRegistry(ActionRegistry registry) {
+		actionRegistry = registry;
+	}
+	
+	protected Iterable<? extends EditPart> getSelection() {
+		return new FilteringIterable<EditPart>(
+				EditPart.class, getViewer().getSelectedEditParts());
+	}
+	
+	protected EditPart getFirstSelection() {
+		Iterator<? extends EditPart> it = getSelection().iterator();
+		if (it.hasNext()) {
+			return it.next();
+		} else return null;
 	}
 	
 	private void populateMenu(
@@ -46,6 +78,33 @@ public class BigraphEditorContextMenuProvider extends ContextMenuProvider {
 				menu.add(new Separator());
 			}
 		}
+	}
+	
+	private static final String GROUP_VARYING =
+			"BigraphEditorContextMenuProvider+Varying";
+	
+	private final void
+			addLinkOptions(final LinkPart p, final IMenuManager menu) {
+		final Link l = p.getModel();
+		
+		MenuManager styleMenu = new MenuManager("Style");
+		Style currentStyle = LinkStyleUtilities.getStyle(l);
+		for (final Style i : Style.values()) {
+			Action a = new Action(i.getDisplayName(), Action.AS_RADIO_BUTTON) {
+				@Override
+				public void run() {
+					if (!isChecked())
+						return;
+					getViewer().getEditDomain().getCommandStack().execute(
+							new ChangeCommand(
+									LinkStyleUtilities.changeStyle(l, i),
+									l.getBigraph()));
+				}
+			};
+			a.setChecked(i.equals(currentStyle));
+			styleMenu.add(a);
+		}
+		menu.appendToGroup(GROUP_VARYING, styleMenu);
 	}
 	
 	@Override
@@ -73,8 +132,15 @@ public class BigraphEditorContextMenuProvider extends ContextMenuProvider {
 		menu.appendToGroup(GEFActionConstants.GROUP_EDIT,
 				getActionRegistry().getAction(ActionFactory.PROPERTIES.getId()));
 		
-		menu.appendToGroup(GEFActionConstants.GROUP_REST,
-				getActionRegistry().getAction(BigraphRelayoutAction.ID));
+		menu.add(new GroupMarker(GROUP_VARYING));
+		
+		EditPart selection = getFirstSelection();
+		if (selection instanceof BigraphPart) {
+			menu.appendToGroup(GROUP_VARYING,
+					getActionRegistry().getAction(BigraphRelayoutAction.ID));
+		} else if (selection instanceof LinkPart) {
+			addLinkOptions((LinkPart)selection, menu);
+		}
 		
 		menu.appendToGroup(GEFActionConstants.GROUP_REST,
 				new Separator());
@@ -84,13 +150,5 @@ public class BigraphEditorContextMenuProvider extends ContextMenuProvider {
 		PaletteViewer pv = getViewer().getEditDomain().getPaletteViewer();
 		populateMenu(getViewer().getEditDomain().getPaletteViewer(),
 				pv.getPaletteRoot(), palette);
-	}
-
-	private ActionRegistry getActionRegistry() {
-		return actionRegistry;
-	}
-	
-	private void setActionRegistry(ActionRegistry registry) {
-		actionRegistry = registry;
 	}
 }
