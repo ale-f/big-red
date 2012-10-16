ale-f/big-red

commit bd9644dbc1a589c98d80036fa9f4fed642647306

[ModelObjectContentProvider] Restructured the input registration process

plugins/dk.itu.big_red/src/dk/itu/big_red/editors/signature/SignatureControlsContentProvider.java
@@ -32,10 +32,13 @@ class SignatureControlsContentProvider extends ModelObjectTreeContentProvider {
 	}
 	
 	@Override
-	protected void setInput(Object oldInput, Object newInput) {
+	protected void unregister(Object oldInput) {
 		if (oldInput instanceof Signature)
 			recursivelyStopListening((Signature)oldInput);
-		super.setInput(oldInput, newInput);
+	}
+	
+	@Override
+	protected void register(Object newInput) {
 		if (newInput instanceof Signature)
 			recursivelyListen((Signature)newInput);
 	}

plugins/dk.itu.big_red/src/dk/itu/big_red/utilities/ui/jface/ModelObjectContentProvider.java
@@ -14,21 +14,25 @@ public abstract class ModelObjectContentProvider implements IContentProvider,
 		return input;
 	}
 	
-	protected void setInput(Object oldInput, Object newInput) {
+	protected void unregister(Object oldInput) {
 		if (oldInput instanceof ModelObject)
 			((ModelObject)oldInput).removePropertyChangeListener(this);
-		input = newInput;
-		if (input instanceof ModelObject)
-			((ModelObject)input).addPropertyChangeListener(this);
 	}
 	
-	protected void setInput(Object newInput) {
-		setInput(input, newInput);
+	protected void register(Object newInput) {
+		if (newInput instanceof ModelObject)
+			((ModelObject)newInput).addPropertyChangeListener(this);
+	}
+	
+	private void setInput(Object oldInput, Object newInput) {
+		unregister(oldInput);
+		input = newInput;
+		register(newInput);
 	}
 	
 	@Override
 	public void dispose() {
-		setInput(null);
+		setInput(null, null);
 	}
 	
 	@Override

commit 80c4e80758c0575056d51576177c064bac36566d

[SignatureControlsContentProvider] Listen to property changes properly
Commit bd9644dbc1a589c98d80036fa9f4fed642647306 inadvertently removed the
most important bit of listener registration code

plugins/dk.itu.big_red/src/dk/itu/big_red/editors/signature/SignatureControlsContentProvider.java
@@ -14,21 +14,19 @@ class SignatureControlsContentProvider extends ModelObjectTreeContentProvider {
 	}
 	
 	private void recursivelyListen(Signature s) {
+		s.addPropertyChangeListener(this);
 		for (Control c : s.getControls())
 			c.addPropertyChangeListener(this);
-		for (Signature t : s.getSignatures()) {
+		for (Signature t : s.getSignatures())
 			recursivelyListen(t);
-			t.addPropertyChangeListener(this);
-		}
 	}
 	
 	private void recursivelyStopListening(Signature s) {
+		for (Signature t : s.getSignatures())
+			recursivelyStopListening(t);
 		for (Control c : s.getControls())
 			c.removePropertyChangeListener(this);
-		for (Signature t : s.getSignatures()) {
-			recursivelyStopListening(t);
-			t.removePropertyChangeListener(this);
-		}
+		s.removePropertyChangeListener(this);
 	}
 	
 	@Override