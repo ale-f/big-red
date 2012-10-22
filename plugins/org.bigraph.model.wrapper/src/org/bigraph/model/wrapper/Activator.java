f329d7f5d2d4a16da7fbd511acda7cbe1a72e759
Alec Faithfull <alexander.faithfull@gmail.com>
2012-10-22 11:24:54 +0200

    [validation.xsd] Added the ability to contribute execution participants
    This commit also extends the wrapper's activator with support for these new
    participants and renames the extension point to reflect its new generality

--- plugins/org.bigraph.model.wrapper/src/org/bigraph/model/wrapper/Activator.java
+++ plugins/org.bigraph.model.wrapper/src/org/bigraph/model/wrapper/Activator.java
@@ -2,7 +2,9 @@ package org.bigraph.model.wrapper;
 
 import java.util.ArrayList;
 
+import org.bigraph.model.assistants.ExecutorManager;
 import org.bigraph.model.assistants.ValidatorManager;
+import org.bigraph.model.changes.IStepExecutor;
 import org.bigraph.model.changes.IStepValidator;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
@@ -14,6 +16,7 @@ import org.osgi.framework.BundleContext;
 public final class Activator extends Plugin {
 	private static Activator instance;
 	
+	private ArrayList<IStepExecutor> executors;
 	private ArrayList<IStepValidator> validators;
 	
 	@Override
@@ -21,12 +24,12 @@ public final class Activator extends Plugin {
 		super.start(context);
 		instance = this;
 		
-		installValidators();
+		install();
 	}
 	
 	@Override
 	public void stop(BundleContext context) throws Exception {
-		uninstallValidators();
+		uninstall();
 		
 		instance = null;
 		super.stop(context);
@@ -36,36 +39,60 @@ public final class Activator extends Plugin {
 		return instance;
 	}
 	
-	public static final String EXTENSION_POINT_VALIDATION =
-			"org.bigraph.model.wrapper.validation";
+	public static final String EXTENSION_POINT_CHANGES =
+			"org.bigraph.model.wrapper.changes";
 	
-	private void installValidators() {
+	private void install() {
+		executors = new ArrayList<IStepExecutor>();
 		validators = new ArrayList<IStepValidator>();
 		
-		ValidatorManager instance = ValidatorManager.getInstance();
+		ExecutorManager eInstance = ExecutorManager.getInstance();
+		ValidatorManager vInstance = ValidatorManager.getInstance();
 		IExtensionRegistry registry = RegistryFactory.getRegistry();
+		
 		for (IConfigurationElement ice :
 				registry.getConfigurationElementsFor(
-						EXTENSION_POINT_VALIDATION)) {
-			IStepValidator validator;
-			try {
-				validator = (IStepValidator)
-						ice.createExecutableExtension("class");
-			} catch (CoreException ce) {
-				ce.printStackTrace();
-				continue;
+						EXTENSION_POINT_CHANGES)) {
+			String name = ice.getName();
+			if ("executor".equals(name)) {
+				IStepExecutor executor;
+				try {
+					executor = (IStepExecutor)
+							ice.createExecutableExtension("class");
+				} catch (CoreException ce) {
+					ce.printStackTrace();
+					continue;
+				}
+				executors.add(executor);
+				eInstance.addExecutor(executor);
+			} else if ("validator".equals(name)) {
+				IStepValidator validator;
+				try {
+					validator = (IStepValidator)
+							ice.createExecutableExtension("class");
+				} catch (CoreException ce) {
+					ce.printStackTrace();
+					continue;
+				}
+				validators.add(validator);
+				vInstance.addValidator(validator);
 			}
-			validators.add(validator);
-			instance.addValidator(validator);
 		}
 	}
 	
-	private void uninstallValidators() {
-		ValidatorManager instance = ValidatorManager.getInstance();
+	private void uninstall() {
+		ValidatorManager vInstance = ValidatorManager.getInstance();
 		for (IStepValidator i : validators)
-			instance.removeValidator(i);
+			vInstance.removeValidator(i);
+		
+		ExecutorManager eInstance = ExecutorManager.getInstance();
+		for (IStepExecutor i : executors)
+			eInstance.removeExecutor(i);
+		
+		executors.clear();
 		validators.clear();
 		
+		executors = null;
 		validators = null;
 	}
 }
