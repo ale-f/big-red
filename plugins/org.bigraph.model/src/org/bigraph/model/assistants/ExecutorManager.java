0ab29fe39079767d4a65171167b03e33d0a25f8f
Alec Faithfull <alexander.faithfull@gmail.com>
2012-10-23 11:28:46 +0200

    [ExecutorManager] Added internal support for IStepExecutor construction
    Although this commit paves the way for nested ExecutorManagers, the code
    that it adds is all private or protected -- for now

--- plugins/org.bigraph.model/src/org/bigraph/model/assistants/ExecutorManager.java
+++ plugins/org.bigraph.model/src/org/bigraph/model/assistants/ExecutorManager.java
@@ -44,30 +44,40 @@ public class ExecutorManager {
 		if (validator != null)
 			validator.tryValidateChange(change);
 		
-		Process p = new Process();
-		IChange ch = p.run(change);
+		IChange ch = run(change);
 		if (ch != null)
 			throw new Error(
 					"BUG: " + ch + " passed validation but couldn't" +
 					"be executed");
 	}
 	
-	private class Process {
-		public IChange run(IChange c) {
-			c.beforeApply();
-			if (!(c instanceof ChangeGroup)) {
-				boolean passes = false;
-				for (IStepExecutor i : getExecutors())
-					passes |= i.executeChange(c);
-				return (passes ? null : c);
-			} else {
-				for (IChange i : (ChangeGroup)c) {
-					IChange j = run(i);
-					if (j != null)
-						return j;
-				}
-				return null;
+	private IChange step(IChange c) {
+		boolean passes = false;
+		for (IStepExecutor i : getExecutors())
+			passes |= i.executeChange(c);
+		return (passes ? null : c);
+	}
+	
+	private IChange run(IChange c) {
+		c.beforeApply();
+		if (!(c instanceof ChangeGroup)) {
+			return step(c);
+		} else {
+			for (IChange i : (ChangeGroup)c) {
+				IChange j = run(i);
+				if (j != null)
+					return j;
 			}
+			return null;
 		}
+	}
+	
+	IStepExecutor createStepExecutor() {
+		return new IStepExecutor() {
+			@Override
+			public boolean executeChange(IChange change_) {
+				return (step(change_) == null);
+			}
+		};
 	}
 }
