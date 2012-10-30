fa3cc411e08356f5280363eef340e5f3decb65d4
Alec Faithfull <alexander.faithfull@gmail.com>
2012-10-30 12:59:02 +0100

    [OutputParser] Use LexerFactory to create lexers

--- plugins/org.bigraph.bigmc.red/src/org/bigraph/bigmc/red/OutputParser.java
+++ plugins/org.bigraph.bigmc.red/src/org/bigraph/bigmc/red/OutputParser.java
@@ -1,6 +1,5 @@
 package org.bigraph.bigmc.red;
 
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.bigraph.model.Bigraph;
@@ -17,15 +16,16 @@ import org.bigraph.model.names.HashMapNamespace;
 import org.bigraph.model.names.INamespace;
 
 import dk.itu.big_red.model.LayoutUtilities;
+
+import org.bigraph.bigmc.red.LexerFactory.Lexer;
+import org.bigraph.bigmc.red.LexerFactory.Lexer.DisappointedException;
 import org.bigraph.extensions.param.ParameterUtilities;
 
 public class OutputParser {
-	private String string;
-	private static final Pattern scanner = Pattern.compile(
+	private static final LexerFactory lf = new LexerFactory(
 			"(\\(|\\)|\\[|\\]|,|-|\\$[0-9]+|[a-zA-Z_][a-zA-Z0-9_]*|\\.|\\|)");
 	
-	private Matcher matcher;
-	private String current;
+	private Lexer lexer;
 	
 	private Signature s;
 	
@@ -34,39 +34,8 @@ public class OutputParser {
 		return this;
 	}
 	
-	private void prime() {
-		if (matcher == null)
-			matcher = scanner.matcher(string);
-		if (current == null)
-			current = (matcher.find() ? matcher.group() : null);
-	}
-	
-	private String accept(Pattern p) {
-		prime();
-		
-		if (current != null && p.matcher(current).matches()) {
-			String previous = current;
-			current = null;
-			return previous;
-		} else return null;
-	}
-	
-	private boolean lookahead(Pattern p) {
-		prime();
-		return (current != null && p.matcher(current).matches());
-	}
-	
-	private String expect(Pattern p) {
-		String s = accept(p);
-		if (s != null) {
-			return s;
-		} else throw new Error("Oh no, expected " + p.toString() + " but current is " + current);
-	}
-	
 	public OutputParser setString(String string) {
-		this.string = string;
-		matcher = null;
-		current = null;
+		lexer = lf.createLexer(string);
 		return this;
 	}
 	
@@ -91,10 +60,11 @@ public class OutputParser {
 	private Bigraph workingBigraph;
 	private INamespace<Link> ns;
 	
-	private void parseChild(Container parent, ChangeGroup cg) {
-		if (accept(P_NIL) != null)
+	private void parseChild(Container parent, ChangeGroup cg)
+			throws DisappointedException {
+		if (lexer.accept(P_NIL) != null)
 			return;
-		String name = accept(P_NAM);
+		String name = lexer.accept(P_NAM);
 		if (name != null) { /* name is a control */
 			Node n = null;
 			String[] parts = name.split("_P__", 2);
@@ -106,12 +76,12 @@ public class OutputParser {
 			} else throw new Error("Control name couldn't be matched");
 			
 			cg.add(parent.changeAddChild(n, Integer.toString(x++)));
-			if (accept(P_LSQ) != null) { /* ports */
+			if (lexer.accept(P_LSQ) != null) { /* ports */
 				int i = 0;
 				do {
-					String linkName = accept(P_NAM);
+					String linkName = lexer.accept(P_NAM);
 					if (linkName == null) {
-						expect(P_DSH);
+						lexer.expect(P_DSH);
 						i++;
 						continue;
 					}
@@ -121,27 +91,28 @@ public class OutputParser {
 						cg.add(workingBigraph.changeAddChild(l, linkName));
 					}
 					cg.add(n.getPorts().get(i++).changeConnect(l));
-				} while (accept(P_COM) != null);
-				expect(P_RSQ);
+				} while (lexer.accept(P_COM) != null);
+				lexer.expect(P_RSQ);
 			}
 			/* Strictly speaking, this doesn't need to be a conditional -- on
 			 * the other hand, it means that the input language can be parsed
 			 * as well */
-			if (accept(P_DOT) != null)
+			if (lexer.accept(P_DOT) != null)
 				parseChildren(n, cg);
-		} else if ((name = accept(P_SIT)) != null) { /* name is a site id */
+		} else if ((name = lexer.accept(P_SIT)) != null) { /* name is a site id */
 			Site s = new Site();
 			cg.add(parent.changeAddChild(s, name.substring(1)));
-		} else throw new Error("What(child-context): " + current);
+		} else throw new Error("What(child-context): " + lexer.getCurrent());
 	}
 	
-	private void parseChildren(Container parent, ChangeGroup cg) {
-		if (lookahead(P_LBR)) {
-			expect(P_LBR);
+	private void parseChildren(Container parent, ChangeGroup cg)
+			throws DisappointedException {
+		if (lexer.lookahead1(P_LBR)) {
+			lexer.expect(P_LBR);
 			do {
 				parseChild(parent, cg);
-			} while (accept(P_BAR) != null);
-			expect(P_RBR);
+			} while (lexer.accept(P_BAR) != null);
+			lexer.expect(P_RBR);
 		} else parseChild(parent, cg);
 	}
 	
@@ -163,6 +134,9 @@ public class OutputParser {
 			return workingBigraph;
 		} catch (ChangeRejectedException cre) {
 			cre.printStackTrace();
+			return null;
+		} catch (DisappointedException de) {
+			de.printStackTrace();
 			return null;
 		}
 	}
