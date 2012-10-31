91d648ac2e5a38858cf931891dc84525d0a3a83f
Alec Faithfull <alexander.faithfull@gmail.com>
2012-10-31 18:17:45 +0100

    [PortSpec] Added an Identifier subclass

--- plugins/org.bigraph.model/src/org/bigraph/model/PortSpec.java
+++ plugins/org.bigraph.model/src/org/bigraph/model/PortSpec.java
@@ -126,4 +126,63 @@ public class PortSpec extends NamedModelObject implements IPort {
 		
 		super.dispose();
 	}
+	
+	public static final class Identifier extends NamedModelObject.Identifier {
+		private final Control.Identifier control;
+		
+		public Identifier(String name, Control.Identifier control) {
+			super(name);
+			this.control = control;
+		}
+		
+		public Control.Identifier getControl() {
+			return control;
+		}
+		
+		@Override
+		public PortSpec lookup(PropertyScratchpad context, Resolver r) {
+			PortSpec n = require(r.lookup(context, this), PortSpec.class);
+			if (n != null && equals(n.getIdentifier(context))) {
+				return n;
+			} else return null;
+		}
+		
+		@Override
+		public Identifier getRenamed(String name) {
+			return new PortSpec.Identifier(name, getControl());
+		}
+		
+		@Override
+		public boolean equals(Object obj_) {
+			if (safeClassCmp(this, obj_)) {
+				Identifier obj = (Identifier)obj_;
+				return
+						safeEquals(getName(), obj.getName()) &&
+						safeEquals(getControl(), obj.getControl());
+			}
+			return false;
+		}
+		
+		@Override
+		public int hashCode() {
+			/* To ensure that the contract for hashCode() isn't violated,
+			 * don't use getControl() in this method */
+			return compositeHashCode(Identifier.class, getName());
+		}
+		
+		@Override
+		public String toString() {
+			return "port specification " + getName() + 
+					" (of " + getControl() + ")";
+		}
+	}
+	
+	public Identifier getIdentifier() {
+		return getIdentifier(null);
+	}
+	
+	public Identifier getIdentifier(PropertyScratchpad context) {
+		return new Identifier(getName(context),
+				getControl().getIdentifier(context));
+	}
 }
