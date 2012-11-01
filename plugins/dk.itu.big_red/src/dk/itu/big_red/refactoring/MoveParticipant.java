5ff79d18e24a98633f8d0d380893b2d176632461
Alec Faithfull <alexander.faithfull@gmail.com>
2012-11-01 14:10:05 +0100

    [MoveParticipant] Added a dummy move-refactoring participant
    Eclipse's weird XML expression language is genuinely surprisingly intuitive

---
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/refactoring/MoveParticipant.java
@@ -0,0 +1,37 @@
+package dk.itu.big_red.refactoring;
+
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.OperationCanceledException;
+import org.eclipse.ltk.core.refactoring.Change;
+import org.eclipse.ltk.core.refactoring.RefactoringStatus;
+import org.eclipse.ltk.core.refactoring.participants.CheckConditionsContext;
+
+public class MoveParticipant extends
+		org.eclipse.ltk.core.refactoring.participants.MoveParticipant {
+	@Override
+	protected boolean initialize(Object element) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RefactoringStatus checkConditions(IProgressMonitor pm,
+			CheckConditionsContext context) throws OperationCanceledException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Change createChange(IProgressMonitor pm) throws CoreException,
+			OperationCanceledException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
