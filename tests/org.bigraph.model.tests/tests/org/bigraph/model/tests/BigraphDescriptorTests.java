d1c876d9bff4d96afee6b5466048c68bf76962f4
Alec Faithfull <alexander.faithfull@gmail.com>
2012-12-03 16:56:56 +0100

    [tests] Added some descriptor execution tests for ChangeRemoveDescriptor

--- tests/org.bigraph.model.tests/tests/org/bigraph/model/tests/BigraphDescriptorTests.java
+++ tests/org.bigraph.model.tests/tests/org/bigraph/model/tests/BigraphDescriptorTests.java
@@ -4,8 +4,10 @@ import java.util.Arrays;
 
 import org.bigraph.model.Bigraph;
 import org.bigraph.model.InnerName;
+import org.bigraph.model.Layoutable;
 import org.bigraph.model.OuterName;
 import org.bigraph.model.Point;
+import org.bigraph.model.Root;
 import org.bigraph.model.changes.ChangeRejectedException;
 import org.bigraph.model.changes.descriptors.ChangeCreationException;
 import org.bigraph.model.changes.descriptors.ChangeDescriptorGroup;
@@ -78,5 +80,53 @@ public class BigraphDescriptorTests {
 		DescriptorExecutorManager.getInstance().tryApplyChange(b,
 				new Point.ChangeDisconnectDescriptor(
 						in.getIdentifier(), on.getIdentifier()));
+	}
+	
+	@Test
+	public void removeAddedRoot()
+			throws ChangeRejectedException, ChangeCreationException {
+		Bigraph b = new Bigraph();
+		Root r = new Root();
+		b.tryApplyChange(b.changeAddChild(r, "0"));
+		DescriptorExecutorManager.getInstance().tryApplyChange(b,
+				new Layoutable.ChangeRemoveDescriptor(
+						r.getIdentifier(), b.getIdentifier()));
+	}
+	
+	@Test(expected = ChangeCreationException.class)
+	public void removeAbsentRoot()
+			throws ChangeRejectedException, ChangeCreationException {
+		Bigraph b = new Bigraph();
+		DescriptorExecutorManager.getInstance().tryApplyChange(b,
+				new Layoutable.ChangeRemoveDescriptor(
+						new Root.Identifier("0"), b.getIdentifier()));
+	}
+	
+	@Test(expected = ChangeCreationException.class)
+	public void removeAddedRootTwice()
+			throws ChangeRejectedException, ChangeCreationException {
+		Bigraph b = new Bigraph();
+		Root r = new Root();
+		b.tryApplyChange(b.changeAddChild(r, "0"));
+		DescriptorExecutorManager.getInstance().tryApplyChange(b, cdg(
+				new Layoutable.ChangeRemoveDescriptor(
+						r.getIdentifier(), b.getIdentifier()),
+				new Layoutable.ChangeRemoveDescriptor(
+						r.getIdentifier(), b.getIdentifier())));
+	}
+	
+	@Test(expected = ChangeCreationException.class)
+	public void removeConnectedPoint()
+			throws ChangeRejectedException, ChangeCreationException {
+		Bigraph b = new Bigraph();
+		InnerName in = new InnerName();
+		OuterName on = new OuterName();
+		b.tryApplyChange(cg(
+				b.changeAddChild(in, "a"),
+				b.changeAddChild(on, "a"),
+				in.changeConnect(on)));
+		DescriptorExecutorManager.getInstance().tryApplyChange(b, cdg(
+				new Layoutable.ChangeRemoveDescriptor(
+						in.getIdentifier(), b.getIdentifier())));
 	}
 }
