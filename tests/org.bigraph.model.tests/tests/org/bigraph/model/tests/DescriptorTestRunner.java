4770adff569fdb5094aecb7f43367c5bbae1e70a
Alec Faithfull <alexander.faithfull@gmail.com>
2012-12-10 13:31:48 +0100

    [DescriptorTestRunner] Introduced a new base class for descriptor tests

---
+++ tests/org.bigraph.model.tests/tests/org/bigraph/model/tests/DescriptorTestRunner.java
@@ -0,0 +1,25 @@
+package org.bigraph.model.tests;
+
+import java.util.Arrays;
+
+import org.bigraph.model.ModelObject.Identifier.Resolver;
+import org.bigraph.model.changes.descriptors.ChangeCreationException;
+import org.bigraph.model.changes.descriptors.ChangeDescriptorGroup;
+import org.bigraph.model.changes.descriptors.IChangeDescriptor;
+import org.bigraph.model.changes.descriptors.experimental.DescriptorExecutorManager;
+
+public abstract class DescriptorTestRunner {
+	static ChangeDescriptorGroup cdg(IChangeDescriptor... changes) {
+		return new ChangeDescriptorGroup(Arrays.asList(changes));
+	}
+	
+	static void run(Resolver r, IChangeDescriptor... cds)
+			throws ChangeCreationException {
+		run(r, cdg(cds));
+	}
+	
+	static void run(Resolver r, ChangeDescriptorGroup cdg)
+			throws ChangeCreationException {
+		DescriptorExecutorManager.getInstance().tryApplyChange(r, cdg);
+	}
+}
