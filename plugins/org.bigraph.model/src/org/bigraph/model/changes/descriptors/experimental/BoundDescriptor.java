228478210f3b81414bb6fd13263140255c3a3e7d
Alec Faithfull <alexander.faithfull@gmail.com>
2012-12-11 15:02:59 +0100

    [BoundDescriptor] Added a bridge between IChanges and IChangeDescriptors
    An IChangeDescriptor with an associated Resolver is essentially an IChange;
    this new class takes advantage of that to allow IChangeDescriptors to be
    executed as Changes

---
+++ plugins/org.bigraph.model/src/org/bigraph/model/changes/descriptors/experimental/BoundDescriptor.java
@@ -0,0 +1,133 @@
+package org.bigraph.model.changes.descriptors.experimental;
+
+import org.bigraph.model.ModelObject.Identifier.Resolver;
+import org.bigraph.model.assistants.ExecutorManager;
+import org.bigraph.model.assistants.PropertyScratchpad;
+import org.bigraph.model.changes.ChangeRejectedException;
+import org.bigraph.model.changes.IChange;
+import org.bigraph.model.changes.IStepExecutor;
+import org.bigraph.model.changes.IStepValidator;
+import org.bigraph.model.changes.descriptors.ChangeCreationException;
+import org.bigraph.model.changes.descriptors.IChangeDescriptor;
+import org.bigraph.model.changes.descriptors.experimental.IDescriptorStepValidator.Callback;
+
+public class BoundDescriptor implements IChange {
+	private final Resolver resolver;
+	private final IChangeDescriptor descriptor;
+	
+	private final class ProcessWrapper
+			implements IDescriptorStepValidator.Process {
+		private final IStepValidator.Process changeProcess;
+		
+		public ProcessWrapper(IStepValidator.Process changeProcess) {
+			this.changeProcess = changeProcess;
+		}
+		
+		@Override
+		public void addCallback(final Callback c) {
+			changeProcess.addCallback(new IStepValidator.Callback() {
+				@Override
+				public void run() throws ChangeRejectedException {
+					try {
+						c.run();
+					} catch (ChangeCreationException cce) {
+						throw new ChangeRejectedException(BoundDescriptor.this,
+								cce.getRationale());
+					}
+				}
+			});
+		}
+		
+		@Override
+		public Resolver getResolver() {
+			return BoundDescriptor.this.getResolver();
+		}
+		
+		@Override
+		public PropertyScratchpad getScratch() {
+			return changeProcess.getScratch();
+		}
+	}
+	
+	private static final class Handler
+			implements IStepValidator, IStepExecutor {
+		private static final Handler INSTANCE = new Handler();
+		
+		@Override
+		public boolean tryValidateChange(Process context, IChange change)
+				throws ChangeRejectedException {
+			if (change instanceof BoundDescriptor) {
+				BoundDescriptor bd = (BoundDescriptor)change;
+				try {
+					DescriptorExecutorManager.getInstance().tryValidateChange(
+							bd.new ProcessWrapper(context),
+							bd.getDescriptor());
+				} catch (ChangeCreationException cce) {
+					throw new ChangeRejectedException(bd, cce.getRationale());
+				}
+			} else return false;
+			return true;
+		}
+		
+		@Override
+		public boolean executeChange(IChange change) {
+			if (change instanceof BoundDescriptor) {
+				BoundDescriptor bd = (BoundDescriptor)change;
+				try {
+					DescriptorExecutorManager.getInstance().tryApplyChange(
+							bd.getResolver(), bd.getDescriptor());
+				} catch (ChangeCreationException cce) {
+					throw new Error(
+							"BUG: BoundDescriptor passed validation " +
+							"but couldn't be executed");
+				}
+			} else return false;
+			return true;
+		}
+	}
+	
+	static {
+		ExecutorManager.getInstance().addHandler(Handler.INSTANCE);
+	}
+	
+	public BoundDescriptor(Resolver resolver, IChangeDescriptor descriptor) {
+		this.resolver = resolver;
+		this.descriptor = descriptor;
+	}
+
+	public Resolver getResolver() {
+		return resolver;
+	}
+	
+	public IChangeDescriptor getDescriptor() {
+		return descriptor;
+	}
+	
+	@Override
+	public BoundDescriptor inverse() {
+		return new BoundDescriptor(getResolver(), getDescriptor().inverse());
+	}
+
+	@Override
+	public boolean canInvert() {
+		return true;
+	}
+
+	@Override
+	public void beforeApply() {
+	}
+
+	@Override
+	public boolean isReady() {
+		return true;
+	}
+
+	@Override
+	public void simulate(PropertyScratchpad context) {
+		try {
+			getDescriptor().simulate(context, getResolver());
+		} catch (ChangeCreationException cce) {
+		}
+	}
+
+}
