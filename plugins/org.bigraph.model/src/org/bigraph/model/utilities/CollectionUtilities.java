a092681a59f52521e114bfd9787092d8ab1a1d72
Alec Faithfull <alexander.faithfull@gmail.com>
2013-01-14 15:55:13 +0100

    [utilities] Added some utility methods for dealing with Iterables
    I think Big Red might be relying on bare Iterables slightly too much. These
    methods will bridge the gap between Iterables and Collections until I come
    up with a better solution

---
+++ plugins/org.bigraph.model/src/org/bigraph/model/utilities/CollectionUtilities.java
@@ -0,0 +1,40 @@
+package org.bigraph.model.utilities;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Iterator;
+
+public abstract class CollectionUtilities {
+	private CollectionUtilities() {}
+	
+	public static final <T> Collection<T> collect(Iterable<? extends T> it) {
+		return collect(it.iterator());
+	}
+	
+	public static final <T> Collection<T> collect(Iterator<? extends T> it) {
+		ArrayList<T> r = new ArrayList<T>();
+		while (it.hasNext())
+			r.add(it.next());
+		return Collections.unmodifiableList(r);
+	}
+	
+	public static final <T> Collection<T> collect(
+			Iterable<? extends T> it, Comparator<? super T> cmp) {
+		return collect(it.iterator(), cmp);
+	}
+	
+	public static final <T> Collection<T> collect(
+			Iterator<? extends T> it, Comparator<? super T> cmp) {
+		ArrayList<T> r = new ArrayList<T>();
+		while (it.hasNext()) {
+			T i = it.next();
+			int position = Collections.binarySearch(r, i, cmp);
+			if (position < 0)
+				position = -(position + 1);
+			r.add(position, i);
+		}
+		return Collections.unmodifiableList(r);
+	}
+}
