30081c1be62790c4e81d4429746e42fef45f8355
Alec Faithfull <alexander.faithfull@gmail.com>
2013-01-17 15:53:55 +0100

    [ChangeExtensions] Register and unregister change descriptor contributions
    This commit also tidies up the code a bit -- in particular, object creation
    (and its error handling) now lives in a separate private static method

--- plugins/org.bigraph.model.wrapper/src/org/bigraph/model/wrapper/ChangeExtensions.java
+++ plugins/org.bigraph.model.wrapper/src/org/bigraph/model/wrapper/ChangeExtensions.java
@@ -5,19 +5,38 @@ import java.util.ArrayList;
 import org.bigraph.model.assistants.ExecutorManager;
 import org.bigraph.model.changes.IStepExecutor;
 import org.bigraph.model.changes.IStepValidator;
+import org.bigraph.model.changes.descriptors.DescriptorExecutorManager;
+import org.bigraph.model.changes.descriptors.IDescriptorStepExecutor;
+import org.bigraph.model.changes.descriptors.IDescriptorStepValidator;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.RegistryFactory;
 
 final class ChangeExtensions {
-	private static final ArrayList<IStepExecutor> executors =
-			new ArrayList<IStepExecutor>();
-	private static final ArrayList<IStepValidator> validators =
-			new ArrayList<IStepValidator>();
+	private static final ArrayList<IStepExecutor>
+			executors = new ArrayList<IStepExecutor>();
+	private static final ArrayList<IStepValidator>
+			validators = new ArrayList<IStepValidator>();
+	
+	private static final ArrayList<IDescriptorStepExecutor>
+			descriptorExecutors = new ArrayList<IDescriptorStepExecutor>();
+	private static final ArrayList<IDescriptorStepValidator>
+			descriptorValidators = new ArrayList<IDescriptorStepValidator>();
+	
+	private static final Object instantiate(IConfigurationElement ice) {
+		try {
+			return ice.createExecutableExtension("class");
+		} catch (CoreException ce) {
+			ce.printStackTrace();
+		}
+		return null;
+	}
 	
 	static void init() {
 		ExecutorManager eInstance = ExecutorManager.getInstance();
+		DescriptorExecutorManager deInstance =
+				DescriptorExecutorManager.getInstance();
 		IExtensionRegistry registry = RegistryFactory.getRegistry();
 		
 		for (IConfigurationElement ice :
@@ -25,33 +44,40 @@ final class ChangeExtensions {
 						Activator.EXTENSION_POINT_CHANGES)) {
 			String name = ice.getName();
 			if ("executor".equals(name)) {
-				IStepExecutor executor;
-				try {
-					executor = (IStepExecutor)
-							ice.createExecutableExtension("class");
-				} catch (CoreException ce) {
-					ce.printStackTrace();
-					continue;
+				IStepExecutor executor = (IStepExecutor)instantiate(ice);
+				if (executor != null) {
+					executors.add(executor);
+					eInstance.addExecutor(executor);
 				}
-				executors.add(executor);
-				eInstance.addExecutor(executor);
 			} else if ("validator".equals(name)) {
-				IStepValidator validator;
-				try {
-					validator = (IStepValidator)
-							ice.createExecutableExtension("class");
-				} catch (CoreException ce) {
-					ce.printStackTrace();
-					continue;
+				IStepValidator validator = (IStepValidator)instantiate(ice);
+				if (validator != null) {
+					validators.add(validator);
+					eInstance.addValidator(validator);
 				}
-				validators.add(validator);
-				eInstance.addValidator(validator);
+			} else if ("descriptorExecutor".equals(name)) {
+				IDescriptorStepExecutor executor =
+						(IDescriptorStepExecutor)instantiate(ice);
+				if (executor != null) {
+					descriptorExecutors.add(executor);
+					deInstance.addExecutor(executor);
+				}
+			} else if ("descriptorValidator".equals(name)) {
+				IDescriptorStepValidator validator =
+						(IDescriptorStepValidator)instantiate(ice);
+				if (validator != null) {
+					descriptorValidators.add(validator);
+					deInstance.addValidator(validator);
+				}
 			}
 		}
 	}
 	
 	static void fini() {
 		ExecutorManager eInstance = ExecutorManager.getInstance();
+		DescriptorExecutorManager deInstance =
+				DescriptorExecutorManager.getInstance();
+		
 		for (IStepExecutor i : executors)
 			eInstance.removeExecutor(i);
 		for (IStepValidator i : validators)
@@ -59,5 +85,13 @@ final class ChangeExtensions {
 		
 		executors.clear();
 		validators.clear();
+		
+		for (IDescriptorStepExecutor i : descriptorExecutors)
+			deInstance.removeExecutor(i);
+		for (IDescriptorStepValidator i : descriptorValidators)
+			deInstance.removeValidator(i);
+		
+		descriptorExecutors.clear();
+		descriptorValidators.clear();
 	}
 }
