db9ea1d31896d640c855c213ca73038e0d1938a1
Alec Faithfull <alexander.faithfull@gmail.com>
2013-02-08 09:48:09 +0100

    [Store] Added a new class for holding orphaned abandoned data
    In the future, it won't be possible to delete an object that still has
    extended data. Store provides dummy ModelObjects on demand that can be
    used to stash extended data away -- or to retrieve it again

---
+++ plugins/org.bigraph.model/src/org/bigraph/model/Store.java
@@ -0,0 +1,166 @@
+package org.bigraph.model;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.bigraph.model.ModelObject.Identifier.Resolver;
+import org.bigraph.model.assistants.PropertyScratchpad;
+import org.bigraph.model.changes.descriptors.ChangeCreationException;
+import org.bigraph.model.changes.descriptors.DescriptorExecutorManager;
+import org.bigraph.model.changes.descriptors.IChangeDescriptor;
+
+public class Store implements Resolver {
+	protected static abstract class StoreChangeDescriptor
+			implements IChangeDescriptor {
+		static {
+			DescriptorExecutorManager.getInstance().addParticipant(
+					new StoreDescriptorHandler());
+		}
+		
+		private final ModelObject.Identifier id;
+		private final EntryIdentifier eid;
+		
+		protected StoreChangeDescriptor(
+				ModelObject.Identifier id, EntryIdentifier eid) {
+			this.id = id;
+			this.eid = eid;
+		}
+		
+		public ModelObject.Identifier getID() {
+			return id;
+		}
+		
+		public EntryIdentifier getEntryID() {
+			return eid;
+		}
+		
+		protected static void doSimulate(PropertyScratchpad context,
+				ModelObject source, ModelObject target) {
+			Map<String, Object>
+				sourceMap = source.getModifiableExtendedDataMap(context),
+				targetMap = target.getModifiableExtendedDataMap(context);
+			sourceMap.putAll(targetMap);
+			targetMap.clear();
+		}
+	}
+	
+	public static final class ToStoreDescriptor
+			extends StoreChangeDescriptor {
+		public ToStoreDescriptor(
+				ModelObject.Identifier id, EntryIdentifier eid) {
+			super(id, eid);
+		}
+		
+		@Override
+		public IChangeDescriptor inverse() {
+			return new ToStoreDescriptor(getID(), getEntryID());
+		}
+		
+		@Override
+		public void simulate(PropertyScratchpad context, Resolver r)
+				throws ChangeCreationException {
+			doSimulate(context, getID().lookup(context, r),
+					getEntryID().lookup(context, r));
+		}
+	}
+	
+	public static final class FromStoreDescriptor
+			extends StoreChangeDescriptor {
+		public FromStoreDescriptor(
+				ModelObject.Identifier id, EntryIdentifier eid) {
+			super(id, eid);
+		}
+		
+		@Override
+		public IChangeDescriptor inverse() {
+			return new ToStoreDescriptor(getID(), getEntryID());
+		}
+		
+		@Override
+		public void simulate(PropertyScratchpad context, Resolver r)
+				throws ChangeCreationException {
+			doSimulate(context, getEntryID().lookup(context, r),
+					getID().lookup(context, r));
+		}
+	}
+	
+	private static final class DummyModelObject extends ModelObject {
+		private final Identifier id;
+		
+		public DummyModelObject(Identifier id) {
+			this.id = id;
+		}
+		
+		@Override
+		public Identifier getIdentifier() {
+			return id;
+		}
+		
+		@Override
+		public Identifier getIdentifier(PropertyScratchpad context) {
+			return id;
+		}
+	}
+	
+	private Long nextID = new Long(0);
+	
+	private long getNextID() {
+		synchronized (nextID) {
+			return nextID++;
+		}
+	}
+	
+	private Map<Long, ModelObject> entries =
+			new HashMap<Long, ModelObject>();
+	
+	public EntryIdentifier createID() {
+		return new EntryIdentifier(getNextID());
+	}
+	
+	public boolean drop(long id) {
+		return (entries.remove(id) != null);
+	}
+	
+	public static class EntryIdentifier implements ModelObject.Identifier {
+		private final long id;
+		
+		public EntryIdentifier(long id) {
+			this.id = id;
+		}
+		
+		public long getID() {
+			return id;
+		}
+		
+		@Override
+		public boolean equals(Object obj) {
+			return (ModelObject.safeClassCmp(this, obj) &&
+					((EntryIdentifier)obj).getID() == getID());
+		}
+		
+		@Override
+		public int hashCode() {
+			return ModelObject.compositeHashCode(getClass(), getID());
+		}
+		
+		@Override
+		public ModelObject lookup(PropertyScratchpad context, Resolver r) {
+			return ModelObject.require(
+					r.lookup(context, this), ModelObject.class);
+		}
+	}
+	
+	@Override
+	public Object lookup(
+			PropertyScratchpad context, ModelObject.Identifier identifier) {
+		if (identifier instanceof EntryIdentifier) {
+			long id = ((EntryIdentifier)identifier).getID();
+			ModelObject result;
+			if (!entries.containsKey(id)) {
+				entries.put(id,
+						result = new DummyModelObject(identifier));
+			} else result = entries.get(id);
+			return result;
+		} else return null;
+	}
+}
---
+++ plugins/org.bigraph.model/src/org/bigraph/model/StoreDescriptorHandler.java
@@ -0,0 +1,59 @@
+package org.bigraph.model;
+
+import org.bigraph.model.ModelObject.Identifier.Resolver;
+import org.bigraph.model.Store.FromStoreDescriptor;
+import org.bigraph.model.Store.StoreChangeDescriptor;
+import org.bigraph.model.Store.ToStoreDescriptor;
+import org.bigraph.model.assistants.PropertyScratchpad;
+import org.bigraph.model.changes.descriptors.ChangeCreationException;
+import org.bigraph.model.changes.descriptors.IChangeDescriptor;
+
+final class StoreDescriptorHandler
+		extends HandlerUtilities.DescriptorHandlerImpl {
+	@Override
+	public boolean tryValidateChange(Process context, IChangeDescriptor change)
+			throws ChangeCreationException {
+		final PropertyScratchpad scratch = context.getScratch();
+		final Resolver resolver = context.getResolver();
+		if (change instanceof StoreChangeDescriptor) {
+			StoreChangeDescriptor cd = (StoreChangeDescriptor)change;
+			
+			ModelObject.Identifier sourceI, targetI;
+			if (cd instanceof FromStoreDescriptor) {
+				sourceI = cd.getEntryID();
+				targetI = cd.getID();
+			} else if (cd instanceof ToStoreDescriptor) {
+				sourceI = cd.getID();
+				targetI = cd.getEntryID();
+			} else return false;
+			
+			tryLookup(cd, sourceI, scratch, resolver, ModelObject.class);
+			ModelObject
+				target = tryLookup(cd,
+						targetI, scratch, resolver, ModelObject.class);
+			
+			if (!target.getExtendedDataMap(scratch).isEmpty())
+				throw new ChangeCreationException(cd,
+						"" + targetI + " mustn't have any extended data");
+		} else return false;
+		return true;
+	}
+
+	@Override
+	public boolean executeChange(Resolver resolver, IChangeDescriptor change) {
+		if (change instanceof StoreChangeDescriptor) {
+			StoreChangeDescriptor cd = (StoreChangeDescriptor)change;
+			
+			ModelObject
+				source = (cd instanceof FromStoreDescriptor ?
+						cd.getEntryID() : cd.getID()).lookup(null, resolver),
+				target = (cd instanceof FromStoreDescriptor ?
+						cd.getID() : cd.getEntryID()).lookup(null, resolver);
+
+			source.getExtendedDataMap().putAll(
+					target.getExtendedDataMap());
+			target.getExtendedDataMap().clear();
+		} else return false;
+		return true;
+	}
+}
