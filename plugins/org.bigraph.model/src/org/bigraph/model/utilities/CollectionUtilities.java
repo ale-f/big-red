25c0df8dcd33b11a43b2bca1e4fd032f4a2453e8
Alec Faithfull <alexander.faithfull@gmail.com>
2013-02-21 12:56:35 +0100

    [CollectionUtilities] Strengthened the return type of the collect methods
    All four methods now return a List, not a bare Collection

--- plugins/org.bigraph.model/src/org/bigraph/model/utilities/CollectionUtilities.java
+++ plugins/org.bigraph.model/src/org/bigraph/model/utilities/CollectionUtilities.java
@@ -1,31 +1,31 @@
 package org.bigraph.model.utilities;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Iterator;
+import java.util.List;
 
 public abstract class CollectionUtilities {
 	private CollectionUtilities() {}
 	
-	public static final <T> Collection<T> collect(Iterable<? extends T> it) {
+	public static final <T> List<T> collect(Iterable<? extends T> it) {
 		return collect(it.iterator());
 	}
 	
-	public static final <T> Collection<T> collect(Iterator<? extends T> it) {
+	public static final <T> List<T> collect(Iterator<? extends T> it) {
 		ArrayList<T> r = new ArrayList<T>();
 		while (it.hasNext())
 			r.add(it.next());
 		return Collections.unmodifiableList(r);
 	}
 	
-	public static final <T> Collection<T> collect(
+	public static final <T> List<T> collect(
 			Iterable<? extends T> it, Comparator<? super T> cmp) {
 		return collect(it.iterator(), cmp);
 	}
 	
-	public static final <T> Collection<T> collect(
+	public static final <T> List<T> collect(
 			Iterator<? extends T> it, Comparator<? super T> cmp) {
 		ArrayList<T> r = new ArrayList<T>();
 		while (it.hasNext()) {
