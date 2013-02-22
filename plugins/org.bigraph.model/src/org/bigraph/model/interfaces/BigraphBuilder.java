bdac260a4d4947954a611fbd3264d4eefce64159
Alec Faithfull <alexander.faithfull@gmail.com>
2013-02-22 13:57:27 +0100

    [BigraphBuilder] Replaced all changes with change descriptors

--- plugins/org.bigraph.model/src/org/bigraph/model/interfaces/BigraphBuilder.java
+++ plugins/org.bigraph.model/src/org/bigraph/model/interfaces/BigraphBuilder.java
@@ -12,26 +12,19 @@ import org.bigraph.model.Point;
 import org.bigraph.model.Root;
 import org.bigraph.model.Signature;
 import org.bigraph.model.Site;
-import org.bigraph.model.assistants.PropertyScratchpad;
 import org.bigraph.model.changes.descriptors.ChangeCreationException;
-import org.bigraph.model.changes.descriptors.ChangeDescriptorGroup;
 import org.bigraph.model.changes.descriptors.DescriptorExecutorManager;
 import org.bigraph.model.changes.descriptors.IChangeDescriptor;
 
 public class BigraphBuilder {
 	private Bigraph b = new Bigraph();
-	private ChangeDescriptorGroup cdg = new ChangeDescriptorGroup();
-	private PropertyScratchpad scratch = new PropertyScratchpad();
 	
-	private void addChange(IChangeDescriptor ch) {
-		if (ch == null)
-			return;
-		ch.simulate(getScratch(), b);
-		cdg.add(ch);
-	}
-	
-	private PropertyScratchpad getScratch() {
-		return scratch;
+	private void change(IChangeDescriptor ch) {
+		try {
+			DescriptorExecutorManager.getInstance().tryApplyChange(b, ch);
+		} catch (ChangeCreationException cce) {
+			/* do nothing */
+		}
 	}
 	
 	public BigraphBuilder(ISignature s) {
@@ -39,53 +32,54 @@ public class BigraphBuilder {
 	}
 	
 	public IEdge newEdge(String name) {
-		Edge e = new Edge();
-		addChange(b.changeAddChild(e, name));
-		return e;
+		Edge.Identifier eid = new Edge.Identifier(name);
+		change(new Container.ChangeAddChildDescriptor(
+				new Bigraph.Identifier(), eid));
+		return eid.lookup(null, b);
 	}
 	
 	public IOuterName newOuterName(String name) {
-		OuterName o = new OuterName();
-		addChange(b.changeAddChild(o, name));
-		return o;
+		OuterName.Identifier oid = new OuterName.Identifier(name);
+		change(new Container.ChangeAddChildDescriptor(
+				new Bigraph.Identifier(), oid));
+		return oid.lookup(null, b);
 	}
 	
 	public IInnerName newInnerName(String name) {
-		InnerName i = new InnerName();
-		addChange(b.changeAddChild(i, name));
-		return i;
+		InnerName.Identifier iid = new InnerName.Identifier(name);
+		change(new Container.ChangeAddChildDescriptor(
+				new Bigraph.Identifier(), iid));
+		return iid.lookup(null, b);
 	}
 	
 	public IRoot newRoot(String name) {
-		Root r = new Root();
-		addChange(b.changeAddChild(r, name));
-		return r;
+		Root.Identifier rid = new Root.Identifier(name);
+		change(new Container.ChangeAddChildDescriptor(
+				new Bigraph.Identifier(), rid));
+		return rid.lookup(null, b);
 	}
 	
 	public INode newNode(IParent parent, IControl c, String name) {
-		Node n = new Node((Control)c);
-		addChange(((Container)parent).changeAddChild(n, name));
-		return n;
+		Node.Identifier nid = new Node.Identifier(
+				name, ((Control)c).getIdentifier());
+		change(new Container.ChangeAddChildDescriptor(
+				((Container)parent).getIdentifier(), nid));
+		return nid.lookup(null, b);
 	}
 	
 	public void newConnection(IPoint p, ILink l) {
-		addChange(new Point.ChangeConnectDescriptor(
-				((Point)p).getIdentifier(getScratch()),
-				((Link)l).getIdentifier(getScratch())));
+		change(new Point.ChangeConnectDescriptor(
+				((Point)p).getIdentifier(), ((Link)l).getIdentifier()));
 	}
 	
 	public ISite newSite(IParent parent, String name) {
-		Site s = new Site();
-		addChange(((Container)parent).changeAddChild(s, name));
-		return s;
+		Site.Identifier sid = new Site.Identifier(name);
+		change(new Container.ChangeAddChildDescriptor(
+				((Container)parent).getIdentifier(), sid));
+		return sid.lookup(null, b);
 	}
 	
 	public Bigraph finish() {
-		try {
-			DescriptorExecutorManager.getInstance().tryApplyChange(b, cdg);
-			return b;
-		} catch (ChangeCreationException e) {
-			return null;
-		}
+		return b;
 	}
 }
