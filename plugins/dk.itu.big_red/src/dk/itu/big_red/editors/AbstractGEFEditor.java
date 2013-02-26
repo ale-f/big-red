aa1abba6b49ed6474505872e8468e07be2199b6d
Alec Faithfull <alexander.faithfull@gmail.com>
2013-02-26 11:00:39 +0100

    [AbstractGEFEditor] Sort controls using NamedModelObject's utility methods

--- plugins/dk.itu.big_red/src/dk/itu/big_red/editors/AbstractGEFEditor.java
+++ plugins/dk.itu.big_red/src/dk/itu/big_red/editors/AbstractGEFEditor.java
@@ -1,20 +1,17 @@
 package dk.itu.big_red.editors;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 import org.bigraph.model.Control;
 import org.bigraph.model.Edge;
 import org.bigraph.model.InnerName;
+import org.bigraph.model.NamedModelObject;
 import org.bigraph.model.Node;
 import org.bigraph.model.OuterName;
 import org.bigraph.model.Root;
 import org.bigraph.model.Signature;
 import org.bigraph.model.Site;
-import org.bigraph.model.utilities.CollectionUtilities;
-import org.bigraph.model.utilities.comparators.ComparatorUtilities;
-import org.bigraph.model.utilities.comparators.ComparatorUtilities.Converter;
 import org.bigraph.model.utilities.comparators.LexicographicStringComparator;
 import org.eclipse.gef.DefaultEditDomain;
 import org.eclipse.gef.GraphicalViewer;
@@ -295,20 +292,11 @@ public abstract class AbstractGEFEditor extends AbstractEditor
 		return paletteRoot;
 	}
 	
-	private static final Comparator<Control> CONTROL_NAME_COMPARATOR =
-			ComparatorUtilities.convertComparator(
-				new Converter<Control, String>() {
-					@Override
-					public String convert(Control object) {
-						return object.getName();
-					}
-				}, LexicographicStringComparator.INSTANCE);
-	
 	private static PaletteContainer sigPop(
 			List<String> names, PaletteContainer pc, Signature signature) {
 		pc.setLabel(signature.toString());
-		for (Control c : CollectionUtilities.collect(
-				signature.getControls(), CONTROL_NAME_COMPARATOR)) {
+		for (Control c : NamedModelObject.order(signature.getControls(),
+				LexicographicStringComparator.INSTANCE)) {
 			if (names.contains(c.getName()))
 				continue;
 			names.add(c.getName());
