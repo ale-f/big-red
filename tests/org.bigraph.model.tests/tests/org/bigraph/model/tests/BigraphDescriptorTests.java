cdd0a53c40fd00069310a618cc7373397b304910
Alec Faithfull <alexander.faithfull@gmail.com>
2013-02-28 15:13:07 +0100

    [BigraphDescriptorTests] Replaced the remaining changes with descriptors

--- tests/org.bigraph.model.tests/tests/org/bigraph/model/tests/BigraphDescriptorTests.java
+++ tests/org.bigraph.model.tests/tests/org/bigraph/model/tests/BigraphDescriptorTests.java
@@ -8,7 +8,6 @@ import org.bigraph.model.OuterName;
 import org.bigraph.model.Point;
 import org.bigraph.model.Root;
 import org.bigraph.model.changes.descriptors.ChangeCreationException;
-import org.bigraph.model.changes.descriptors.DescriptorExecutorManager;
 import org.junit.Test;
 
 public class BigraphDescriptorTests extends DescriptorTestRunner {
@@ -48,109 +47,89 @@ public class BigraphDescriptorTests extends DescriptorTestRunner {
 	@Test
 	public void connectInnerToOuterDescriptor()
 			throws ChangeCreationException {
-		Bigraph b = new Bigraph();
-		InnerName in = new InnerName();
-		OuterName on = new OuterName();
-		DescriptorExecutorManager.getInstance().tryApplyChange(null, cdg(
-				b.changeAddChild(in, "a"),
-				b.changeAddChild(on, "b")));
-		run(b,
-				new Point.ChangeConnectDescriptor(
-						in.getIdentifier(), on.getIdentifier()));
+		Bigraph.Identifier b = new Bigraph.Identifier();
+		InnerName.Identifier in = new InnerName.Identifier("a");
+		OuterName.Identifier on = new OuterName.Identifier("b");
+		run(new Bigraph(),
+				new Container.ChangeAddChildDescriptor(b, in),
+				new Container.ChangeAddChildDescriptor(b, on),
+				new Point.ChangeConnectDescriptor(in, on));
 	}
 	
 	@Test(expected = ChangeCreationException.class)
 	public void connectInnerToOuterDescriptorTwice()
 			throws ChangeCreationException {
-		Bigraph b = new Bigraph();
-		InnerName in = new InnerName();
-		OuterName on = new OuterName();
-		DescriptorExecutorManager.getInstance().tryApplyChange(null, cdg(
-				b.changeAddChild(in, "a"),
-				b.changeAddChild(on, "b")));
-		run(b,
-				new Point.ChangeConnectDescriptor(
-						in.getIdentifier(), on.getIdentifier()),
-				new Point.ChangeConnectDescriptor(
-						in.getIdentifier(), on.getIdentifier()));
+		Bigraph.Identifier b = new Bigraph.Identifier();
+		InnerName.Identifier in = new InnerName.Identifier("a");
+		OuterName.Identifier on = new OuterName.Identifier("b");
+		run(new Bigraph(),
+				new Container.ChangeAddChildDescriptor(b, in),
+				new Container.ChangeAddChildDescriptor(b, on),
+				new Point.ChangeConnectDescriptor(in, on),
+				new Point.ChangeConnectDescriptor(in, on));
 	}
 	
 	@Test
 	public void connectAndDisconnectInnerFromOuterDescriptor()
 			throws ChangeCreationException {
-		Bigraph b = new Bigraph();
-		InnerName in = new InnerName();
-		OuterName on = new OuterName();
-		DescriptorExecutorManager.getInstance().tryApplyChange(null, cdg(
-				b.changeAddChild(in, "a"),
-				b.changeAddChild(on, "b")));
-		run(b,
-				new Point.ChangeConnectDescriptor(
-						in.getIdentifier(), on.getIdentifier()),
-				new Point.ChangeDisconnectDescriptor(
-						in.getIdentifier(), on.getIdentifier()));
+		Bigraph.Identifier b = new Bigraph.Identifier();
+		InnerName.Identifier in = new InnerName.Identifier("a");
+		OuterName.Identifier on = new OuterName.Identifier("b");
+		run(new Bigraph(),
+				new Container.ChangeAddChildDescriptor(b, in),
+				new Container.ChangeAddChildDescriptor(b, on),
+				new Point.ChangeConnectDescriptor(in, on),
+				new Point.ChangeDisconnectDescriptor(in, on));
 	}
 	
 	@Test(expected = ChangeCreationException.class)
 	public void disconnectInnerFromOuterDescriptor()
 			throws ChangeCreationException {
-		Bigraph b = new Bigraph();
-		InnerName in = new InnerName();
-		OuterName on = new OuterName();
-		DescriptorExecutorManager.getInstance().tryApplyChange(null, cdg(
-				b.changeAddChild(in, "a"),
-				b.changeAddChild(on, "b")));
-		run(b,
-				new Point.ChangeDisconnectDescriptor(
-						in.getIdentifier(), on.getIdentifier()));
+		Bigraph.Identifier b = new Bigraph.Identifier();
+		InnerName.Identifier in = new InnerName.Identifier("a");
+		OuterName.Identifier on = new OuterName.Identifier("b");
+		run(new Bigraph(),
+				new Container.ChangeAddChildDescriptor(b, in),
+				new Container.ChangeAddChildDescriptor(b, on),
+				new Point.ChangeDisconnectDescriptor(in, on));
 	}
 	
 	@Test
 	public void removeAddedRoot() throws ChangeCreationException {
-		Bigraph b = new Bigraph();
-		Root r = new Root();
-		DescriptorExecutorManager.getInstance().tryApplyChange(null,
-				b.changeAddChild(r, "0"));
-		run(b,
-				new Container.ChangeRemoveChildDescriptor(
-						b.getIdentifier(), r.getIdentifier()));
+		Bigraph.Identifier b = new Bigraph.Identifier();
+		Root.Identifier r = new Root.Identifier("0");
+		run(new Bigraph(),
+				new Container.ChangeAddChildDescriptor(b, r),
+				new Container.ChangeRemoveChildDescriptor(b, r));
 	}
 	
 	@Test(expected = ChangeCreationException.class)
 	public void removeAbsentRoot() throws ChangeCreationException {
-		Bigraph b = new Bigraph();
 		run(new Bigraph(),
 				new Container.ChangeRemoveChildDescriptor(
-						b.getIdentifier(), new Root.Identifier("0")));
+						new Bigraph.Identifier(), new Root.Identifier("0")));
 	}
 	
 	@Test(expected = ChangeCreationException.class)
 	public void removeAddedRootTwice() throws ChangeCreationException {
-		Bigraph b = new Bigraph();
-		Root r = new Root();
-		DescriptorExecutorManager.getInstance().tryApplyChange(null,
-				b.changeAddChild(r, "0"));
-		run(b,
-				new Container.ChangeRemoveChildDescriptor(
-						b.getIdentifier(), r.getIdentifier()),
-				new Container.ChangeRemoveChildDescriptor(
-						b.getIdentifier(), r.getIdentifier()));
+		Bigraph.Identifier b = new Bigraph.Identifier();
+		Root.Identifier r = new Root.Identifier("0");
+		run(new Bigraph(),
+				new Container.ChangeAddChildDescriptor(b, r),
+				new Container.ChangeRemoveChildDescriptor(b, r),
+				new Container.ChangeRemoveChildDescriptor(b, r));
 	}
 	
 	@Test(expected = ChangeCreationException.class)
 	public void removeConnectedPoint() throws ChangeCreationException {
-		Bigraph b = new Bigraph();
-		InnerName in = new InnerName();
-		OuterName on = new OuterName();
-		DescriptorExecutorManager.getInstance().tryApplyChange(null, cdg(
-				b.changeAddChild(in, "a"),
-				b.changeAddChild(on, "a")));
-		run(b,
-				new Point.ChangeConnectDescriptor(
-						new InnerName.Identifier("a"),
-						new OuterName.Identifier("a")),
-				new Container.ChangeRemoveChildDescriptor(
-						b.getIdentifier(), in.getIdentifier()));
+		Bigraph.Identifier b = new Bigraph.Identifier();
+		InnerName.Identifier in = new InnerName.Identifier("a");
+		OuterName.Identifier on = new OuterName.Identifier("a");
+		run(new Bigraph(),
+				new Container.ChangeAddChildDescriptor(b, in),
+				new Container.ChangeAddChildDescriptor(b, on),
+				new Point.ChangeConnectDescriptor(in, on),
+				new Container.ChangeRemoveChildDescriptor(b, in));
 	}
 	
 	@Test
